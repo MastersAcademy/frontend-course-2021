9411ce07b9bc267a243bf21dc8f7b47c79ca247b
Chakipaki <belyeve2@icloud.com>
2021-10-27 22:54:02 +0300

    Upgrate

--- homeworks/ivan.bieliaiev_Ckakipaki/2-dom/app.js
+++ homeworks/ivan.bieliaiev_Ckakipaki/2-dom/app.js
@@ -5,25 +5,25 @@ let amountOfMesseges = (document.querySelectorAll('.messege__item')).length;
 
 function createMessege(){
     let div = document.createElement('div');
-        div.classList = `messege__item mes__item${amountOfMesseges + 1}`;
-        let p = document.createElement('p');
-        p.classList = 'messege__text';
-        p.innerText = inputText.value;
-        let a = document.createElement('a');
-        a.classList = `delete__button delete__button${amountOfMesseges + 1}`;
-        a.innerText = "del";
-        p.appendChild(a);
-        div.appendChild(p);
-        messeges.appendChild(div);
-        inputText.value = '';
-        amountOfMesseges++;
+    div.classList = `messege__item mes__item${amountOfMesseges + 1}`;
+    let p = document.createElement('p');
+    p.classList = 'messege__text';
+    p.innerText = inputText.value;
+    let a = document.createElement('a');
+    a.classList = `delete__button delete__button${amountOfMesseges + 1}`;
+    a.innerHTML = '&#9746';
+    div.appendChild(p);
+    div.appendChild(a);
+    messeges.appendChild(div);
+    inputText.value = '';
+    amountOfMesseges++;
 }
 // For click (adding and removing messeges)
 document.addEventListener('click', function(e){
     console.log(e.target.classList[0])
     if(e.target.classList[0] === 'delete__button'){
         let tar = document.querySelector(`.${e.target.classList[1]}`);
-        messeges.removeChild(tar.parentElement.parentElement)
+        messeges.removeChild(tar.parentElement)
     } else if (e.target.classList[0] === 'input__button'){
         if(inputText.value.length > 0) {
             createMessege()
