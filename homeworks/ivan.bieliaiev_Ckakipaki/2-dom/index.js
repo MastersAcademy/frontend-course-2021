b5c4a0157f7f183f0396c16e2d23514232e776b6
Chakipaki <belyeve2@icloud.com>
2021-10-27 23:08:12 +0300

    Update2

--- homeworks/ivan.bieliaiev_Ckakipaki/2-dom/index.js
+++ homeworks/ivan.bieliaiev_Ckakipaki/2-dom/index.js
@@ -4,12 +4,12 @@ const messeges = document.querySelector('.messeges');
 let amountOfMesseges = (document.querySelectorAll('.messege__item')).length;
 
 function createMessege() {
-    let div = document.createElement('div');
+    const div = document.createElement('div');
     div.classList = `messege__item mes__item${amountOfMesseges + 1}`;
-    let p = document.createElement('p');
+    const p = document.createElement('p');
     p.classList = 'messege__text';
     p.innerText = inputText.value;
-    let a = document.createElement('a');
+    const a = document.createElement('a');
     a.classList = `delete__button delete__button${amountOfMesseges + 1}`;
     a.innerHTML = '&#9746';
     div.appendChild(p);
@@ -19,19 +19,17 @@ function createMessege() {
     amountOfMesseges++;
 }
 // For click (adding and removing messeges)
-document.addEventListener('click', function(e) {
-        console.log(e.target.classList[0])
-        if (e.target.classList[0] === 'delete__button') {
-            let tar = document.querySelector(`.${e.target.classList[1]}`);
-            messeges.removeChild(tar.parentElement)
-        } else if (e.target.classList[0] === 'input__button') {
-            if (inputText.value.length > 0) {
-                createMessege()
-            }
+document.addEventListener('click', (e) => {
+    if (e.target.classList[0] === 'delete__button') {
+        let tar = document.querySelector(`.${e.target.classList[1]}`);
+        messeges.removeChild(tar.parentElement)
+    } else if (e.target.classList[0] === 'input__button') {
+        if (inputText.value.length > 0) {
+            createMessege()
         }
-
-    })
-    // For Enter key
+    }
+})
+// For Enter key
 document.addEventListener('keypress', function(e) {
     if (e.key === 'Enter') {
         if (inputText.value.length > 0) {
