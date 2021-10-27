c85760920c302625c5f3897ce7deb714c4c0fae8
Andrii Morozov <slon88888@ukr.net>
2021-10-28 00:27:48 +0300

    changed next version

--- homeworks/Andrii.Morozov_AndriiMorozov88/2 - DOM/script.js
+++ homeworks/Andrii.Morozov_AndriiMorozov88/2 - DOM/script.js
@@ -1,19 +1,19 @@
-let mainChat = document.querySelector(".chatBody");
-document.querySelector('#btn').onclick = sendMessage;
-document.onkeydown = function(e) {
-    if ( e.keyCode == 13) {
-        sendMessage ()};
-    };
-    function sendMessage () {
-    let message = document.querySelector(".messageInput");
-    let result = message.value;
-    let chatMessage = document.createElement('div');
+function sendMessage() {
+    const message = document.querySelector('.messageInput');
+    const result = message.value;
+    const chatMessage = document.createElement('div');
     mainChat.append(chatMessage);
-    chatMessage.className = "chatMessage";
+    chatMessage.className = 'chatMessage';
     chatMessage.innerText = result;
-    let resultLength = result.length;
     chatMessage.style.width = 40 + 20*result.length + 'px';
     document.querySelector('input[type=text]').value = ""}
+const mainChat = document.querySelector('.chatBody');
+document.querySelector('#btn').onclick = sendMessage;
+document.onkeydown = function( e )  {
+    if ( e.keyCode === 13)  {
+        sendMessage()       };
+    };
+    
 
     
     
