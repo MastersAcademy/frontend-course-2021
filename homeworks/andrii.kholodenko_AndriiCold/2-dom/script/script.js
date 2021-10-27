c30e1de6def5e051f8aa4f20c2cea93fd9ef0b58
Andrii <andrsiholodenko@gmail.com>
2021-10-27 18:37:57 +0300

    v1

--- homeworks/andrii.kholodenko_AndriiCold/2-dom/script/script.js
+++ homeworks/andrii.kholodenko_AndriiCold/2-dom/script/script.js
@@ -1,31 +1,29 @@
-const clickMouse = document.querySelector(".send");
-const message = document.querySelector(".message");
-const tmpl = document.querySelector("#tmpl");
-const p = tmpl.content.querySelector(".content");
-const clickKey = document.querySelector(".write_message");
-
+const clickMouse = document.querySelector('.send');
+const message = document.querySelector('.message');
+const tmpl = document.querySelector('#tmpl');
+const p = tmpl.content.querySelector('.content');
+const clickKey = document.querySelector('.write_message');
 function createElement() {
-    let text = document.getElementById('myTextArea').value
+    const text = document.getElementById('myTextArea').value;
     if (text.length > 0) {
         p.textContent = text;
-        let item = tmpl.content.cloneNode(true);
+        const item = tmpl.content.cloneNode(true);
         message.prepend(item);
-        let clear = document.getElementsByTagName('textarea');
-        for (let i = 0; i < clear.length; i++)
+        const clear = document.getElementsByTagName('textarea');
+        for (let i = 0; i < clear.length; i++) {
             clear[i].value = '';
+        }
         return false;
     }
+    return 0;
 }
-
-clickMouse.addEventListener("click", () => {
+clickMouse.addEventListener('click', () => {
     if (clickMouse.click) {
         createElement();
     }
 });
-clickKey.addEventListener("keyup", function (event) {
-    if (event.key === "Enter") {
-        event.preventDefault();
+clickKey.addEventListener('keyup', (event) => {
+    if (event.key === 'Enter') {
         createElement();
     }
 });
-
