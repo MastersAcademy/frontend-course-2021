24bde60d91da71e668661c4aad54b8948db6d46f
kovalov <vlad.kovalov.92@gmail.com>
2021-10-27 18:25:20 +0300

    solved linter issues

--- homeworks/vladyslav.kovalov_kovalov/2-dom/app.js
+++ homeworks/vladyslav.kovalov_kovalov/2-dom/app.js
@@ -1,25 +1,25 @@
 class App {
     constructor() {
-        this.dialogue = document.querySelector("[data-dialogue]");
-        this.template = document.querySelector("[data-chat-template]");
+        this.dialogue = document.querySelector('[data-dialogue]');
+        this.template = document.querySelector('[data-chat-template]');
     }
 
     create(message) {
-        const element = document.createElement("span");
-        element.classList.add("message");
-        element.dataset.dialogueMessage = "";
+        const element = document.createElement('span');
+        element.classList.add('message');
+        element.dataset.dialogueMessage = '';
 
-        const text = document.createElement("span");
-        text.classList.add("message__text");
+        const text = document.createElement('span');
+        text.classList.add('message__text');
         text.textContent = message;
 
-        const button = document.createElement("button");
-        button.classList.add("message__button");
-        button.dataset.messsageButton = "";
+        const button = document.createElement('button');
+        button.classList.add('message__button');
+        button.dataset.messsageButton = '';
 
-        const icon = document.createElement("i");
-        icon.classList.add("message__icon", "fas", "fa-times");
-        icon.dataset.messageButtonIcon = "";
+        const icon = document.createElement('i');
+        icon.classList.add('message__icon', 'fas', 'fa-times');
+        icon.dataset.messageButtonIcon = '';
 
         button.append(icon);
         element.append(text);
@@ -35,41 +35,37 @@ class App {
     }
 
     remove(element) {
-        if (confirm("Are you sure?")) {
-            this.dialogue.removeChild(element);
-        }
+        this.dialogue.removeChild(element);
     }
 
     clear() {
-        this.dialogue.innerHTML = "";
+        this.dialogue.innerHTML = '';
     }
 }
 
 const app = new App();
 
 document
-    .querySelector("[data-message-form]")
-    .addEventListener("submit", (e) => {
-        const message = document.querySelector("[data-message-input]");
+    .querySelector('[data-message-form]')
+    .addEventListener('submit', (e) => {
+        const message = document.querySelector('[data-message-input]');
 
         if (message.value) {
             const element = app.create(message.value);
             app.add(element);
-            message.value = "";
+            message.value = '';
         }
 
         e.preventDefault();
     });
 
-document.querySelector("[data-clear]").addEventListener("click", () => {
-    if (confirm("Are you sure?")) {
-        app.clear();
-    }
+document.querySelector('[data-clear]').addEventListener('click', () => {
+    app.clear();
 });
 
-document.querySelector("[data-dialogue]").addEventListener("click", (e) => {
-    if (e.target.dataset.messageButtonIcon === "") {
-        const currentMessage = e.target.closest("[data-dialogue-message]");
+document.querySelector('[data-dialogue]').addEventListener('click', (e) => {
+    if (e.target.dataset.messageButtonIcon === '') {
+        const currentMessage = e.target.closest('[data-dialogue-message]');
         app.remove(currentMessage);
     }
 });
