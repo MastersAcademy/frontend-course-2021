ec7790cef76ef6a7a38e9127e188c3c8bd0eeeff
Chakipaki <belyeve2@icloud.com>
2021-10-29 00:50:02 +0300

    All task done with additional and with BEM method

--- homeworks/ivan.bieliaiev_Ckakipaki/2-dom/index.js
+++ homeworks/ivan.bieliaiev_Ckakipaki/2-dom/index.js
@@ -1,37 +1,49 @@
 const inputText = document.querySelector('.input__text');
-const messeges = document.querySelector('.messeges');
+const messeges = document.querySelector('.chat__messeges');
+const inputBtn = document.querySelector('.input__button');
 
 let amountOfMesseges = (document.querySelectorAll('.messege__item')).length;
 
+inputText.addEventListener('keydown', (e) => {
+    const keyCode = e.which || e.keyCode;
+    if(keyCode === 13 && !e.shiftKey) {
+        e.preventDefault();
+    }
+})
 function createMessege() {
     if (inputText.value.length > 0) {
         const div = document.createElement('div');
         div.classList = `messege__item mes__item${amountOfMesseges + 1}`;
         const p = document.createElement('p');
-        p.classList = 'messege__text';
+        p.classList = 'item__text';
         p.innerText = inputText.value;
         const a = document.createElement('a');
-        a.classList = `delete__button delete__button${amountOfMesseges + 1}`;
+        a.classList = `item__delete item__del${amountOfMesseges + 1}`;
         a.innerHTML = '&#9746';
         div.appendChild(p);
         div.appendChild(a);
         messeges.appendChild(div);
         inputText.value = '';
         amountOfMesseges++;
+        messeges.scrollTop = messeges.scrollHeight;
+        
     }
 }
-// For click (adding and removing messeges)
+// For click on button (adding and removing messeges)
 document.addEventListener('click', (e) => {
-    if (e.target.classList[0] === 'delete__button') {
+    if (e.target.classList[0] === 'item__delete') {
         const tar = document.querySelector(`.${e.target.classList[1]}`);
         messeges.removeChild(tar.parentElement);
-    } else if (e.target.classList[0] === 'input__button') {
-        createMessege();
     }
 });
+
+inputBtn.addEventListener('click', (e) => {
+    createMessege();
+})
 // For Enter key
-document.addEventListener('keypress', (e) => {
-    if (e.key === 'Enter') {
+document.addEventListener('keydown', (e) => {
+    if(e.shiftKey === false && e.key ===  "Enter") {
         createMessege();
+        e.preventDefault();
     }
 });
