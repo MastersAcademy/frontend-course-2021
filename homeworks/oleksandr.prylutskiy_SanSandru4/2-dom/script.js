d85d0bfa81fd87b99629f5a9f4593c3752a60641
Oleksandr Prylutskiy <fleks1986@gmail.com>
2021-10-28 23:50:20 +0300

    messages are sent but not deleted

--- homeworks/oleksandr.prylutskiy_SanSandru4/2-dom/script.js
+++ homeworks/oleksandr.prylutskiy_SanSandru4/2-dom/script.js
@@ -6,7 +6,7 @@ let message = "";
 
 btnSubmit.addEventListener("click", () => {
     message = input.value;
-    chat.innerHTML += '<div class="msg"><span class="closebtn" onclick="this.parentElement.style.display="none";">&times;</span>' + message + '</div>';
+    chat.innerHTML += '<div class="msg"><span class="cls-btn">&times;</span>' + message + '</div>';
     input.value = '';
     let scr = chat.scrollHeight;
     chat.scrollTop = scr;
@@ -14,16 +14,14 @@ btnSubmit.addEventListener("click", () => {
 
 input.addEventListener('keyup', (event) => {
     if (event.keyCode === 13){
-        if (event.shiftKey) {
             message = input.value;
             chat.innerHTML += '<div class="msg"><span class="cls-btn">&times;</span>' + message + '</div>';
             input.value = '';
             let scr = chat.scrollHeight;
             chat.scrollTop = scr;
-        }
     }
 })
 
 cross.addEventListener('click', () => {
-    parentNode.style.display = "none";
+    this.parentElement.style.display = "none";
 })
