5e7041e7e8552040a0a3709f6dbcd5792f9dacb2
Andrii <andrsiholodenko@gmail.com>
2021-10-28 23:21:13 +0300

    v3

--- homeworks/andrii.kholodenko_AndriiCold/2-dom/script/script.js
+++ homeworks/andrii.kholodenko_AndriiCold/2-dom/script/script.js
@@ -1,29 +1,16 @@
-const clickMouse = document.querySelector('.send');
-const message = document.querySelector('.message');
-const tmpl = document.querySelector('#tmpl');
-const p = tmpl.content.querySelector('.content');
-const clickKey = document.querySelector('.write_message');
-function createElement() {
-    const text = document.getElementById('myTextArea').value;
-    if (text.length > 0) {
-        p.textContent = text;
-        const item = tmpl.content.cloneNode(true);
-        message.prepend(item);
-        const clear = document.getElementsByTagName('textarea');
-        for (let i = 0; i < clear.length; i++) {
-            clear[i].value = '';
+const formFieldEl = document.querySelector('[data-form-send]');
+const wrapperMessageFieldEl = document.querySelector('[data-message-wrapper]');
+const templateFieldEl = document.querySelector('[data-template]');
+const messageText = templateFieldEl.content.querySelector('[data-message]');
+formFieldEl.addEventListener('submit', (Event) => {
+    Event.preventDefault();
+    if (formFieldEl.submit) {
+        const writtenText = document.querySelector('[data-writen-text]').value;
+        if (writtenText.length > 0) {
+            messageText.textContent = writtenText;
+            const item = templateFieldEl.content.cloneNode(true);
+            wrapperMessageFieldEl.prepend(item);
+            formFieldEl.reset();
         }
-        return false;
-    }
-    return 0;
-}
-clickMouse.addEventListener('click', () => {
-    if (clickMouse.click) {
-        createElement();
-    }
-});
-clickKey.addEventListener('keyup', (event) => {
-    if (event.key === 'Enter') {
-        createElement();
     }
 });
