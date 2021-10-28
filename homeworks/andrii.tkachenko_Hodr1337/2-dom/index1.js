7fd030be67392ed4867a4dfea84a6d92fdaad3b9
Andrii Tkachenko <hodr1337@ukr.net>
2021-10-28 03:22:31 +0300

    DOM-2 Tkachenko

--- homeworks/andrii.tkachenko_Hodr1337/2-dom/index1.js
+++ homeworks/andrii.tkachenko_Hodr1337/2-dom/index1.js
@@ -1,17 +1,18 @@
-let input = document.getElementById("textArea");
-let button = document.querySelector("button");
+const input = document.getElementById('textArea');
+const button = document.querySelector('button');
 
-button.addEventListener("click", myClick);
 function myClick() {
-     let message = document.getElementById("textArea").value;
+     const message = document.getElementById('textArea').value;
      input.value = '';
      let createNew = document.createElement('div');
      createNew.textContent = message;
-     createNew.classList.add("created-message");
+     createNew.classList.add('created-message');
      document.body.querySelector('[data-chat]').append(createNew);
 }
+
+button.addEventListener('click', myClick);
 
 input.addEventListener('keypress', (e) => {
      if (!e.shiftKey && e.key === 'Enter') {
           myClick();
-     }});
+    }});
