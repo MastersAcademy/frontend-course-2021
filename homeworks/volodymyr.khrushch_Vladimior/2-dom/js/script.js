e42b2660aa6e5cc5ce9f73e6ac45d28ef6263d20
Volodymyr.Khruchsh <vladimir.khrush@gmail.com>
2021-10-28 13:13:44 +0300

    homework 2-dom

---
+++ homeworks/volodymyr.khrushch_Vladimior/2-dom/js/script.js
@@ -0,0 +1,41 @@
+// get the button and input field and close button
+const button = document.querySelector('[data-btn]');
+const input = document.querySelector('[data-input]');
+const close = document.getElementsByClassName('close');
+
+// create the element received from the input field and the delete element
+let elem = function newElement() {
+    const li = document.createElement('li');
+    li.classList.add('message');
+    const inputValue = document.querySelector('[data-input]').value.replace(/\n/g, '<br />');
+    li.innerHTML = inputValue;
+    if (inputValue === '') {
+        alert('You must write something!');
+    } else {
+        document.querySelector('[data-app]').append(li);
+    }
+    document.querySelector('[data-input]').value = '';
+
+    const span = document.createElement('div');
+    const txt = document.createTextNode('\u2A2F');
+    span.className = 'close';
+    span.append(txt);
+    li.append(span);
+
+    for (i = 0; i < close.length; i++) {
+        close[i].onclick = function () {
+            let delet = this.parentElement;
+            delet.style.display = 'none';
+        }
+    }
+};
+//send by pressing the enter, and the transition is the shift enter
+input.addEventListener('keydown', function (event) {
+    if (event.code == 'Enter' && event.shiftKey) {} else if (event.code == 'Enter') {
+        event.preventDefault();
+        button.click();
+    }
+});
+
+// listen to the button and call the function
+button.addEventListener("click", elem);
