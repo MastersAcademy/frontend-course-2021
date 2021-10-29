2741a6433116a50acc1196bd5aafa92562f763e5
Andrii Tkachenko <hodr1337@ukr.net>
2021-10-29 17:52:08 +0300

    Fixed acording to comments

--- homeworks/andrii.tkachenko_Hodr1337/2-dom/index1.js
+++ homeworks/andrii.tkachenko_Hodr1337/2-dom/index1.js
@@ -1,19 +1,24 @@
 const input = document.getElementById('textArea');
 const button = document.querySelector('button');
 
-function myClick() {
-    const message = document.getElementById('textArea').value;
-    input.value = '';
-    const createNew = document.createElement('div');
-    createNew.textContent = message;
-    createNew.classList.add('created-message');
-    document.body.querySelector('[data-chat]').append(createNew);
+
+function sendMessage() {
+    if (input.value.length > 0) {
+        const message = input.value;
+        input.value = '';
+        const messageChatHistory = document.createElement('div');
+        messageChatHistory.textContent = message;
+        messageChatHistory.classList.add('created-message');
+        document.body.querySelector('[data-chat]').append(messageChatHistory)
+    } else {
+        alert("Can not send empty message")
+    }
 }
 
-button.addEventListener('click', myClick);
+button.addEventListener('click', sendMessage);
 
 input.addEventListener('keypress', (e) => {
     if (!e.shiftKey && e.key === 'Enter') {
-        myClick();
+        sendMessage();
     }
 });
