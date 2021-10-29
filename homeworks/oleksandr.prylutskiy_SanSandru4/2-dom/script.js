0f2c5b848807a606d871efc4b05aacc0d8bb1972
Oleksandr Prylutskiy <fleks1986@gmail.com>
2021-10-29 21:19:16 +0300

    New branch 2-dom

--- homeworks/oleksandr.prylutskiy_SanSandru4/2-dom/script.js
+++ homeworks/oleksandr.prylutskiy_SanSandru4/2-dom/script.js
@@ -1,9 +1,9 @@
 const chat = document.querySelector(".divMessages");
 const input = document.querySelector(".inputMessage");
-const btnSubmit = document.querySelector(".btnSend");
+const btnSub = document.querySelector(".btnSend");
 let message = "";
 
-btnSubmit.addEventListener("click", () => {
+btnSub.addEventListener("click", () => {
     message = input.value;
     chat.innerHTML += `${<div class="msg">message</div>}`;
     input.value = '';
@@ -14,7 +14,7 @@ btnSubmit.addEventListener("click", () => {
 input.addEventListener('keyup', (event) => {
     if (event.keyCode === 13){
         message = input.value;
-        chat.innerHTML += `${<div class="msg">message</div>}`;
+        chat.innerHTML += `${<div class=".messageBox">message</div>}`;
         input.value = '';
         const scr = chat.scrollHeight;
         chat.scrollTop = scr;
