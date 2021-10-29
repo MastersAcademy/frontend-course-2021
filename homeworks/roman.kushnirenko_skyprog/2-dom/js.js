8d66ee521c4da7f86efef079d10255e3945fe36b
Roma <romgofast@gmail.com>
2021-10-29 15:30:04 +0300

    fix2

--- homeworks/roman.kushnirenko_skyprog/2-dom/js.js
+++ homeworks/roman.kushnirenko_skyprog/2-dom/js.js
@@ -2,6 +2,27 @@ const chatMessages = document.querySelector('[data-all_messages]');
 const chatFormInput = document.querySelector('[data-text_input]');
 const chatFormSubmit = document.querySelector('[data-text_submit]');
 
+function creatMassege() {
+    const chatMessageContent = document.createElement('div');
+    chatMessageContent.className = 'chat_message_content';
+    chatMessages.append(chatMessageContent);
+
+    const message = chatFormInput.value.replace(/\n/g, '<br/>');
+    chatFormInput.value = '';
+    chatMessageContent.innerHTML = message;
+
+    const closeMessage = document.createElement('button');
+    closeMessage.textContent = 'x';
+    closeMessage.className = 'close_message';
+    chatMessageContent.append(closeMessage);
+
+    chatMessages.scrollTop = 9999;
+
+    closeMessage.addEventListener('click', () => {
+        chatMessages.removeChild(chatMessageContent);
+    });
+}
+
 chatFormSubmit.addEventListener('click', (event) => {
     event.preventDefault();
     if (!chatFormInput.value == '') {
@@ -9,30 +30,9 @@ chatFormSubmit.addEventListener('click', (event) => {
     }
 });
 
-function creatMassege() {
-    let chat_message_content = document.createElement('div');
-    chat_message_content.className = 'chat_message_content';
-    chatMessages.append(chat_message_content);
-
-    let message = chatFormInput.value.replace(/\n/g, '<br/>');
-    chatFormInput.value = '';
-    chat_message_content.innerHTML = message;
-
-    let close_message = document.createElement('button');
-    close_message.textContent = 'x';
-    close_message.className = 'close_message';
-    chat_message_content.append(close_message);
-
-    chatMessages.scrollTop = 9999;
-
-    close_message.addEventListener('click', () => {
-        chatMessages.removeChild(chat_message_content);
-    });
-};
-
 chatFormInput.addEventListener('keypress', (e) => {
     if (!e.shiftKey && e.key === 'Enter') {
         e.preventDefault();
         creatMassege();
     }
-})
+});
