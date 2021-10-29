357fd2a45da437c367d4f619518bd70e125029e2
deadhard90 <deadhard90@gmail.com>
2021-10-29 14:23:38 +0300

    fixed bugs in js file v4.5 homework2

--- homeworks/leonid.lushchiy_deadhar90/2-dom/js/messager.js
+++ homeworks/leonid.lushchiy_deadhar90/2-dom/js/messager.js
@@ -64,7 +64,7 @@ send.addEventListener('click', () => {
 inputArea.addEventListener('keyup', (event) => {
     if (event.code === 'Enter' && !event.shiftKey) {
         send.click();
-    } else {
-        return false;
+    if (event.code === event.shiftKey) {
+        event.preventDefault();
     }
 });
