d8200aa4d09bea45df485dc64f0378735b4904c3
yuriy <petrenko.yuriy.1985@gmail.com>
2021-10-29 13:57:05 +0300

    2-dom update

--- homeworks/yurii.petrenko_YuriiPetrenko1907/2-dom/main.js
+++ homeworks/yurii.petrenko_YuriiPetrenko1907/2-dom/main.js
@@ -3,59 +3,61 @@ const deskMessageInput = document.getElementById('description-message');
 const MessagesWrapper = document.querySelector('.messages-wrapper');
 
 let messages;
+// eslint-disable-next-line no-unused-expressions
 !localStorage.messages ? messages = [] : messages = JSON.parse(localStorage.getItem('messages'));
 
+// eslint-disable-next-line no-unused-vars
 let MessageItemElems = [];
 
 function Message(description) {
     this.description = description;
 }
 
-const createTemplate = (message, index) => {
-    return `
+const createTemplate = (message, index) => `
         <div class="message-item">
             <div class="description">${message.description}</div>
                 <div class="buttons">
                     <button onclick="deleteMessage(${index})" class="btn-delete">X</button>
                 </div>
         </div>
-    `
-}
+    `;
 
 const fillHtmlList = () => {
-    MessagesWrapper.innerHTML = "";
-    if(messages.length > 0) {
+    MessagesWrapper.innerHTML = '';
+    if (messages.length > 0) {
         messages.forEach((item, index) => {
             MessagesWrapper.innerHTML += createTemplate(item, index);
         });
         MessageItemElems = document.querySelectorAll('.message-item');
     }
-}
+};
 
 fillHtmlList();
 
 const updateLocal = () => {
     localStorage.setItem('messages', JSON.stringify(messages));
-}
+};
 
 const update = () => {
     updateLocal();
     fillHtmlList();
     deskMessageInput.value = '';
-}
+};
 
 addMessageBtn.addEventListener('click', () => {
     messages.push(new Message(deskMessageInput.value));
     update();
-})
+});
 
 document.addEventListener('keypress', (event) => {
     if (event.key === 'Enter') {
-    messages.push(new Message(deskMessageInput.value));
-    update ();}
-})
+        messages.push(new Message(deskMessageInput.value));
+        update();
+    }
+});
 
-const deleteMessage = index => {
+// eslint-disable-next-line no-unused-vars
+const deleteMessage = (index) => {
     messages.splice(index, 1);
     update();
-}
+};
