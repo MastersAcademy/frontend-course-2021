1fbdd8d0a8fb9df53991236e5a88ad2547239dfc
Andrii <andrsiholodenko@gmail.com>
2021-10-29 18:47:56 +0300

    v4

--- homeworks/andrii.kholodenko_AndriiCold/2-dom/script/script.js
+++ homeworks/andrii.kholodenko_AndriiCold/2-dom/script/script.js
@@ -1,16 +1,15 @@
-const formFieldEl = document.querySelector('[data-form-send]');
-const wrapperMessageFieldEl = document.querySelector('[data-message-wrapper]');
-const templateFieldEl = document.querySelector('[data-template]');
-const messageText = templateFieldEl.content.querySelector('[data-message]');
-formFieldEl.addEventListener('submit', (Event) => {
+const formSendEl = document.querySelector('[data-form-send]');
+const messageWrapperEl = document.querySelector('[data-message-wrapper]');
+const templateEl = document.querySelector('[data-template]');
+formSendEl.addEventListener('submit', (Event) => {
     Event.preventDefault();
-    if (formFieldEl.submit) {
+    if (formSendEl.submit) {
         const writtenText = document.querySelector('[data-writen-text]').value;
         if (writtenText.length > 0) {
-            messageText.textContent = writtenText;
-            const item = templateFieldEl.content.cloneNode(true);
-            wrapperMessageFieldEl.prepend(item);
-            formFieldEl.reset();
+            const cloneNode = templateEl.content.cloneNode(true);
+            messageWrapperEl.prepend(cloneNode);
+            document.querySelector('[data-message]').textContent = writtenText;
+            formSendEl.reset();
         }
     }
 });
