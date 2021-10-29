46d7d07d5bd70aac95176b8694f762716c623c93
igordordev <88446424+igordordev@users.noreply.github.com>
2021-10-29 13:35:36 +0300

    fixed let to const

--- homeworks/igor.doroshenko_igordordev/2-dom/script.js
+++ homeworks/igor.doroshenko_igordordev/2-dom/script.js
@@ -1,11 +1,11 @@
-let messageForm = document.querySelector('.input-form');
-let messageList = document.querySelector('.messages-list');
-let messageField = document.querySelector('.input-form__field');
+const messageForm = document.querySelector('.input-form');
+const messageList = document.querySelector('.messages-list');
+const messageField = document.querySelector('.input-form__field');
 
 messageForm.onsubmit = function (evt) {
     evt.preventDefault();
 
-    let newMessage = document.createElement('li');
+    const newMessage = document.createElement('li');
     newMessage.classList.add('messages-list__item');
     newMessage.textContent = messageField.value;
     messageField.value = '';
