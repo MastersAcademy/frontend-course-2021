eb6bf44228b8713990de75552439ba3547bf4ae2
Andrii <andrsiholodenko@gmail.com>
2021-10-29 20:31:45 +0300

    renamed the variable and changed querySelector

--- homeworks/andrii.kholodenko_AndriiCold/2-dom/script/script.js
+++ homeworks/andrii.kholodenko_AndriiCold/2-dom/script/script.js
@@ -1,15 +1,13 @@
 const formSendEl = document.querySelector('[data-form-send]');
 const messageWrapperEl = document.querySelector('[data-message-wrapper]');
 const templateEl = document.querySelector('[data-template]');
-formSendEl.addEventListener('submit', (Event) => {
-    Event.preventDefault();
-    if (formSendEl.submit) {
-        const writtenText = document.querySelector('[data-writen-text]').value;
-        if (writtenText.length > 0) {
-            const cloneNode = templateEl.content.cloneNode(true);
-            messageWrapperEl.prepend(cloneNode);
-            document.querySelector('[data-message]').textContent = writtenText;
-            formSendEl.reset();
-        }
+formSendEl.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const writtenText = document.querySelector('[data-writen-text]').value;
+    if (writtenText.length > 0) {
+        const messageEL = templateEl.content.cloneNode(true);
+        messageWrapperEl.prepend(messageEL);
+        messageWrapperEl.querySelector('[data-message]').textContent = writtenText;
+        formSendEl.reset();
     }
 });
