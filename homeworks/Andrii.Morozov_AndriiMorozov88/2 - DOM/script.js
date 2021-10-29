e4d68d362ab9d086eac2fdaa5908ca4837fc22b9
Andrii Morozov <slon88888@ukr.net>
2021-10-29 23:24:41 +0300

    home task

--- homeworks/Andrii.Morozov_AndriiMorozov88/2 - DOM/script.js
+++ homeworks/Andrii.Morozov_AndriiMorozov88/2 - DOM/script.js
@@ -1,10 +1,10 @@
-const mainChat = document.querySelector('.chatBody');
+const mainChat = document.querySelector('.chat__body');
 function sendMessage() {
-    const message = document.querySelector('.messageInput');
+    const message = document.querySelector('.message__input');
     const result = message.value;
     const chatMessage = document.createElement('div');
     mainChat.append(chatMessage);
-    chatMessage.className = 'chatMessage';
+    chatMessage.className = 'chat__message';
     chatMessage.innerText = result;
     document.querySelector('input[type=text]').value = '';
 }
