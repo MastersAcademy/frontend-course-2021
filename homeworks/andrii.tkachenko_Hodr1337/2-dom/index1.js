194efea38a171a4f5e7fc1e7454308baf63a885d
Andrii Tkachenko <hodr1337@ukr.net>
2021-10-30 03:50:59 +0300

    Added the validation for string with spaces only

--- homeworks/andrii.tkachenko_Hodr1337/2-dom/index1.js
+++ homeworks/andrii.tkachenko_Hodr1337/2-dom/index1.js
@@ -1,8 +1,16 @@
 const input = document.getElementById('textArea');
 const button = document.querySelector('button');
 
+function onlySpacesCheck() {
+    if (input.value.trim().length > 0) {
+        return true
+    } else {
+        return false
+    };
+}
+
 function sendMessage() {
-    if (input.value.length > 0) {
+    if (input.value.length > 0 && onlySpacesCheck() === true) {
         const message = input.value;
         input.value = '';
         const messageChatHistory = document.createElement('div');
@@ -10,8 +18,8 @@ function sendMessage() {
         messageChatHistory.classList.add('created-message');
         document.body.querySelector('[data-chat]').append(messageChatHistory);
     } else {
-        alert('Can not send empty message');
-    }
+        alert('Can not send empty message')
+    };
 }
 
 button.addEventListener('click', sendMessage);
