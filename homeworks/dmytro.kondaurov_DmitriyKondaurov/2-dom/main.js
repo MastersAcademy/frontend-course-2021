374e55a4a10dc5792ca34cea58abc7593d6379f9
Dmytro Kondaurov <invisibl@ukr.net>
2021-10-30 10:13:19 +0300

    fix mentor comments

--- homeworks/dmytro.kondaurov_DmitriyKondaurov/2-dom/main.js
+++ homeworks/dmytro.kondaurov_DmitriyKondaurov/2-dom/main.js
@@ -1,15 +1,20 @@
+const addNewMessage = (text) => {
+    const newEl = document.createElement('li');
+    document.body.querySelector('[data-main-box]').append(newEl);
+    newEl.classList.add('main-box_msg');
+    newEl.textContent = text;
+};
+
 const sendMessage = (event) => {
-    if (document.querySelector('[data-send-txt]').value) {
+    const newMessageText = document.querySelector('[data-send-txt]').value;
+    if (newMessageText) {
         const textBox = document.querySelector('[data-main-box]');
-        const newMessageText = document.querySelector('[data-send-txt]').value;
         document.querySelector('[data-send-txt]').value = '';
-        const newEl = document.createElement('li');
-        newEl.textContent = newMessageText;
-        newEl.classList.add('main-box_msg');
-        document.body.querySelector('[data-main-box]').append(newEl);
+        addNewMessage(newMessageText);
         textBox.scrollTop = textBox.scrollHeight;
     }
     event.preventDefault();
 };
+
 const sendBtn = document.querySelector('[data-send-btn]');
-sendBtn.addEventListener('click', sendMessage, false);
+sendBtn.addEventListener('click', sendMessage);
