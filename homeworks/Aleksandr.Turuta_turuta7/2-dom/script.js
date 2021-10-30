084cb50cfe1e5131e2361558ca438d88d38c1396
Aleksandr <turuta51@gmail.com>
2021-10-30 19:30:25 +0300

    fix function listenerButtonDeleted

--- homeworks/Aleksandr.Turuta_turuta7/2-dom/script.js
+++ homeworks/Aleksandr.Turuta_turuta7/2-dom/script.js
@@ -2,38 +2,26 @@ const buttonSendMessageEl = document.querySelector('[data-message-send]');
 const textValueMessageEl = document.querySelector('[data-message-field]');
 const scrollContentEl = document.querySelector('[data-main-container]');
 const messageListEl = document.querySelector('[data-container-messages]');
+const messageTemplateEl = document.querySelector('[data-message-template]').content;
 
-let index = 1;
-
-const listenerButtonDeleted = (buttonDeleteMessage) => {
-    buttonDeleteMessage.addEventListener('click', () => {
-        document.querySelector(`[data-number-message="${buttonDeleteMessage.dataset.buttonDelete}"]`).remove();
-    });
+const listenerButtonDeleted = (messageEl) => {
+    messageEl.querySelector('[data-button-delete-message]')
+        .addEventListener('click', () => messageEl.remove());
 };
 
 const createMessage = () => {
-    const messageTemplateEl = document.querySelector('[data-message-template]').content;
-
-    const messageEl = messageTemplateEl.cloneNode(true);
+    const messageEl = messageTemplateEl.cloneNode(true).firstElementChild;
 
     messageEl.querySelector('[data-message-text]')
         .textContent = textValueMessageEl.value;
 
-    messageEl.querySelector('[data-new-message]')
-        .setAttribute('data-number-message', index);
-
-    const buttonDeleteMessage = messageEl.querySelector('[data-button-delete-message]');
-    buttonDeleteMessage.setAttribute('data-button-delete', index++);
-
+    listenerButtonDeleted(messageEl);
     messageListEl.append(messageEl);
 
     textValueMessageEl.value = '';
 
     scrollContentEl.scrollTop = scrollContentEl.scrollHeight;
-
-    listenerButtonDeleted(buttonDeleteMessage);
 };
-
 buttonSendMessageEl.addEventListener('click', () => {
     if (textValueMessageEl.value) createMessage();
 });
