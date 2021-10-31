00e45bbe01379ccf0f2d052fd57f5df3301408fc
Вася Танащук <vtanaschuk@gmail.com>
2021-10-31 17:10:34 +0200

    update dom homework

--- homeworks/vasil.tanashchuk_vtanaschuk/2-dom/lib/script.js
+++ homeworks/vasil.tanashchuk_vtanaschuk/2-dom/lib/script.js
@@ -1,33 +1,32 @@
 function addClose() {
-    const myNodeList = document.getElementsByTagName('li');
+    const myNodeList = document.getElementsByTagName('article');
 
     for (let i = 0; i < myNodeList.length; i++) {
-        if (myNodeList[i].getElementsByTagName('span').length === 0) {
-            const span = document.createElement('span');
+        if (myNodeList[i].getElementsByTagName('button').length === 0) {
+            const btnEl = document.createElement('button');
             const text = document.createTextNode('\u00D7');
-            // span.setAttribute('onclick', 'removeElement(this)');
-            span.className = 'close';
-            span.appendChild(text);
-            myNodeList[i].appendChild(span);
+            btnEl.className = 'close';
+            btnEl.appendChild(text);
+            myNodeList[i].appendChild(btnEl);
         }
     }
 }
-const inputBnt = document.querySelector('[data-message-input-btn]');
 
-inputBnt.addEventListener('click', () => {
-    const input = document.querySelector('[data-message-input]');
-    const newMessage = input.value;
-    const list = document.querySelector('[data-message-otput]');
+const inputForm = document.querySelector('[data-message-form]');
+const inputEl = document.querySelector('[data-message-input]');
+
+inputForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const postListEl = document.querySelector('[data-message-otput]');
+    const postTemplateEl = document.querySelector('[data-post-template]');
+    const newMessage = inputEl.value;
+
     if (newMessage) {
-        const newLi = document.createElement('li');
-        const text = document.createTextNode(newMessage);
-        newLi.className = 'message';
-        newLi.appendChild(text);
-        list.appendChild(newLi);
+        const postEl = postTemplateEl.content.cloneNode(true);
+        postEl.querySelector('[data-post-title]').textContent = newMessage;
+        postListEl.appendChild(postEl);
         addClose();
+        inputEl.value = '';
     }
 });
-
-// function removeElement(el) {
-//     el.parentElement.remove();
-// }
