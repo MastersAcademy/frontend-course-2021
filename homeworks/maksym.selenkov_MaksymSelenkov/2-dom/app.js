9cbd9e23f05d5ab9528472d0f3b6c2c91e8862b5
MaksymSelenkov <snegopad100@gmail.com>
2021-10-31 09:25:01 +0200

    fixedreview

--- homeworks/maksym.selenkov_MaksymSelenkov/2-dom/app.js
+++ homeworks/maksym.selenkov_MaksymSelenkov/2-dom/app.js
@@ -2,34 +2,20 @@ const mainArea = document.querySelector('[data-main]');
 const inputData = document.querySelector('[data-input]');
 const buttonSubmit = document.querySelector('[data-submit]');
 
-function createMessage() {
+function insertMessage() {
     if (inputData.value.length > 0) {
         const message = document.createElement('div');
         message.classList.add('message-style');
         message.textContent = inputData.value;
         mainArea.append(message);
         inputData.value = '';
-
-        message.insertAdjacentHTML('beforeend', '<img src=\'icon.jpg\' style=\'width: 20px\'>');
-        const icons = document.querySelectorAll('img');
-        icons.forEach((icon) => {
-            icon.addEventListener('click', (event) => {
-                event.target.parentNode.remove(event.target);
-            });
-        });
     }
 }
 
-buttonSubmit.addEventListener('click', createMessage);
+buttonSubmit.addEventListener('click', insertMessage);
 
 inputData.addEventListener('keypress', (event) => {
-    if (event.key === 'Enter' && event.shiftKey !== true) {
-        createMessage();
-    }
-});
-
-inputData.addEventListener('keypress', (event) => {
-    if (event.key === 'Enter' && event.shiftKey) {
-        console.log('some magic');
+    if (event.key === 'Enter') {
+        insertMessage();
     }
 });
