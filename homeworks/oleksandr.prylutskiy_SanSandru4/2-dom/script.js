c0555c8ad83d9f5e52cb3069ef1702fd11dccb3e
Oleksandr Prylutskiy <fleks1986@gmail.com>
2021-11-01 19:21:58 +0200

    fix code and style

--- homeworks/oleksandr.prylutskiy_SanSandru4/2-dom/script.js
+++ homeworks/oleksandr.prylutskiy_SanSandru4/2-dom/script.js
@@ -3,25 +3,25 @@ const input = document.querySelector('.input-message');
 const btnSub = document.querySelector('.btn-send');
 let message = '';
 
-btnSub.addEventListener('click', () => {
-    chatMessage();
-    chatScroll(chat);
-});
-
-input.addEventListener('keyup', (event) => {
-    if (event.keyCode === 13) {
-        chatMessage();
-        chatScroll(chat);
-    }
-});
-
 function chatMessage() {
     message = input.value;
     chat.innerHTML += `<div class="message-box">${message}</div>`;
     input.value = '';
 }
 
-function chatScroll (inValue) {
-    const scr = inValue.scrollHeight;
-    inValue.scrollTop = scr;
+function chatScroll() {
+    const scr = chat.scrollHeight;
+    chat.scrollTop = scr;
 }
+
+btnSub.addEventListener('click', () => {
+    chatMessage();
+    chatScroll();
+});
+
+input.addEventListener('keyup', (event) => {
+    if (event.keyCode === 13) {
+        chatMessage();
+        chatScroll();
+    }
+});
