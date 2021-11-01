bb5dcb6a9d45d64c049edfc6ebd73ea2b63031e8
ErnestMk1 <ernmyr434@gmail.com>
2021-11-01 17:26:49 +0200

    changed logic in script

--- homeworks/Ernest.Myrmyr_ErnestMk1/2-dom/script.js
+++ homeworks/Ernest.Myrmyr_ErnestMk1/2-dom/script.js
@@ -2,16 +2,6 @@ const msgFromUserEl = document.querySelector('[data-text]');
 const listOfMessages = document.querySelector('[data-list]');
 const formEl = document.querySelector('[data-form]');
 
-msgFromUserEl.addEventListener('keypress', (e) => {
-    if (e.key === 'Enter') {
-        e.preventDefault();
-        const element = document.createElement('li');
-        element.textContent = msgFromUserEl.value;
-        listOfMessages.appendChild(element);
-        msgFromUserEl.value = '';
-    }
-});
-
 formEl.addEventListener('submit', (el) => {
     el.preventDefault();
     const element = document.createElement('li');
