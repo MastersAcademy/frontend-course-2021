df9c4fd30df84a1fd5ba23212817cd41171261d8
Chakipaki <belyeve2@icloud.com>
2021-11-02 23:15:03 +0200

    On fast hand add index.html with styles and app.js

---
+++ homeworks/ivan.bieliaiev_Ckakipaki/3-async/app.js
@@ -0,0 +1,20 @@
+/*
+Before sending a request for posts, add a timeout of 3 seconds
+
+When opening the page, show the loading and hide it after downloading the posts
+
+Render a list of posts according to the attached layout
+
+Add sorting and title filter to posts that have been rendered.
+
+Sorting has 3 options:
+a. Initial version (without sorting)
+b. Sort from A to Z
+c. Sort from Z to A
+
+In the filter field when entering text, only those posts are displayed whose titles match the text (non-case-sensitive match)
+For example: the text "ed T" will leave posts with the titles "red text", "rED T"
+
+Sorting and filtering should work in combination, ie when displaying search results, the selected sorting should be applied to them.
+
+*/
