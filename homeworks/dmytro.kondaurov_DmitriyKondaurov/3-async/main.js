a06300498b31c0e77d57fc42bfaa1dd1fcbdcc75
Dmytro Kondaurov <invisibl@ukr.net>
2021-11-04 01:40:52 +0200

    add preloader

--- homeworks/dmytro.kondaurov_DmitriyKondaurov/3-async/main.js
+++ homeworks/dmytro.kondaurov_DmitriyKondaurov/3-async/main.js
@@ -1,33 +1,16 @@
-const removeMessage = (event) => {
-    const parent = event.target.parentElement;
-    if (event.target.className === 'close') {
-        parent.remove();
-    }
+window.onload = () => {
+    document.querySelector('.loader_inner').classList.add('hide');
+    document.querySelector('.loader').classList.add('hide');
 };
 
-const addNewMessage = (text) => {
-    const newEl = document.createElement('li');
-    const newElCloseBtn = document.createElement('span');
-    newEl.classList.add('main-box_msg');
-    newElCloseBtn.classList.add('close');
-    newEl.textContent = text;
-    newElCloseBtn.textContent = '×';
-    newEl.appendChild(newElCloseBtn);
-    document.body.querySelector('[data-main-box]').append(newEl);
-};
-
-const sendMessage = (event) => {
-    const newMessageText = document.querySelector('[data-send-txt]').value;
-    if (newMessageText) {
-        const textBox = document.querySelector('[data-main-box]');
-        document.querySelector('[data-send-txt]').value = '';
-        addNewMessage(newMessageText);
-        textBox.scrollTop = textBox.scrollHeight;
-    }
-    event.preventDefault();
-};
-
-const sendBtn = document.querySelector('[data-send-btn]');
-const messageBox = document.querySelector('[data-main-box]');
-messageBox.addEventListener('click', removeMessage);
-sendBtn.addEventListener('click', sendMessage);
+setTimeout(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => response.json())
+        .then((json) => {
+            console.log(json);
+            // json.map((post) => {
+            // post.title
+            // post.body
+            // })
+        });
+}, 3000);
