8192095cbf0609d80c3f054173ce52f1d5a32c69
Chakipaki <belyeve2@icloud.com>
2021-11-04 14:21:12 +0200

    Added remove active button

--- homeworks/ivan.bieliaiev_Ckakipaki/3-async/app.js
+++ homeworks/ivan.bieliaiev_Ckakipaki/3-async/app.js
@@ -1,47 +1,55 @@
 const url = 'https://jsonplaceholder.typicode.com/posts';
 const blog = document.querySelector('[data-blog]');
 
-async function timeOut(ms){
-    setTimeout(() => console.log('timeout', ms / 1000, 'seconds'), ms);
-}
-
+// Rendering posts from JSON
 async function renderPosts(url) {
     try {
-        console.log('Rendering');
         const response = await fetch(url);
         let content = await response.json();
         content = await content.slice(0, 10);
-        console.log(content);
-        createPost(content, )
+        createPosts(content);
     }
     catch (e) {
         console.log(e);
     }
 }
 
-function createPost(content){
-    for(let  i = 0; i < content.length; i++){
-        const article = document.createElement('article');
-        const p = document.createElement('p');
-        const h1 = document.createElement('h1');
-        const div = document.createElement('div');
-        const button = document.createElement('button');
-        article.setAttribute(`data-post`, content[i].id)
-        button.attributes = `data-delete-post = ${content[i].id}`;
-        article.classList = 'blog__post';
-        p.classList = 'post__text';
-        h1.classList = 'post__title';
-        div.classList = 'post__delete';
-        button.classList = 'delete__button';
-        button.textContent = "delete";
-        h1.textContent = content[i].title;
-        p.textContent = content[i].body;
-        div.appendChild(button);
-        article.appendChild(h1);
-        article.appendChild(p);
-        article.appendChild(div);
-        blog.appendChild(article);
+// Create post markup with value text, title, id
+function postMarkupPost(id, title, text){
+    const article = document.createElement('article');
+    const p = document.createElement('p');
+    const h1 = document.createElement('h1');
+    const div = document.createElement('div');
+    const button = document.createElement('button');
+    article.setAttribute(`data-post`, id);
+    article.classList = 'blog__post';
+    button.setAttribute(`data-delete-post`, id);
+    button.classList = 'delete__button';
+    p.classList = 'post__text';
+    h1.classList = 'post__title';
+    div.classList = 'post__delete';
+    button.textContent = "delete";
+    h1.textContent = title;
+    p.textContent = text;
+    div.appendChild(button);
+    article.appendChild(h1);
+    article.appendChild(p);
+    article.appendChild(div);
+    blog.appendChild(article);
+}
+
+// Appending posts to blog container
+function createPosts(content){
+    for(let i = 0; i < content.length; i++){
+        postMarkupPost(i, content[i].title, content[i].body);
     }
 }
 
 renderPosts(url)
+
+
+document.addEventListener('click', e => {
+    if(e.classList = "[data-delete-post]") {
+        blog.removeChild(e.target.parentElement.parentElement)
+    }
+})
