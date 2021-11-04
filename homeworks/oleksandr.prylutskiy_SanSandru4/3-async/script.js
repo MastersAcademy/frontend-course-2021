9e357ffc676c2791f30b51d301686e4ee73be674
Oleksandr Prylutskiy <fleks1986@gmail.com>
2021-11-05 00:58:17 +0200

    added filter

--- homeworks/oleksandr.prylutskiy_SanSandru4/3-async/script.js
+++ homeworks/oleksandr.prylutskiy_SanSandru4/3-async/script.js
@@ -1,6 +1,8 @@
-const url = 'https://jsonplaceholder.typicode.com/posts';
-const blogMsg = document.querySelector('[data-blog-msg]');
-const sortPost= document.querySelector('[data-blog-sort]');
+const url = "https://jsonplaceholder.typicode.com/posts";
+const blogMsg = document.querySelector("[data-blog-msg]");
+const sortPost= document.querySelector("[data-blog-sort]");
+const filterPostTitle = document.querySelector("[data-blog-filter]");
+const postStr = "";
 
 function forward(field) {
     return (a, b) => a[field] > b[field] ? 1 : -1;
@@ -11,16 +13,16 @@ function back(field) {
 };
 
 function render(bTitle, bBody) {
-    let msg = document.createElement('ol');
-    msg.classList.add('blog-msg');
+    let msg = document.createElement("ol");
+    msg.classList.add("blog-msg");
     blogMsg.appendChild(msg);
-    let postTitle = document.createElement('H1');
+    let postTitle = document.createElement("H1");
     postTitle.innerHTML = bTitle;
-    postTitle.classList.add('post-title');
+    postTitle.classList.add("post-title");
     msg.appendChild(postTitle);
-    let postBody = document.createElement('p');
+    let postBody = document.createElement("p");
     postBody.innerHTML = bBody;
-    postBody.classList.add('post-body');
+    postBody.classList.add("post-body");
     msg.appendChild(postBody);
 };
 
@@ -33,35 +35,43 @@ function deleteEl(element) {
 fetch(url)
 .then((response) => response.json())
 .then((json) => {
-    let blogArr = json;
+    const blogArr = json;
     for (let i = 0; i < blogArr.length; i++) {
         render(`${blogArr[i].title}`, `${blogArr[i].body}`);
     };
 
-    sortPost.addEventListener('change', function() {
+    sortPost.addEventListener("change", function() {
         if(`${sortPost.value}` == "No sort") {
-            console.log(`${sortPost.value}`);
+            const blogSort = blogArr;
             deleteEl(blogMsg);
-            for (let i = 0; i < blogArr.length; i++) {
-                render(`${blogArr[i].title}`, `${blogArr[i].body}`);
+            for (let i = 0; i < blogSort.length; i++) {
+                render(`${blogSort[i].title}`, `${blogSort[i].body}`);
             };
         }
         else if(`${sortPost.value}` == "Sort A-Z") {
             deleteEl(blogMsg);
-            blogArr.sort(forward('title'));
+            blogArr.sort(forward("title"));
             for (let i = 0; i < blogArr.length; i++) {
                 render(`${blogArr[i].title}`, `${blogArr[i].body}`);
             };
         }
         else if(`${sortPost.value}` == "Sort Z-A") {
             deleteEl(blogMsg);
-            blogArr.sort(back('title'));
+            blogArr.sort(back("title"));
             for (let i = 0; i < blogArr.length; i++) {
                 render(`${blogArr[i].title}`, `${blogArr[i].body}`);
             };
         };
-    }
-    
-    )
-});
+    })
+
+    filterPostTitle.addEventListener("change", (event) => {
+        deleteEl(blogMsg);
+        const postStr = filterPostTitle.value;
+        console.log(postStr);
+        const blogFilter = blogArr.filter(x => {return x.title.toLowerCase().indexOf(postStr.toLowerCase()) > -1;});
+        for (let i = 0; i < blogFilter.length; i++) {
+            render(`${blogFilter[i].title}`, `${blogFilter[i].body}`);
+        };
+    })
+})
 
