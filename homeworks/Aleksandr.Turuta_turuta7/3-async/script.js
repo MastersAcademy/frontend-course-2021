0300f53cf21d3426313712329a70ba16e8d6d3de
Aleksandr <turuta51@gmail.com>
2021-11-05 01:23:13 +0200

    add event listener delete

--- homeworks/Aleksandr.Turuta_turuta7/3-async/script.js
+++ homeworks/Aleksandr.Turuta_turuta7/3-async/script.js
@@ -2,28 +2,54 @@ const messageListEl = document.querySelector('[data-container-messages]');
 const postTemplateEl = document.querySelector('[data-post]').content;
 const inputFilterEl = document.querySelector('[data-input]');
 const buttonSortEl = document.querySelector('[data-button-sort]');
+const loadingTemplateEl = document.querySelector('[data-loader]').content;
+const loadingEl = loadingTemplateEl.cloneNode(true).firstElementChild;
 
 let postsData = [];
 let cloneData = [];
+
 buttonSortEl.textContent = 'Sort off';
 
 // 0 - sortUp, 1-sortDown, 3-sor default
 let stateSort = 2;
 
-const URL = 'https://jsonplaceholder.typicode.com/posts';
+const url = 'https://jsonplaceholder.typicode.com/posts';
 
 // loading page
-const loadingTemplateEl = document.querySelector('[data-loader]').content;
-const loadingEl = loadingTemplateEl.cloneNode(true).firstElementChild;
 document.querySelector('[data-window-loader]').append(loadingEl);
 
 const dataPosts = async () => {
-    const returnPosts = await fetch(URL);
+    const returnPosts = await fetch(url);
     postsData = await returnPosts.json();
     cloneData = postsData;
 };
 
-const render = (data) => {
+const deletePost = async (id) => {
+    const response = await fetch(`${url}/${id}`, { method: 'DELETE' });
+    return response;
+};
+
+const addEventListenerPost = async () => {
+    const parent = document.querySelectorAll('[data-id-post]');
+
+    [...parent].forEach((postData) => {
+        postData.querySelector('[data-image]').addEventListener('click', async () => {
+            postsData = postsData.filter((post) => post.id !== Number(postData.dataset.idPost));
+            cloneData = postsData;
+
+            const resultResponse = await deletePost(postData.dataset.idPost);
+            if (resultResponse.status === 200) {
+                alert(`message: id:${postData.dataset.idPost} delete`);
+                postData.remove();
+            } else {
+                alert('message: error delete ');
+            }
+        });
+        return null;
+    });
+};
+
+const renderPost = (data) => {
     if (stateSort === 0) {
         data.sort((a, b) => a.title.localeCompare(b.title));
         buttonSortEl.textContent = 'Sort ▲';
@@ -38,9 +64,12 @@ const render = (data) => {
     data.forEach((element) => {
         const messageEl = postTemplateEl.cloneNode(true).firstElementChild;
         messageEl.querySelector('[data-title]').textContent = element.title;
+        messageEl.setAttribute('data-id-post', element.id);
         messageEl.querySelector('[data-body]').textContent = element.body;
         messageListEl.append(messageEl);
     });
+
+    addEventListenerPost();
 };
 
 const cleanDataPosts = async () => {
@@ -53,7 +82,7 @@ const cleanDataPosts = async () => {
 setTimeout(() => {
     dataPosts()
         .then(() => loadingEl.remove())
-        .then(() => render(postsData))
+        .then(() => renderPost(postsData))
         .catch((e) => console.log(e));
 }, 3000);
 
@@ -61,12 +90,12 @@ buttonSortEl.addEventListener('click', async () => {
     await cleanDataPosts();
     if (stateSort >= 2) stateSort = 0;
     else stateSort++;
-    render(cloneData);
+    renderPost(cloneData);
 });
 
 inputFilterEl.addEventListener('input', async () => {
     await cleanDataPosts();
     cloneData = postsData.filter((item) => item.title.toLowerCase()
         .includes(inputFilterEl.value.toLowerCase()));
-    render(cloneData);
+    renderPost(cloneData);
 });
