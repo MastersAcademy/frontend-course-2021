588202a116d3d162d968acded754a00cc08ab6ab
Andrii Morozov <slon88888@ukr.net>
2021-11-05 01:32:37 +0200

    2v

--- homeworks/Andrii.Morozov_AndriiMorozov88/3-async/script_blog.js
+++ homeworks/Andrii.Morozov_AndriiMorozov88/3-async/script_blog.js
@@ -1,12 +1,12 @@
 const blogContainer = document.querySelector('[data-blog-container]');
 function fetchData() {
     fetch('https://jsonplaceholder.typicode.com/posts')
-   .then((response) => {
-     return response.json();
-   })
-   .then(function(data) {
-     for (let i = 0; i < data.length; i++) {
-        let blogElement = document.createElement('div');
+        .then((response) => {
+            return response.json();
+        })
+        .then(function (data) {
+            for (let i = 0; i < data.length; i++) {
+                const blogElement = document.createElement('div');
         blogElement.className = 'blog_container-el';
         blogContainer.append(blogElement);
         blogElement.innerText = data[i].title;
@@ -64,7 +64,7 @@ function fetchDataFilter() {
    .then(function(data) {
     blogContainer.innerHTML = "";
      for (let i = 0; i < data.length; i++) {
-         if (data[i].title.indexOf(filter.value) != -1) {
+         if (data[i].title.indexOf(filter.value) !== -1) {
          
         let blogElement = document.createElement('div');
         blogElement.className = 'blog_container-el';
