7ebb9d3e7d54deff5cd7d5b4bc538e93e5cccb80
RevskyAndrey <andrey.g1362@gmail.com>
2021-11-05 14:06:29 +0200

    task in progress

--- homeworks/andrii.revskyi_RevskyAndrey/3-async/js/main.js
+++ homeworks/andrii.revskyi_RevskyAndrey/3-async/js/main.js
@@ -1,9 +1,15 @@
+const preloader = document.querySelector('[data-preloader]');
 const list = document.querySelector('[data-list]');
+const selectEl = document.querySelector('[data-select]');
+const filterEl = document.querySelector('[data-filter]');
 
 function createItem({ title, body }) {
     const template = document.createElement('div');
     template.classList.add('item');
-    template.innerHTML = `<h3> ${title}</h3> <p>${body}</p> <button name="delete" class="item__delete">X</button>`;
+    template.innerHTML = `
+        <h3 class="item__title"> ${title}</h3>
+        <p class="item__text">${body}</p>
+        <button name="delete" class="item__delete">Х</button>`;
     list.appendChild(template);
 }
 
@@ -13,15 +19,15 @@ list.addEventListener('click', (e) => {
     }
 });
 
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault();
-//     const inputElement = [...e.target].find((el) => el.name && el.name === 'message');
-//     const message = inputElement.value.trim();
-//     if (message.length > 0) {
-//         createItem(message);
-//         inputElement.value = '';
-//     }
-// });
+selectEl.addEventListener('change', (e) => {
+    e.preventDefault();
+    console.log(selectEl.value);
+});
+
+filterEl.addEventListener('keyup', (e) => {
+    e.preventDefault();
+    console.log(filterEl.value);
+});
 
 async function getPost() {
     const url = 'https://jsonplaceholder.typicode.com/posts';
@@ -29,13 +35,18 @@ async function getPost() {
     return response;
 }
 
-async function render() {
-    const arr = await getPost();
-    console.log(arr);
+function render(arr) {
     arr.forEach((el) => {
-        console.log({ el });
         createItem(el);
     });
 }
 
-render();
+async function init() {
+    const data = await getPost();
+    setTimeout(() => {
+        preloader.classList.add('hide');
+        render(data);
+    }, 3000);
+}
+
+init();
