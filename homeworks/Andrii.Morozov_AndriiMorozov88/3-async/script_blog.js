e9d652c95e745d11fa217af166d744ec83aed508
Andrii Morozov <slon88888@ukr.net>
2021-11-05 10:25:40 +0200

    10v

--- homeworks/Andrii.Morozov_AndriiMorozov88/3-async/script_blog.js
+++ homeworks/Andrii.Morozov_AndriiMorozov88/3-async/script_blog.js
@@ -42,30 +42,29 @@ function fetchDataZ() {
                 }
             }
             for (let i = 0; i < data.length; i++) {
-                let blogElement = document.createElement('div');
+                const blogElement = document.createElement('div');
                 blogElement.className = 'blog_container-el';
                 blogContainer.append(blogElement);
                 blogElement.innerText = data[i].title;
-            }  
-    });
+            }
+        });
 }
 function fetchDataFilter() {
     fetch('https://jsonplaceholder.typicode.com/posts')
-   .then((response) => response.json())
-   .then((data) => {
-    blogContainer.innerHTML = "";
-    for (let i = 0; i < data.length; i++) {
-         if (data[i].title.indexOf(filter.value) !== -1) {
-         
-        let blogElement = document.createElement('div');
-        blogElement.className = 'blog_container-el';
-        blogContainer.append(blogElement);
-        blogElement.innerText = data[i].title;
-         }
-         }
-         filter.value = ''; 
-    });
- }
+        .then((response) => response.json())
+        .then((data) => {
+            blogContainer.innerHTML = "";
+            for (let i = 0; i < data.length; i++) {
+                if (data[i].title.indexOf(filter.value) !== -1) {
+                    let blogElement = document.createElement('div');
+                    blogElement.className = 'blog_container-el';
+                    blogContainer.append(blogElement);
+                    blogElement.innerText = data[i].title;
+                }
+            }
+        filter.value = ''; 
+        });
+}
  
 document.querySelector('[data-btn]').addEventListener('click', () => {
 let sort = document.querySelector('[data-sort]');
