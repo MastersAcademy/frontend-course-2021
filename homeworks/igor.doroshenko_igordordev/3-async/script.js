286a62e1cf278ce1b7c1710306e7ac803de46fe8
igordordev <88446424+igordordev@users.noreply.github.com>
2021-11-05 17:04:59 +0200

    lint errors fixed

--- homeworks/igor.doroshenko_igordordev/3-async/script.js
+++ homeworks/igor.doroshenko_igordordev/3-async/script.js
@@ -45,7 +45,8 @@ async function fetchArticles() {
     sortSelect.addEventListener('change', () => {
         sortedArticles = sortArticles(articlesDefault, sortSelect);
         articlesList.innerHTML = '';
-        createArticles(sortedArticles); });
+        createArticles(sortedArticles);
+    });
 
     filterInput.addEventListener('input', () => {
         sortedArticles = sortArticles(articlesDefault, sortSelect);
@@ -53,7 +54,8 @@ async function fetchArticles() {
         filteredArticles = filteredArticles.filter((article) => article.title
             .includes(filterInput.value.toLowerCase()));
         articlesList.innerHTML = '';
-        createArticles(filteredArticles); });
-    }
+        createArticles(filteredArticles); 
+    });
+}
 
 fetchArticles();
