1dc14999f1a6e04f4878183095068401b4afcd5a
Andrii Morozov <slon88888@ukr.net>
2021-11-05 10:05:43 +0200

    6v

--- homeworks/Andrii.Morozov_AndriiMorozov88/3-async/script_blog.js
+++ homeworks/Andrii.Morozov_AndriiMorozov88/3-async/script_blog.js
@@ -1,8 +1,8 @@
 const blogContainer = document.querySelector('[data-blog-container]');
 function fetchData() {
     fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())
-        .then(data => {
+        .then((response) => response.json())
+        .then((data) => {
             for (let i = 0; i < data.length; i++) {
                 const blogElement = document.createElement('div');
                 blogElement.className = 'blog_container-el';
@@ -13,8 +13,8 @@ function fetchData() {
 }
 function fetchDataA() {
     fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(response => response.json())
-        .then(data => {
+        .then((response) => response.json())
+        .then((data) => {
             for (let i = 0; i < data.length; i++) {
                 for (let j = 0; j < data.length; j++) {
                     if (data[i].title < data[j].title) {
@@ -22,25 +22,25 @@ function fetchDataA() {
                     }
                 }
             }
-     for (let i = 0; i < data.length; i++) {
-        let blogElement = document.createElement('div');
-        blogElement.className = 'blog_container-el';
-        blogContainer.append(blogElement);
-        blogElement.innerText = data[i].title;
-     }   
+            for (let i = 0; i < data.length; i++) {
+                const blogElement = document.createElement('div');
+                blogElement.className = 'blog_container-el';
+                blogContainer.append(blogElement);
+                blogElement.innerText = data[i].title;
+            }   
     });
- }
+}
 function fetchDataZ() {
     fetch('https://jsonplaceholder.typicode.com/posts')
-       .then(response => response.json())
-       .then(data => {
-           for (let i = 0; i < data.length; i++) {
-               for (let j = 0; j < data.length; j++) {
-                   if (data[i].title > data[j].title) {
+       .then((response) => response.json())
+       .then((data) => {
+            for (let i = 0; i < data.length; i++) {
+                for (let j = 0; j < data.length; j++) {
+                    if (data[i].title > data[j].title) {
                        [data[i], data[j]] = [data[j], data[i]]; 
+                    }
+                }
             }
-        }
-       }
      for (let i = 0; i < data.length; i++) {
         let blogElement = document.createElement('div');
         blogElement.className = 'blog_container-el';
@@ -52,8 +52,8 @@ function fetchDataZ() {
 }
 function fetchDataFilter() {
     fetch('https://jsonplaceholder.typicode.com/posts')
-   .then(response => response.json())
-   .then(data => {
+   .then((response) => response.json())
+   .then((data) => {
     blogContainer.innerHTML = "";
     for (let i = 0; i < data.length; i++) {
          if (data[i].title.indexOf(filter.value) !== -1) {
