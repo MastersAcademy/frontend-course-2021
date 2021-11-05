ce712dfcc6f039021402721327a12ce48b41359e
Volodymyr.Khruchsh <vladimir.khrush@gmail.com>
2021-11-05 11:19:31 +0200

    3-async

---
+++ homeworks/volodymyr.khrushch_Vladimior/3-async/js/script.js
@@ -0,0 +1,43 @@
+const container = document.querySelector("[data-blogs]");
+const sorting = document.querySelector("[data-sort]");
+const load = document.querySelector("[data-load]");
+
+const renderPosts = async (search) => {
+  let url = "https://jsonplaceholder.typicode.com/posts";
+  if (search) {
+    url += `?_sort=title&_order=${search}`;
+  }
+  const render = await fetch(url);
+  const posts = await render.json();
+  load.style.display = 'none';
+  let blogPosts = "";
+  posts.forEach((posts) => {
+    blogPosts += `
+        <div class='content__posts__card' data-card>
+        <h3 class='content__posts__title' data-title>${posts.title}</h3>
+        <p class='content__posts__text'>${posts.body}</p>
+        </div>
+        `;
+  });
+
+  container.innerHTML = blogPosts;
+};
+
+// Sorting
+sorting.addEventListener("change", (event) => {
+  let result = `${event.target.value}`;
+  renderPosts(result.trim());
+});
+
+// Filter
+document.querySelector('[data-input]').addEventListener('input', e => {
+  const val = e.target.value.toLowerCase();
+
+  container.querySelectorAll('[data-title]').forEach(term => {
+    term.closest('[data-card]').style.display = term.innerText.toLowerCase().includes(val)
+      ? 'block'
+      : 'none';
+  });
+});
+
+renderPosts();
