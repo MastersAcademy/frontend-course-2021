0706e6b4c9221cd52053cbc7ebecc28a0a283354
igordordev <88446424+igordordev@users.noreply.github.com>
2021-11-05 15:27:25 +0200

    added 3-async

---
+++ homeworks/igor.doroshenko_igordordev/3-async/script.js
@@ -0,0 +1,67 @@
+const articlesList = document.querySelector('.articles-area');
+const loadingBLock = document.querySelector('.loading-window');
+const url = 'https://jsonplaceholder.typicode.com/posts';
+const sortSelect = document.querySelector('[article-sort-option]');
+const filterInput = document.querySelector('[article-filter-input]');
+
+const timeDelay = milliSecond => {
+    return new Promise(r => setTimeout(() => r(), milliSecond));
+}
+
+fetchArticles();
+
+async function fetchArticles() {
+    await timeDelay(3000);
+    const response = await fetch(url);
+    const data = await response.json();
+
+    let articlesDefault = data;
+    let sortedArticles = sortArticles(articlesDefault, sortSelect);
+    createArticles(sortedArticles);
+    loadingBLock.classList.add('loading-window--hidden');
+
+    sortSelect.addEventListener('change', function () {
+        sortedArticles = sortArticles(articlesDefault, sortSelect);
+        articlesList.innerHTML = '';
+        
+        createArticles(sortedArticles);
+    });
+
+    filterInput.addEventListener('input', function () {
+        sortedArticles = sortArticles(articlesDefault, sortSelect);
+        let filteredArticles = sortedArticles.slice();
+        filteredArticles = filteredArticles.filter(article => article.title.includes(filterInput.value.toLowerCase()));
+        articlesList.innerHTML = '';
+
+        createArticles(filteredArticles);
+    });
+
+}
+
+function sortArticles(articlesArr, sortCondition) {
+    let articlesSorted = articlesArr.slice();
+    if (sortCondition.value === 'sort-a-z') {
+        articlesSorted.sort((a, b) => (a.title > b.title) ? 1 : -1);
+    } else if (sortCondition.value === 'sort-z-a') {
+        articlesSorted.sort((a, b) => (a.title > b.title) ? 1 : -1).reverse();
+    } else if (sortCondition.value === 'sort-default') {
+        articlesSorted = articlesArr;
+    }
+    return articlesSorted;
+}
+
+function createArticles(sortedArticles) {
+    sortedArticles.forEach(article => {
+        let newArticle = document.createElement('article');
+        let newArticleHeader = document.createElement('h2');
+        let newArticleContent = document.createElement('p');
+        newArticleHeader.classList.add('article__title');
+        newArticleHeader.textContent = article.title;
+        newArticleContent.classList.add('article__content');
+        newArticleContent.textContent = article.body;
+        newArticle.classList.add('articles-area__article', 'article');
+        newArticle.appendChild(newArticleHeader);
+        newArticle.appendChild(newArticleContent);
+        articlesList.append(newArticle);
+    });
+}
