368e121df39f96c2ab9cf7d143127284690ad99d
Andrii Morozov <slon88888@ukr.net>
2021-11-05 10:56:50 +0200

    14v

--- homeworks/Andrii.Morozov_AndriiMorozov88/3-async/script_blog.js
+++ homeworks/Andrii.Morozov_AndriiMorozov88/3-async/script_blog.js
@@ -19,7 +19,7 @@ function fetchDataA() {
             for (let i = 0; i < data.length; i++) {
                 for (let j = 0; j < data.length; j++) {
                     if (data[i].title < data[j].title) {
-                        [data[i], data[j]] = [data[j], data[i]];
+                        let = [data[i], data[j]] = [data[j], data[i]];
                     }
                 }
             }
@@ -38,7 +38,7 @@ function fetchDataZ() {
             for (let i = 0; i < data.length; i++) {
                 for (let j = 0; j < data.length; j++) {
                     if (data[i].title > data[j].title) {
-                        [data[i], data[j]] = [data[j], data[i]];
+                        let = [data[i], data[j]] = [data[j], data[i]];
                     }
                 }
             }
