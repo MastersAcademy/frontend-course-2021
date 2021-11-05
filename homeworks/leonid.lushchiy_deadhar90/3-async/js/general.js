097e4382da6bfcf6ec0d293ee213c2203db3cbe9
deadhard90 <deadhard90@gmail.com>
2021-11-05 13:08:10 +0200

    resave file whith LF, and check errors

--- homeworks/leonid.lushchiy_deadhar90/3-async/js/general.js
+++ homeworks/leonid.lushchiy_deadhar90/3-async/js/general.js
@@ -1,51 +1,48 @@
-const section = document.querySelector('section');
 const sectionTweets = document.querySelector('[data-tweets]');
-const filtring = document.querySelector('[data-select-option]')
+const filtring = document.querySelector('[data-select-option]');
 
-//from MDN
+// from MDN
 const requestURL = 'https://jsonplaceholder.typicode.com/posts';
 const request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
 
-let b = new Promise(function(resolve, reject){
+const load = new Promise((resolve, reject) => {
     request.onload = setTimeout(function doBlog() {
-    const blog = request.response;
-    resolve(showTweets(blog));
-    reject(showError());
-  }, 3000);
+        const blog = request.response;
+        resolve(showTweets(blog));
+        reject(showError());
+    }, 3000);
 });
 
 // do element from Array
 function showTweets(jsonObj) {
     const tweets = jsonObj;
-    for (var i = 0; i < tweets.length; i++) {
+    let i = 0;
+    for (i; i < tweets.length; i++) {
         const myArticle = document.createElement('article');
         myArticle.className = 'tweet';
         const myH1 = document.createElement('h1');
         const myPara1 = document.createElement('p');
-
         myH1.textContent = tweets[i].title;
         myPara1.textContent = tweets[i].body;
-
         myArticle.appendChild(myH1);
         myArticle.appendChild(myPara1);
-        
         sectionTweets.appendChild(myArticle);
     }
 }
 
 // want to do error but not work
-function showError(){
-    let squer = document.querySelector('.rollingSquer');
+function showError() {
+    const squer = document.querySelector('.rollingSquer');
     squer.classList.add('error');
     squer.classList.remove('rollingSquer');
 }
 
 // hidden preloader when load from server
-b.then(function () {
-    let preloaderEl = document.querySelector('[data-preloader]');
+load.then(function() {
+    const preloaderEl = document.querySelector('[data-preloader]');
     preloaderEl.classList.add('hidden');
     preloaderEl.classList.remove('visible');
 });
@@ -57,42 +54,43 @@ filtring.addEventListener('change', () => {
     if (filtring.options[filtring.selectedIndex].value === 'withOut') {
         document.location.reload();
     } if (filtring.options[filtring.selectedIndex].value === 'fromAtoZ') {
-        Array.prototype.map.call(nodesToSort, function(node) {
+        Array.prototype.map.call(nodesToSort, (node) => {
             return {
-                node:node,
-                relevantText: node.querySelector('h1').textContent
+                node: node,
+                relevantText: node.querySelector('h1').textContent,
             };
-        }).sort(function(a,b) {
+        }).sort((a,b) => {
             return a.relevantText.localeCompare(b.relevantText);
-        }).forEach(function(item){
+        }).forEach((item) => {
             dataTweets.appendChild(item.node);
         });
     } if (filtring.options[filtring.selectedIndex].value === 'fromZtoA') {
-         Array.prototype.map.call(nodesToSort, function(node) {
+        Array.prototype.map.call(nodesToSort, (node) => {
             return {
-                node:node,
-                relevantText: node.querySelector('h1').textContent
+                node: node,
+                relevantText: node.querySelector('h1').textContent,
             };
-        }).reverse(function(a,b) {
+        }).reverse((a,b) => {
             return b.relevantText.localeCompare(a.relevantText);
-        }).forEach(function(item){
+        }).forEach((item) => {
             dataTweets.appendChild(item.node);
         });
     }
 });
 
 // filtring text from input
-const input = document.querySelector('[data-mySearch]')
+const input = document.querySelector('[data-mySearch]');
 input.addEventListener('input', () => {
     const filter = input.value.toUpperCase();
     const tweets = document.querySelector('[data-tweets]');
     const article = tweets.getElementsByTagName('article');
-    for (i = 0; i < article.length; i++) {
-      H1 = article[i].getElementsByTagName('h1')[0];
-      if (H1.innerHTML.toUpperCase().trim().indexOf(filter) > -1) {
-        article[i].style.display = "";
-      } else {
-        article[i].style.display = "none";
-      }
+    let i = 0;
+    for (i; i < article.length; i++) {
+        const H1 = article[i].getElementsByTagName('h1')[0];
+        if (H1.innerHTML.toUpperCase().trim().indexOf(filter) > -1) {
+            article[i].style.display = ''
+        } else {
+            article[i].style.display = 'none';
+        }
     }
 });
