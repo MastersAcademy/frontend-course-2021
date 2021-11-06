d1cdcf6b75ae06aa955a4d9d9467936f12696d32
Chakipaki <belyeve2@icloud.com>
2021-11-06 09:31:17 +0200

    END
    I can’t resolve all problem, waiting for you check))

--- homeworks/ivan.bieliaiev_Ckakipaki/3-async/app.js
+++ homeworks/ivan.bieliaiev_Ckakipaki/3-async/app.js
@@ -49,13 +49,16 @@ function createPostsList(posts) {
 }
 // Delete post
 function addDeleteSupport() {
+    const deleteMessege = document.createElement('div');
+    function presentMessege(el) {
+        deleteMessege.innerText = `Post: ${el.target.attributes[0].value} deleted`;
+        deleteMessege.classList.add('post__delete__outText');
+        document.body.appendChild(deleteMessege);
+    }
     document.addEventListener('click', (e) => {
-        const deleteMessege = document.createElement('div');
         if (e.target.classList[0] === 'delete__button') {
-            deleteMessege.innerText = `Post: ${e.target.attributes[0].value} deleted`;
-            deleteMessege.classList.add('post__delete__outText');
-            document.body.appendChild(deleteMessege);
-            blog.removeChild(e.target.parentElement.parentElement);
+            blog.removeChild(e.target.closest('article'));
+            presentMessege(e);
         }
         setTimeout(() => {
             document.body.removeChild(deleteMessege);
