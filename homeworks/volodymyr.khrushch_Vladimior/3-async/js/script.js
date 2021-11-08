21fc0fbcc088a0c55559f48c02d48a9eec558a8c
Volodymyr.Khruchsh <vladimir.khrush@gmail.com>
2021-11-08 11:05:21 +0200

    correction of eslint

--- homeworks/volodymyr.khrushch_Vladimior/3-async/js/script.js
+++ homeworks/volodymyr.khrushch_Vladimior/3-async/js/script.js
@@ -2,6 +2,24 @@ const containerEl = document.querySelector('[data-blogs]');
 const sortingEl = document.querySelector('[data-sort]');
 const loadEl = document.querySelector('[data-load]');
 
+// add posts
+const addPosts = async (result) => {
+    const addPostBox = await result;
+    let blogPosts = '';
+
+    addPostBox.forEach((posts) => {
+        blogPosts += `
+        <section class='content__posts__card' data-card>
+        <h3 class='content__posts__title' data-title>${posts.title}</h3>
+        <p class='content__posts__text'>${posts.body}</p>
+        </section>
+        `;
+    });
+
+    loadEl.style.display = 'none';
+    containerEl.innerHTML = blogPosts;
+};
+
 const renderPosts = async (sorting) => {
     let url = 'https://jsonplaceholder.typicode.com/posts';
     if (sorting) {
@@ -23,29 +41,10 @@ document.querySelector('[data-input]').addEventListener('input', (e) => {
     const val = e.target.value.toLowerCase();
 
     containerEl.querySelectorAll('[data-title]').forEach((element) => {
-        element.closest('[data-card]').hidden = element.innerText.toLowerCase().includes(val)
-            ? false
-            : true;
+        element.closest('[data-card]').style.display = element.innerText.toLowerCase().includes(val)
+            ? ''
+            : 'none';
     });
 });
-
-// add posts
-const addPosts = async (result) => {
-
-    const addPostBox = await result;
-    let blogPosts = '';
-
-    addPostBox.forEach((posts) => {
-        blogPosts += `
-        <section class='content__posts__card' data-card>
-        <h3 class='content__posts__title' data-title>${posts.title}</h3>
-        <p class='content__posts__text'>${posts.body}</p>
-        </section>
-        `;
-    });
-
-    loadEl.style.display = 'none';
-    containerEl.innerHTML = blogPosts;
-};
 
 renderPosts();
