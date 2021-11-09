afe99192308460a3630cc00da154e08a7841b1cd
Oleksandr Prylutskiy <fleks1986@gmail.com>
2021-11-09 22:22:52 +0200

    create fails and quick styling

---
+++ homeworks/oleksandr.prylutskiy_SanSandru4/4-modules/time.js
@@ -0,0 +1,32 @@
+/**
+ * @param date - date string of any supported format
+ * @returns array of Friday dates in a month the date from
+ *
+export function getMondaysOfMonth(date) {
+    return [1, 8, 15, 22, 29];
+}
+
+/**
+ * @param date - date string of any supported format
+ * @returns {boolean} true if month of a date has 31 day, otherwise returns false
+ *
+export function isMonthLong(date) {
+    return false;
+}
+
+/**
+ * @param date - date string of any supported format
+ * @returns {number} number of days in a shortest week of the date month
+ *
+export function shortestWeekDaysNumber(date) {
+    return 3;
+}
+
+/**
+ * @param date - date string of any supported format
+ * @returns {number} number of full weeks in the date month. To be full, week should start
+ * and end in the same month
+ *
+export function fullWeeksNumberInMonth(date) {
+    return 3;
+} */
