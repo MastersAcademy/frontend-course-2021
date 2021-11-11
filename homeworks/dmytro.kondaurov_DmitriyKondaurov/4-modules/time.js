eaafa3e72962423a4a980a140e22708b2f5c0087
Dmytro Kondaurov <invisibl@ukr.net>
2021-11-12 01:00:18 +0200

    TGet number of days in the shortest week logic done

--- homeworks/dmytro.kondaurov_DmitriyKondaurov/4-modules/time.js
+++ homeworks/dmytro.kondaurov_DmitriyKondaurov/4-modules/time.js
@@ -22,3 +22,29 @@ export function isMonthLong(date) {
     dateObject.setDate(31);
     return dateObject.getDate() === 31;
 }
+/**
+ * @param date - date string of any supported format
+ * @returns {number} of days in a shortest week of the date month
+ */
+export function shortestWeekDaysNumber(date) {
+    const dateObject = new Date(date);
+    const dateYear = dateObject.getFullYear();
+    const dateMonth = dateObject.getMonth();
+    dateObject.setDate(1);
+    const firstWeekLength = 7 - ((dateObject.getDay() + 6) % 7);
+    let lastWeekLength;
+    if (isMonthLong(date)) {
+        dateObject.setDate(31);
+        lastWeekLength = ((dateObject.getDay() + 6) % 7) + 1;
+    } else if (dateMonth !== 1) {
+        dateObject.setDate(30);
+        lastWeekLength = ((dateObject.getDay() + 6) % 7) + 1;
+    } else if (((dateYear % 4 === 0) && (dateYear % 100 !== 0)) || (dateYear % 400 === 0)) {
+        dateObject.setDate(29);
+        lastWeekLength = ((dateObject.getDay() + 6) % 7) + 1;
+    } else {
+        dateObject.setDate(28);
+        lastWeekLength = ((dateObject.getDay() + 6) % 7) + 1;
+    }
+    return Math.min(firstWeekLength, lastWeekLength);
+}
