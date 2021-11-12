4524bcea3f871613b8d46d25d2442bda8db10681
ConstantinRusenko <krusenko@gmail.com>
2021-11-12 19:35:28 +0200

    add task with *

--- homeworks/konstantin.rusenko_ConstantinRusenko/4-modules/index.js
+++ homeworks/konstantin.rusenko_ConstantinRusenko/4-modules/index.js
@@ -1,11 +1,30 @@
 import * as time from './time.js';
 
-const buttonEl = document.querySelector('button');
+const mondaysButton = document.querySelector('[data-get-mondays]');
+const longMonthButton = document.querySelector('[data-month-length]');
+const shortestWeekButton = document.querySelector('[data-shortest-week]');
+const fullWeekButton = document.querySelector('[data-full-weeks]');
 const inputEl = document.querySelector('input');
+const selectEl = document.querySelector('[data-select]');
 
-buttonEl.addEventListener('click', () => {
-    console.log(time.isMonthLong(inputEl.value));
-    console.log(time.getMondaysOfMonth(inputEl.value));
-    console.log(time.fullWeeksNumberInMonth(inputEl.value));
-    console.log(time.shortestWeekDaysNumber(inputEl.value));
+longMonthButton.addEventListener('click', () => {
+    document.querySelector('[data-length]').innerHTML = `${time.isMonthLong(inputEl.value)}`;
 });
+
+mondaysButton.addEventListener('click', () => {
+    document.querySelector('[data-monday]').innerHTML = `${(time.getMondaysOfMonth(inputEl.value))}`;
+});
+
+shortestWeekButton.addEventListener('click', () => {
+    document.querySelector('[data-shortest]').innerHTML = `${time.shortestWeekDaysNumber(inputEl.value)}`;
+});
+
+fullWeekButton.addEventListener('click', () => {
+    document.querySelector('[data-full]').innerHTML = `${time.fullWeeksNumberInMonth(inputEl.value)}`;
+});
+
+document.addEventListener('DOMContentLoaded', () => time.digitalClock(selectEl.value));
+
+document.addEventListener('change', () => setInterval(() => {
+    time.digitalClock(selectEl.value);
+}, 1000));
--- homeworks/konstantin.rusenko_ConstantinRusenko/4-modules/time.js
+++ homeworks/konstantin.rusenko_ConstantinRusenko/4-modules/time.js
@@ -1,7 +1,12 @@
-export const getMondaysOfMonth = (date) => {
+const getMonthLength = (date) => {
     const nowDate = date.split('-');
     nowDate.splice(2, 1, '0');
     const monthLength = new Date(...nowDate).getDate();
+    return monthLength;
+};
+
+export const getMondaysOfMonth = (date) => {
+    const monthLength = getMonthLength(date);
     const arrOfMond = [];
     for (let i = 1; i <= monthLength; i++) {
         const monthDay = new Date(date);
@@ -12,16 +17,12 @@ export const getMondaysOfMonth = (date) => {
 };
 
 export const isMonthLong = (date) => {
-    const nowDate = date.split('-');
-    nowDate.splice(2, 1, '0');
-    const monthLength = new Date(...nowDate).getDate();
+    const monthLength = getMonthLength(date);
     return monthLength === 31;
 };
 
 export const fullWeeksNumberInMonth = (date) => {
-    const nowDate = date.split('-');
-    nowDate.splice(2, 1, '0');
-    const monthLength = new Date(...nowDate).getDate();
+    const monthLength = getMonthLength(date);
     const arrOfMond = getMondaysOfMonth(date);
     return (monthLength + 1 - arrOfMond[arrOfMond.length - 1] === 7)
         ? arrOfMond.length : arrOfMond.length - 1;
@@ -30,13 +31,21 @@ export const fullWeeksNumberInMonth = (date) => {
 export const shortestWeekDaysNumber = (date) => {
     let firstWeek = 7;
     let lastWeek = 7;
-    const nowDate = date.split('-');
-    nowDate.splice(2, 1, '0');
-    const monthLength = new Date(...nowDate).getDate();
+    const monthLength = getMonthLength(date);
     const arrOfMond = getMondaysOfMonth(date);
     if (arrOfMond[0] !== 1) firstWeek = arrOfMond[0] - 1;
     if (arrOfMond[arrOfMond.length - 1] !== monthLength - 6) {
         lastWeek = monthLength + 1 - arrOfMond[arrOfMond.length - 1];
     }
     return firstWeek > lastWeek ? lastWeek : firstWeek;
+};
+
+export const digitalClock = (timeZone) => {
+    const date = new Date();
+    let hours = +timeZone === 0 ? date.getHours() : (date.getHours() + +timeZone);
+    let minutes = date.getMinutes();
+    if (hours > 23) hours %= 24;
+    if (hours < 10) hours = `0${hours}`;
+    if (minutes < 10) minutes = `0${minutes}`;
+    document.querySelector('[data-clock]').innerHTML = `${hours}:${minutes}`;
 };
