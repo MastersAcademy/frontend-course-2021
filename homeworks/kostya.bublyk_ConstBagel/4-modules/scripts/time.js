d64dee4283733996fc81ac792e6d0ac43a94e85d
Kostya <qa.kost@gmail.com>
2021-11-12 15:26:36 +0200

    format files

--- homeworks/kostya.bublyk_ConstBagel/4-modules/scripts/time.js
+++ homeworks/kostya.bublyk_ConstBagel/4-modules/scripts/time.js
@@ -1,45 +1,47 @@
 /**
  * Internal function for creating calendar as matrix (array)
  */
- function getMatrixOfCurrentMonth (pickedDate) {
+function getMatrixOfCurrentMonth(pickedDate) {
     const date = new Date(pickedDate);
     const amountDaysPerWeek = 7;
     date.setDate(1);
     const before = Array(date.getDay()).fill(null);
     date.setMonth(date.getMonth() + 1);
     date.setDate(0);
-    const after = Array(amountDaysPerWeek - (date.getDay()+1)).fill(null);
-    const pickedMonth = Array(date.getDate()).fill(1)
+    const after = Array(amountDaysPerWeek - (date.getDay() + 1)).fill(null);
+    const pickedMonth = Array(date.getDate())
+        .fill(1)
         .map((numb, index) => {
             date.setDate(numb + index);
             return {
-              date: date.getDate(),
-              dayOfWeek: date.getDay()
+                date: date.getDate(),
+                dayOfWeek: date.getDay(),
             };
         });
     const fullWeeksOfPickedMonth = [...before, ...pickedMonth, ...after];
-    return Array(fullWeeksOfPickedMonth.length / amountDaysPerWeek).fill(null)
-        .map(() => fullWeeksOfPickedMonth.splice(0, amountDaysPerWeek)
-            .filter(Boolean));
+    return Array(fullWeeksOfPickedMonth.length / amountDaysPerWeek)
+        .fill(null)
+        .map(() =>
+            fullWeeksOfPickedMonth.splice(0, amountDaysPerWeek).filter(Boolean)
+        );
 }
 
 /**
  * @param date - date string of any supported format
  * @returns array of Friday dates in a month the date from
  */
-export function getFridaysOfMonth (date) {
-    return getMatrixOfCurrentMonth(date)
-        .reduce((result, week) => {
-            const foundDay = week.find(day => day.dayOfWeek === 5);
-            return foundDay ? [...result, foundDay.date] : result;
-        } , []);
+export function getFridaysOfMonth(date) {
+    return getMatrixOfCurrentMonth(date).reduce((result, week) => {
+        const foundDay = week.find((day) => day.dayOfWeek === 5);
+        return foundDay ? [...result, foundDay.date] : result;
+    }, []);
 }
 
 /**
  * @param date - date string of any supported format
  * @returns {boolean} true if month of a date has 31 day, otherwise returns false
  */
-export function isMonthLong (date) {
+export function isMonthLong(date) {
     const pickedDate = new Date(date);
     pickedDate.setMonth(pickedDate.getMonth() + 1);
     pickedDate.setDate(0);
@@ -50,15 +52,17 @@ export function isMonthLong (date) {
  * @param date - date string of any supported format
  * @returns {number} number of days in a shortest week of the date month
  */
-export function shortestWeekDaysNumber (date) {
-    return Math.min(...getMatrixOfCurrentMonth(date).map((week) => week.length));
+export function shortestWeekDaysNumber(date) {
+    return Math.min(
+        ...getMatrixOfCurrentMonth(date).map((week) => week.length)
+    );
 }
 
 /**
  * @param date - date string of any supported format
  * @returns {number} number of full weeks in the date month. To be full, week should start and end in the same month
  */
-export function fullWeeksNumberInMonth (date) {
-    return getMatrixOfCurrentMonth(date)
-        .filter((week) => week.length === 7).length;
+export function fullWeeksNumberInMonth(date) {
+    return getMatrixOfCurrentMonth(date).filter((week) => week.length === 7)
+        .length;
 }
