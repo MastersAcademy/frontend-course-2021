b533ee2178822a529708889afc4d6117f7662ac9
Kostya <qa.kost@gmail.com>
2021-11-12 09:19:31 +0200

    add project with modules

---
+++ homeworks/kostya.bublyk_ConstBagel/4-modules/README.md
@@ -0,0 +1,5 @@
+**Get started**
+* Install local server by running `npm install -g http-server` (make sure that nobe ver > 12);
+* Pass to root directory of the Project when index.html file is located;
+* Run `http-server -c-1` command;
+* Open any browser and paste `http://127.0.0.1:8080` or `http://192.168.1.2:8080` address.
---
+++ homeworks/kostya.bublyk_ConstBagel/4-modules/scripts/script.js
@@ -0,0 +1,55 @@
+import {
+    getFridaysOfMonth,
+    isMonthLong,
+    shortestWeekDaysNumber,
+    fullWeeksNumberInMonth
+} from './time.js'; 
+
+const inputDateField = document.querySelector('[ data-role="input-date-field"]');
+const getFridaysButton = document.querySelector('[ data-role="get-fridays-button"]');
+const isMonthLongButton = document.querySelector('[ data-role="is-month-long-button"]');
+const daysShortestWeekButton = document.querySelector('[data-role="days-shortest-week-button"]');
+const fullWeekInMonthButton = document.querySelector('[data-role="full-week-month-button"]');
+let date = '';
+
+/**
+ * make all buttons disabled by default (before picking date)
+ */
+document.querySelectorAll('button')
+    .forEach((button) => { button.disabled = true; });
+
+/**
+ * function for displaying results
+ */
+function displayResult (selector, result) {
+    document.querySelector(selector).textContent = result;
+}
+
+/**
+ * Event listeners:
+ */
+inputDateField.addEventListener('change', (event) => { 
+    date = event.target.value;
+    document.querySelectorAll('button')
+        .forEach((button) => { button.disabled = false; });
+});
+
+getFridaysButton.addEventListener('click', () => {
+    displayResult('[data-role="get-fridays-output"]',
+        `[${getFridaysOfMonth(date)}]`);
+});
+
+isMonthLongButton.addEventListener('click', () => {
+    displayResult('[data-role="is-month-long-output"]',
+        isMonthLong(date));
+});
+
+daysShortestWeekButton.addEventListener('click', () => {
+    displayResult('[data-role="days-shortest-week-output"]',
+        shortestWeekDaysNumber(date));
+});
+
+fullWeekInMonthButton.addEventListener('click', () => {
+    displayResult('[data-role="full-week-month-output"]',
+        fullWeeksNumberInMonth(date));
+});
