fe781b17fdc89ca60094226c4a15acc18b7394b1
MaksymSelenkov <snegopad100@gmail.com>
2021-11-12 14:29:08 +0200

    refactoring

--- homeworks/maksym.selenkov_MaksymSelenkov/4-modules/app.js
+++ homeworks/maksym.selenkov_MaksymSelenkov/4-modules/app.js
@@ -2,26 +2,26 @@ import {
     getMondaysOfMonth, isMonthLong, fullWeeksNumberInMonth, shortestWeekDaysNumber,
 } from './time.js';
 
+function userDate() {
+    return new Date(document.querySelector('[data-input]').value);
+}
+
 const mondaysBtnEl = document.querySelector('[data-mondays]');
 mondaysBtnEl.addEventListener('click', () => {
-    const userDate = new Date(document.querySelector('[data-input]').value);
-    mondaysBtnEl.after(getMondaysOfMonth(userDate));
+    document.querySelector('[data-result-mondays]').textContent = getMondaysOfMonth(userDate());
 });
 
 const longMonthEl = document.querySelector('[data-month]');
 longMonthEl.addEventListener('click', () => {
-    const userDate = new Date(document.querySelector('[data-input]').value);
-    longMonthEl.after(isMonthLong(userDate));
+    document.querySelector('[data-result-month]').textContent = isMonthLong(userDate());
 });
 
 const shortWeekEl = document.querySelector('[data-short-week]');
 shortWeekEl.addEventListener('click', () => {
-    const userDate = new Date(document.querySelector('[data-input]').value);
-    shortWeekEl.after(shortestWeekDaysNumber(userDate));
+    document.querySelector('[data-result-short-week]').textContent = shortestWeekDaysNumber(userDate());
 });
 
 const fullWeekEl = document.querySelector('[data-full-weeks]');
 fullWeekEl.addEventListener('click', () => {
-    const userDate = new Date(document.querySelector('[data-input]').value);
-    fullWeekEl.after(fullWeeksNumberInMonth(userDate));
+    document.querySelector('[data-result-full-weeks]').textContent = fullWeeksNumberInMonth(userDate());
 });
--- homeworks/maksym.selenkov_MaksymSelenkov/4-modules/time.js
+++ homeworks/maksym.selenkov_MaksymSelenkov/4-modules/time.js
@@ -1,26 +1,18 @@
 export function getMondaysOfMonth(date) {
     const mondaysArray = [];
-    const days = new Date(date.getFullYear(), date.getMonth() + 1, 0);
-    for (let i = 1; i <= days.getDate(); i++) {
+    const days = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+    for (let i = 1; i <= days; i++) {
         const day = new Date(date.getFullYear(), date.getMonth(), i);
         if (day.getDay() === 1) {
             mondaysArray.push(day.getDate());
         }
     }
-    const result = document.createElement('div');
-    result.textContent = mondaysArray;
-    return result;
+    return mondaysArray;
 }
 
 export function isMonthLong(date) {
     const longMonths = [0, 2, 4, 6, 7, 9, 11];
-    const result = document.createElement('div');
-    if (longMonths.includes(date.getMonth())) {
-        result.textContent = 'true';
-    } else {
-        result.textContent = 'false';
-    }
-    return result;
+    return longMonths.includes(date.getMonth());
 }
 
 export function shortestWeekDaysNumber(date) {
@@ -29,11 +21,19 @@ export function shortestWeekDaysNumber(date) {
     console.log(lastDay);
     const firstWeek = 7 - firstDay;
     const lastWeek = lastDay + 1;
-    const result = document.createElement('div');
-    result.textContent = firstWeek > lastWeek ? lastWeek : firstWeek;
-    return result;
+    return firstWeek > lastWeek ? lastWeek : firstWeek;
 }
 
 export function fullWeeksNumberInMonth(date) {
-    return date;
+    let counter = 0;
+    const days = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+    for (let i = 1; i <= days; i++) {
+        const firstWeekDay = new Date(date.getFullYear(), date.getMonth(), i).getDay();
+        const lastWeekDay = new Date(date.getFullYear(), date.getMonth(), i + 6).getDay();
+        const remainder = days - i;
+        if (remainder >= 7 && firstWeekDay === 0 && lastWeekDay === 6) {
+            counter += 1;
+        }
+    }
+    return counter;
 }
