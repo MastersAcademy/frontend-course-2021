e239ee7408716a58367b53a1d9bab960a3b05bb0
igordordev <88446424+igordordev@users.noreply.github.com>
2021-11-13 18:31:49 +0200

    added 4-modules hometask

---
+++ homeworks/igor.doroshenko_igordordev/4-modules/script.js
@@ -0,0 +1,66 @@
+/*jslint es6:true*/
+
+import {
+    getFridaysOfMonth,
+    isMonthLong,
+    shortestWeekDaysNumber,
+    fullWeeksNumberInMonth,
+    subtractHours,
+    addHours
+} from '/time.js';
+
+const dateInputEl = document.querySelector('[month-information__input]');
+const buttonFridaysEl = document.querySelector('[months-fridays-button]');
+const resultFridaysEl = document.querySelector('[months-fridays-result]');
+const buttonMonthLongEl = document.querySelector('[month-long-button]');
+const resultMonthLongEl = document.querySelector('[month-long-result]');
+const buttonShortestWeekEl = document.querySelector('[shortest-week-button]');
+const resultShortestWeekEl = document.querySelector('[shortest-week-result]');
+const buttonFullWeeksEl = document.querySelector('[full-weeks-button]');
+const resultFullWeeksEl = document.querySelector('[full-weeks-result]');
+const countryListEl = document.querySelector('[country-list]');
+const currentTimeEl = document.querySelector('[current-time]');
+const worldTimeEl = document.querySelector('[world-time__result]');
+const currentDate = new Date(Date());
+
+currentTimeEl.textContent = subtractHours(currentDate, 0);
+worldTimeEl.textContent = subtractHours(currentDate, 0);
+
+buttonFridaysEl.addEventListener('click', () => {
+    resultFridaysEl.textContent = getFridaysOfMonth(dateInputEl);
+});
+
+buttonMonthLongEl.addEventListener('click', () => {
+    resultMonthLongEl.textContent = isMonthLong(dateInputEl);
+});
+
+buttonShortestWeekEl.addEventListener('click', () => {
+    resultShortestWeekEl.textContent = shortestWeekDaysNumber(dateInputEl);
+});
+
+buttonFullWeeksEl.addEventListener('click', () => {
+    resultFullWeeksEl.textContent = fullWeeksNumberInMonth(dateInputEl);
+});
+
+setInterval(function liveClock() {
+    const currentHours = ('0' + currentDate.getHours()).substr(-2);
+    const currentMinutes = ('0' + currentDate.getMinutes()).substr(-2);
+
+    currentTimeEl.textContent = currentHours + ':' + currentMinutes;
+
+    //worldTimeEl.textContent = subtractHours(currentDate, 5);
+
+    countryListEl.addEventListener('change', function () {
+        if (countryListEl.value === 'current') {
+            worldTimeEl.textContent = subtractHours(currentDate, 0);
+        } else if (countryListEl.value === 'tokyo') {
+            worldTimeEl.textContent = addHours(currentDate, 7);
+        } else if (countryListEl.value === 'london') {
+            worldTimeEl.textContent = subtractHours(currentDate, 2);
+        } else if (countryListEl.value === 'new-york') {
+            worldTimeEl.textContent = subtractHours(currentDate, 7);
+        }
+    });
+});
+
+setInterval(liveClock(), 1000);
---
+++ homeworks/igor.doroshenko_igordordev/4-modules/time.js
@@ -0,0 +1,86 @@
+/*jslint es6:true*/
+
+function getFridaysOfMonth(date) {
+    const inputedDate = new Date(date.value);
+    const inputedMonth = inputedDate.getMonth();
+    const fridays = [];
+    inputedDate.setDate(1);
+
+    while (inputedDate.getDay() !== 5) {
+        inputedDate.setDate(inputedDate.getDate() + 1);
+    }
+
+    while (inputedDate.getMonth() === inputedMonth) {
+        fridays.push(inputedDate.getDate());
+        inputedDate.setDate(inputedDate.getDate() + 7);
+    }
+
+    return fridays;
+}
+
+function isMonthLong(date) {
+    const inputedDate = new Date(date.value);
+    const inputedMonth = inputedDate.getMonth();
+    const inputedYear = inputedDate.getFullYear();
+    const daysInMonth = new Date(inputedYear, inputedMonth + 1, 0).getDate();
+
+    return daysInMonth > 30;
+}
+
+function shortestWeekDaysNumber(date) {
+    const inputedDate = new Date(date.value);
+    const inputedMonth = inputedDate.getMonth();
+    const inputedYear = inputedDate.getFullYear();
+    const firstDay = new Date(inputedYear, inputedMonth, 1).getDay();
+    const lastDay = new Date(inputedYear, inputedMonth + 1, 0).getDay();
+    const daysInFirstWeek = (firstDay > 0) ? 8 - firstDay : 1;
+    const daysInLastWeek = (lastDay > 0) ? lastDay : 7;
+
+    return (daysInFirstWeek <= daysInLastWeek) ? daysInFirstWeek : daysInLastWeek;
+}
+
+function fullWeeksNumberInMonth(date) {
+    const inputedDate = new Date(date.value);
+    console.log('work! ' + inputedDate);
+    const inputedMonth = inputedDate.getMonth();
+    const inputedYear = inputedDate.getFullYear();
+    const daysInMonth = new Date(inputedYear, inputedMonth + 1, 0).getDate();
+    const firstDay = new Date(inputedYear, inputedMonth, 1).getDay();
+    const lastDay = new Date(inputedYear, inputedMonth + 1, 0).getDay();
+    const daysInFirstWeek = (firstDay === 0) ? 1 : 8 - firstDay;
+    console.log('daysInFirstWeek! ' + daysInFirstWeek);
+    console.log('lastDay! ' + lastDay);
+    console.log('daysInMonth! ' + daysInMonth);
+
+    const fullWeeks = (daysInFirstWeek < 7) ? (daysInMonth - lastDay - daysInFirstWeek) / 7 : (daysInMonth - lastDay) / 7;
+    return fullWeeks;
+}
+
+function subtractHours(date, hours) {
+    const originalDate = new Date(date);
+    const timeZoneHours = originalDate.getHours() - hours;
+    const currentDate = new Date(originalDate.setHours(timeZoneHours));
+    const printHours = ('0' + currentDate.getHours()).substr(-2);
+    const printMinutes = ('0' + currentDate.getMinutes()).substr(-2);
+
+    return printHours + ':' + printMinutes;
+}
+
+function addHours(date, hours) {
+    const originalDate = new Date(date);
+    const timeZoneHours = originalDate.getHours() + hours;
+    const currentDate = new Date(originalDate.setHours(timeZoneHours));
+    const printHours = ('0' + currentDate.getHours()).substr(-2);
+    const printMinutes = ('0' + currentDate.getMinutes()).substr(-2);
+
+    return printHours + ':' + printMinutes;
+}
+
+export {
+    getFridaysOfMonth,
+    isMonthLong,
+    shortestWeekDaysNumber,
+    fullWeeksNumberInMonth,
+    subtractHours,
+    addHours
+};
