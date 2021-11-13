8a26064456840811f2e23661b228768c825f2147
VladislavMedvedyev <vlad.medvedyev@gmail.com>
2021-11-13 15:57:02 +0200

    Task end

--- homeworks/vladyslav.medvediev_VladislavMedvedyev/4-modules/time.js
+++ homeworks/vladyslav.medvediev_VladislavMedvedyev/4-modules/time.js
@@ -1,32 +1,65 @@
-/**
- * @param date - date string of any supported format
- * @returns array of Friday dates in a month the date from
- */
 export function getMondaysOfMonth() {
-    return [1, 8, 15, 22, 29];
+    const selectedYear = document.querySelector('[data-date__input]').value.slice(0, 4);
+    const selectedMonth = document.querySelector('[data-date__input]').value.slice(5, 7);
+    const getMondaysOutputEl = document.querySelector('[data-get-mondays__output]');
+    const d = new Date(selectedYear, selectedMonth, 0);
+    const month = d.getMonth();
+    const mondaysArr = [];
+    d.setDate(1);
+    while (d.getDay() !== 1) {
+        d.setDate(d.getDate() + 1);
+    }
+    while (d.getMonth() === month) {
+        mondaysArr.push(new Date(d.getTime()).toDateString());
+        d.setDate(d.getDate() + 7);
+    }
+    const monday = mondaysArr.toString();
+    const mondays = monday.match(/\b\d{2}\b/g);
+    getMondaysOutputEl.textContent = `[${mondays}]`;
 }
-
-/**
- * @param date - date string of any supported format
- * @returns {boolean} true if month of a date has 31 day, otherwise returns false
- */
 export function isMonthLong() {
-    return false;
+    const selectedYear = document.querySelector('[data-date__input]').value.slice(0, 4);
+    const selectedMonth = document.querySelector('[data-date__input]').value.slice(5, 7);
+    const monthLongOutputEl = document.querySelector('[data-month-long__output]');
+    const numberDaysInMonth = new Date(selectedYear, selectedMonth, 0).getDate();
+    function ifNumberDays() {
+        if (numberDaysInMonth < 31) {
+            return false;
+        }
+        return true;
+    }
+    monthLongOutputEl.textContent = ifNumberDays();
 }
-
-/**
- * @param date - date string of any supported format
- * @returns {number} number of days in a shortest week of the date month
- */
 export function shortestWeekDaysNumber() {
-    return 3;
+    const selectedYear = document.querySelector('[data-date__input]').value.slice(0, 4);
+    const selectedMonth = document.querySelector('[data-date__input]').value.slice(5, 7);
+    const shortestWeekDaysOutputEl = document.querySelector('[data-shortest-week-days__output]');
+    const firstDayinMonth = new Date(selectedYear, selectedMonth - 1, 1).getDay();
+    const lastDayinMonth = new Date(selectedYear, selectedMonth, 0).getDay();
+    const daysToFullWeeks = 7 - firstDayinMonth;
+    const daysAfterFullWeeks = lastDayinMonth + 1;
+    if (daysToFullWeeks < daysAfterFullWeeks) {
+        shortestWeekDaysOutputEl.textContent = daysToFullWeeks;
+    } else {
+        shortestWeekDaysOutputEl.textContent = daysAfterFullWeeks;
+    }
 }
-
-/**
- * @param date - date string of any supported format
- * @returns {number} number of full weeks in the date month.
- * To be full, week should start and end in the same month
- */
 export function fullWeeksNumberInMonth() {
-    return 3;
+    const selectedYear = document.querySelector('[data-date__input]').value.slice(0, 4);
+    const selectedMonth = document.querySelector('[data-date__input]').value.slice(5, 7);
+    const fullWeeksOutputEl = document.querySelector('[data-full-weeks__output]');
+    const numberDaysInMonth = new Date(selectedYear, selectedMonth, 0).getDate();
+    const firstDayinMonth = new Date(selectedYear, selectedMonth - 1, 1).getDay();
+    const lastDayinMonth = new Date(selectedYear, selectedMonth, 0).getDay();
+    const daysToFullWeeks = 7 - firstDayinMonth;
+    const daysAfterFullWeeks = lastDayinMonth + 1;
+    if (lastDayinMonth < 6 && daysToFullWeeks < 7) {
+        const fullWeeksNumber = (numberDaysInMonth - (daysToFullWeeks + daysAfterFullWeeks))
+            / 7;
+        fullWeeksOutputEl.textContent = fullWeeksNumber;
+    } else {
+        const fullWeeksNumber = (numberDaysInMonth - (daysToFullWeeks + daysAfterFullWeeks))
+            / 7 + 1;
+        fullWeeksOutputEl.textContent = fullWeeksNumber;
+    }
 }
