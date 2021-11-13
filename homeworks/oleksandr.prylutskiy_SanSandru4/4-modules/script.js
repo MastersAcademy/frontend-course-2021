c76c73626e59865aed19e1880b0e8db40a02141a
Oleksandr Prylutskiy <fleks1986@gmail.com>
2021-11-13 18:20:53 +0200

    fixed validation input data

--- homeworks/oleksandr.prylutskiy_SanSandru4/4-modules/script.js
+++ homeworks/oleksandr.prylutskiy_SanSandru4/4-modules/script.js
@@ -3,27 +3,27 @@ import {
 } from './time.js';
 
 const numDate = document.querySelector('[data-take-calendar]');
-const getMondays = document.querySelector('[data-get-monday]');
-const outMondays = document.querySelector('[data-out-monday]');
-const longMonth = document.querySelector('[data-long-month]');
-const outLongMonth = document.querySelector('[data-out-long-month]');
-const shortWeek = document.querySelector('[data-shortest-week]');
-const outShortWeek = document.querySelector('[data-short-week]');
-const fullWeeks = document.querySelector('[data-full-weeks]');
-const outFullWeeks = document.querySelector('[data-out-full-weeks]');
+const getMondaysEl = document.querySelector('[data-get-monday]');
+const outMondaysEl = document.querySelector('[data-out-monday]');
+const longMonthEl = document.querySelector('[data-long-month]');
+const outLongMonthEl = document.querySelector('[data-out-long-month]');
+const shortWeekEl = document.querySelector('[data-shortest-week]');
+const outShortWeekEl = document.querySelector('[data-short-week]');
+const fullWeeksEl = document.querySelector('[data-full-weeks]');
+const outFullWeeksEl = document.querySelector('[data-out-full-weeks]');
 
-getMondays.addEventListener('click', () => {
-    outMondays.textContent = getMondaysOfMonth(numDate);
+getMondaysEl.addEventListener('click', () => {
+    outMondaysEl.textContent = getMondaysOfMonth(numDate);
 });
 
-longMonth.addEventListener('click', () => {
-    outLongMonth.textContent = isMonthLong(numDate);
+longMonthEl.addEventListener('click', () => {
+    outLongMonthEl.textContent = isMonthLong(numDate);
 });
 
-shortWeek.addEventListener('click', () => {
-    outShortWeek.textContent = shortestWeekDaysNumber(numDate);
+shortWeekEl.addEventListener('click', () => {
+    outShortWeekEl.textContent = shortestWeekDaysNumber(numDate);
 });
 
-fullWeeks.addEventListener('click', () => {
-    outFullWeeks.textContent = fullWeeksNumberInMonth(numDate);
+fullWeeksEl.addEventListener('click', () => {
+    outFullWeeksEl.textContent = fullWeeksNumberInMonth(numDate);
 });
--- homeworks/oleksandr.prylutskiy_SanSandru4/4-modules/time.js
+++ homeworks/oleksandr.prylutskiy_SanSandru4/4-modules/time.js
@@ -1,85 +1,111 @@
 export function getMondaysOfMonth(date) {
     const inputData = date.value;
-    const inputDataArray = inputData.split('-');
-    const year = inputDataArray[0];
-    const month = +inputDataArray[1];
-    const day = inputDataArray[2];
-    const mondayInMonth = new Date(year, month - 1, day);
     const mondays = [];
-    while (mondayInMonth.getDay() !== 1) {
-        mondayInMonth.setDate(mondayInMonth.getDay() + 1);
+
+    if (inputData === '') {
+        alert('Enter date, please!');
+    } else {
+        const inputDataArray = inputData.split('-');
+        const year = inputDataArray[0];
+        const month = +inputDataArray[1];
+        const day = inputDataArray[2];
+        const dateDay = new Date(year, month - 1, day);
+        while (dateDay.getDay() !== 1) {
+            dateDay.setDate(dateDay.getDay() + 1);
+        }
+        while (dateDay.getMonth() + 1 === month) {
+            mondays.push(new Date(dateDay.getTime()).getDate());
+            dateDay.setDate(dateDay.getDate() + 7);
+        }
     }
-    while (mondayInMonth.getMonth() + 1 === month) {
-        mondays.push(new Date(mondayInMonth.getTime()).getDate());
-        mondayInMonth.setDate(mondayInMonth.getDate() + 7);
-    }
+
     return mondays;
 }
 
 export function isMonthLong(date) {
     const inputData = date.value;
-    const inputDataArray = inputData.split('-');
-    const year = inputDataArray[0];
-    const month = inputDataArray[1];
-    const lengthMonth = new Date(year, month, 0);
-    const longMonth = lengthMonth.getDate();
-    if (longMonth === 31) {
-        return true;
-    } return false;
+    let result = '';
+
+    if (inputData === '') {
+        alert('Enter date, please!');
+    } else {
+        const inputDataArray = inputData.split('-');
+        const year = inputDataArray[0];
+        const month = inputDataArray[1];
+        const lengthMonth = new Date(year, month, 0);
+        const longMonth = lengthMonth.getDate();
+        if (longMonth === 31) {
+            result = 'true';
+        } else result = 'false';
+    }
+
+    return result;
 }
 
 export function shortestWeekDaysNumber(date) {
     const inputData = date.value;
-    const inputDataArray = inputData.split('-');
-    const year = inputDataArray[0];
-    const month = inputDataArray[1];
-    const firstDayMonth = new Date(year, month - 1, 1);
-    const lastDayMonth = new Date(year, month, 0);
     let shortWeek;
     let firstWeek = 0;
     let lastWeek = 1;
-    while (firstDayMonth.getDay() !== 1) {
-        firstDayMonth.setDate(firstDayMonth.getDate() + 1);
-        firstWeek++;
+
+    if (inputData === '') {
+        alert('Enter date, please!');
+    } else {
+        const inputDataArray = inputData.split('-');
+        const year = inputDataArray[0];
+        const month = inputDataArray[1];
+        const firstDayMonth = new Date(year, month - 1, 1);
+        const lastDayMonth = new Date(year, month, 0);
+        while (firstDayMonth.getDay() !== 1) {
+            firstDayMonth.setDate(firstDayMonth.getDate() + 1);
+            firstWeek++;
+        }
+        while (lastDayMonth.getDay() !== 1) {
+            lastDayMonth.setDate(lastDayMonth.getDate() - 1);
+            lastWeek++;
+        }
+        if (lastWeek < firstWeek || firstWeek === 0) {
+            shortWeek = lastWeek;
+        } else shortWeek = firstWeek;
     }
-    while (lastDayMonth.getDay() !== 1) {
-        lastDayMonth.setDate(lastDayMonth.getDate() - 1);
-        lastWeek++;
-    }
-    if (lastWeek < firstWeek) {
-        shortWeek = lastWeek;
-    } else if (firstWeek === 0) {
-        shortWeek = lastWeek;
-    } else shortWeek = firstWeek;
 
     return shortWeek;
 }
 
 export function fullWeeksNumberInMonth(date) {
     const inputData = date.value;
-    const inputDataArray = inputData.split('-');
-    const year = inputDataArray[0];
-    const month = inputDataArray[1];
-    const firstDayMonth = new Date(year, month - 1, 1);
-    const lastDayMonth = new Date(year, month, 0);
-    const daysInMonth = lastDayMonth.getDate();
     let numberDay;
     let firstWeek = 0;
     let lastWeek = 1;
-    while (firstDayMonth.getDay() !== 1) {
-        firstDayMonth.setDate(firstDayMonth.getDate() + 1);
-        firstWeek++;
+    let resultNumWeeks;
+
+    if (inputData === '') {
+        alert('Enter date, please!');
+    } else {
+        const inputDataArray = inputData.split('-');
+        const year = inputDataArray[0];
+        const month = inputDataArray[1];
+        const firstDayMonth = new Date(year, month - 1, 1);
+        const lastDayMonth = new Date(year, month, 0);
+        const daysInMonth = lastDayMonth.getDate();
+
+        while (firstDayMonth.getDay() !== 1) {
+            firstDayMonth.setDate(firstDayMonth.getDate() + 1);
+            firstWeek++;
+        }
+        while (lastDayMonth.getDay() !== 1) {
+            lastDayMonth.setDate(lastDayMonth.getDate() - 1);
+            lastWeek++;
+        }
+
+        if (firstWeek < 7 && lastWeek < 7) {
+            numberDay = daysInMonth - (firstWeek + lastWeek);
+        } else if (firstWeek < 7 && lastWeek === 7) {
+            numberDay = daysInMonth - firstWeek;
+        } else if (firstWeek === 7 && lastWeek < 7) {
+            numberDay = daysInMonth - lastWeek;
+        }
+        resultNumWeeks = Math.ceil(numberDay / 7);
     }
-    while (lastDayMonth.getDay() !== 1) {
-        lastDayMonth.setDate(lastDayMonth.getDate() - 1);
-        lastWeek++;
-    }
-    if (firstWeek < 7 && lastWeek < 7) {
-        numberDay = daysInMonth - (firstWeek + lastWeek);
-    } else if (firstWeek < 7 && lastWeek === 7) {
-        numberDay = daysInMonth - firstWeek;
-    } else if (firstWeek === 7 && lastWeek < 7) {
-        numberDay = daysInMonth - lastWeek;
-    }
-    return Math.ceil(numberDay / 7);
+    return resultNumWeeks;
 }
