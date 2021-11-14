fa1f39b9b5151ff36cce5bcad703332456630d26
ConstantinRusenko <krusenko@gmail.com>
2021-11-14 13:51:47 +0200

    add code style fixes

--- homeworks/konstantin.rusenko_ConstantinRusenko/4-modules/index.js
+++ homeworks/konstantin.rusenko_ConstantinRusenko/4-modules/index.js
@@ -1,25 +1,25 @@
 import * as time from './time.js';
 
-const mondaysButton = document.querySelector('[data-get-mondays]');
-const longMonthButton = document.querySelector('[data-month-length]');
-const shortestWeekButton = document.querySelector('[data-shortest-week]');
-const fullWeekButton = document.querySelector('[data-full-weeks]');
+const mondaysButtonEl = document.querySelector('[data-get-mondays]');
+const longMonthButtonEl = document.querySelector('[data-month-length]');
+const shortestWeekButtonEl = document.querySelector('[data-shortest-week]');
+const fullWeekButtonEl = document.querySelector('[data-full-weeks]');
 const inputEl = document.querySelector('input');
 const selectEl = document.querySelector('[data-select]');
 
-longMonthButton.addEventListener('click', () => {
+longMonthButtonEl.addEventListener('click', () => {
     document.querySelector('[data-length]').innerHTML = `${time.isMonthLong(inputEl.value)}`;
 });
 
-mondaysButton.addEventListener('click', () => {
+mondaysButtonEl.addEventListener('click', () => {
     document.querySelector('[data-monday]').innerHTML = `${(time.getMondaysOfMonth(inputEl.value))}`;
 });
 
-shortestWeekButton.addEventListener('click', () => {
+shortestWeekButtonEl.addEventListener('click', () => {
     document.querySelector('[data-shortest]').innerHTML = `${time.shortestWeekDaysNumber(inputEl.value)}`;
 });
 
-fullWeekButton.addEventListener('click', () => {
+fullWeekButtonEl.addEventListener('click', () => {
     document.querySelector('[data-full]').innerHTML = `${time.fullWeeksNumberInMonth(inputEl.value)}`;
 });
 
