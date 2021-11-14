1eb45e8882bdb3a8b95e21d5bd0b02dfa7303b2a
Roma <romgofast@gmail.com>
2021-11-14 13:09:41 +0200

    fix

--- homeworks/roman.kushnirenko_skyprog/4-modules/js.js
+++ homeworks/roman.kushnirenko_skyprog/4-modules/js.js
@@ -1,37 +1,35 @@
-import { getMondays } from './time.js';
+import { numberOfDays, getFullWeeksMonth, cityTime } from './time.js';
 
 const inputDate = document.querySelector('.input');
-const currentTime = document.querySelector('[data-current-time]');
 const mondaysBtn = document.querySelector('[data-mondays-btn]');
-export const mondaysShow = document.querySelector('[data-mondays]');
+const mondaysShow = document.querySelector('[data-mondays]');
 const monthlongBtn = document.querySelector('[data-monthlong-btn]');
-const monthLong = document.querySelector('[data-monthlong]');
 const fullWeeksBtn = document.querySelector('[data-fullweeks-btn]');
-const fullWeeksShow = document.querySelector('[data-fullweeks]');
 const selectCityTime = document.querySelector('[data-select-localtime]');
-
-export let input;
+const currentTime = document.querySelector('[data-current-time]');
+let input;
 let dateEntered;
 let mm;
-let dd;
 let year;
 
-function cityTime(city) {
-    let localTime = new Date().toLocaleString('en-US', { timeZone: `${city}` });
+const currentOclock = setInterval(() => {
+    let localTime = new Date().toLocaleString('en-US', { timeZone: 'europe/kiev' });
     localTime = new Date(localTime);
-    let showHour = localTime.getHours();
-    let showMinut = localTime.getMinutes();
-    let showSeconds = localTime.getSeconds();
-    currentTime.innerHTML = showHour.toLocaleString() + ' : ' + showMinut.toLocaleString() + ' : ' + showSeconds.toLocaleString()
-}
+    const showHour = localTime.getHours();
+    const showMinut = localTime.getMinutes();
+    const showSeconds = localTime.getSeconds();
+    currentTime.innerHTML = `${showHour.toLocaleString()} : ${showMinut.toLocaleString()} : ${showSeconds.toLocaleString()}`;
+}, 0);
 
 selectCityTime.addEventListener('change', function () {
-    let idIntervals = 0;
     if (this.value === 'ukraine') {
+        clearInterval(currentOclock);
         setInterval(cityTime, 0, 'europe/kiev');
     } else if (this.value === 'tokyo') {
+        clearInterval(currentOclock);
         setInterval(cityTime, 0, 'asia/tokyo');
     } else if (this.value === 'new-york') {
+        clearInterval(currentOclock);
         setInterval(cityTime, 0, 'America/New_York');
     }
 });
@@ -41,104 +39,32 @@ inputDate.addEventListener('change', (e) => {
     input = inputDate.value;
     dateEntered = new Date(input);
     mm = dateEntered.getMonth();
-    dd = dateEntered.getDate();
     year = dateEntered.getFullYear();
 });
 
-mondaysBtn.addEventListener('click', (e) => {
-    e.preventDefault();
-
+mondaysBtn.addEventListener('click', () => {
+    function getMondays() {
+        const d = new Date(input);
+        const month = d.getMonth();
+        const mondays = [];
+        d.setDate(1);
+        while (d.getDay() !== 1) {
+            d.setDate(d.getDate() + 1);
+        }
+        while (d.getMonth() === month) {
+            mondays.push(new Date(d.getTime()));
+            d.setDate(d.getDate() + 7);
+        }
+        const dateMondays = mondays.map((item) => item.getDate());
+        mondaysShow.textContent = dateMondays;
+    }
     getMondays(dateEntered, mm);
 });
 
 monthlongBtn.addEventListener('click', () => {
-    function numberOfDays(iMonth, iYear) {
-        let myDate = new Date(iYear, iMonth + 1, 1);
-        let newDate = new Date(myDate - 1);
-        let trueOrFalse;
-        if (newDate.getDate() === 31) {
-            trueOrFalse = true;
-        } else {
-            trueOrFalse = false;
-        }
-        return monthLong.textContent = trueOrFalse;
-    }
     numberOfDays(mm, year);
 });
 
 fullWeeksBtn.addEventListener('click', () => {
-    function getFullWeeksStartAndEndInMonth(month, year, _start) {
-        let weeks = [],
-            firstDate = new Date(year, month, 1),
-            lastDate = new Date(year, month + 1, 0),
-            numDays = lastDate.getDate()
-
-        let start = 1;
-        let end = 7 - firstDate.getDay();
-        if (firstDate.getDay() === _start) {
-            end = 7;
-        } else {
-            preMonthEndDay = new Date(year, month, 0);
-            start = preMonthEndDay.getDate() + 1 - firstDate.getDay() + _start;
-            end = 7 - firstDate.getDay() + _start;
-            weeks.push({ start: start, end: end });
-            start = end + 1;
-            end = end + 7
-        }
-        while (start <= numDays) {
-            weeks.push({ start: start, end: end });
-            start = end + 1;
-            end = end + 7;
-            if (end > numDays) {
-                end = end - numDays + _start;
-                weeks.push({ start: start, end: end });
-                break;
-            }
-        }
-        let fullWeeks = 0;
-        weeks.forEach((item) => {
-            if (item.end - item.start === 6) {
-                fullWeeks += 1;
-            }
-        });
-        return fullWeeksShow.textContent = fullWeeks
-    }
-    getFullWeeksStartAndEndInMonth(mm, year, 1);
+    getFullWeeksMonth(mm, year, 1);
 });
-
-// function shortestWeekDaysNumber(month, year, _start) {
-//     let weeks = [],
-//         firstDate = new Date(year, month, 1),
-//         lastDate = new Date(year, month + 1, 0),
-//         numDays = lastDate.getDate()
-
-//     let start = 1
-//     let end = 7 - firstDate.getDay()
-//     if (firstDate.getDay() === _start) {
-//         end = 7
-//     } else {
-//         preMonthEndDay = new Date(year, month, 0)
-//         start = preMonthEndDay.getDate() + 1 - firstDate.getDay() + _start
-//         end = 7 - firstDate.getDay() + _start
-//         weeks.push({ start: start, end: end })
-//         start = end + 1
-//         end = end + 7
-//     }
-//     while (start <= numDays) {
-//         weeks.push({ start: start, end: end })
-//         start = end + 1
-//         end = end + 7
-//         if (end > numDays) {
-//             end = end - numDays + _start
-//             weeks.push({ start: start, end: end })
-//             break;
-//         }
-//     }
-//     let shortestWeekDays = [];
-//     weeks.forEach((item) => {
-//         if (item.end - item.start < 6) {
-//             shortestWeekDays.push(item.end % item.start);
-//         }
-//     });
-// }
-// shortestWeekDaysNumber(11, 2021, 1);
--- homeworks/roman.kushnirenko_skyprog/4-modules/time.js
+++ homeworks/roman.kushnirenko_skyprog/4-modules/time.js
@@ -1,19 +1,64 @@
-import {input, mondaysShow} from './js.js';
+const currentTime = document.querySelector('[data-current-time]');
+const monthLong = document.querySelector('[data-monthlong]');
+const fullWeeksShow = document.querySelector('[data-fullweeks]');
 
-export function getMondays(dateEnter, mon) {
-        let d = new Date(input),
-        month = d.getMonth(),
-        mondays = [];
-    d.setDate(1);
-    while (d.getDay() !== 1) {
-        d.setDate(d.getDate() + 1);
+export function cityTime(city) {
+    let localTime = new Date().toLocaleString('en-US', { timeZone: `${city}` });
+    localTime = new Date(localTime);
+    const showHour = localTime.getHours();
+    const showMinut = localTime.getMinutes();
+    const showSeconds = localTime.getSeconds();
+    currentTime.innerHTML = `${showHour.toLocaleString()} : ${showMinut.toLocaleString()} : ${showSeconds.toLocaleString()}`;
+}
+
+export function numberOfDays(iMonth, iYear) {
+    const myDate = new Date(iYear, iMonth + 1, 1);
+    const newDate = new Date(myDate - 1);
+    let trueOrFalse;
+    if (newDate.getDate() === 31) {
+        trueOrFalse = true;
+    } else {
+        trueOrFalse = false;
     }
-    while (d.getMonth() === month) {
-        mondays.push(new Date(d.getTime()));
-        d.setDate(d.getDate() + 7);
+    monthLong.textContent = trueOrFalse;
+}
+
+export function getFullWeeksMonth(month, years, _start) {
+    const weeks = [];
+    const firstDate = new Date(years, month, 1);
+    const lastDate = new Date(years, month + 1, 0);
+    const numDays = lastDate.getDate();
+
+    let start = 1;
+    let end = 7 - firstDate.getDay();
+    if (firstDate.getDay() === _start) {
+        end = 7;
+    } else {
+        const preMonthEndDay = new Date(years, month, 0);
+        start = preMonthEndDay.getDate() + 1 - firstDate.getDay() + _start;
+        end = 7 - firstDate.getDay() + _start;
+        weeks.push({ startD: start, endD: end });
+        start = end + 1;
+        end += 7;
     }
-    let dateMondays = mondays.map((item) => {
-        return item.getDate();
+    while (start <= numDays) {
+        weeks.push({
+            startD: start,
+            endD: end,
+        });
+        start = end + 1;
+        end += 7;
+        if (end > numDays) {
+            end = end - numDays + _start;
+            weeks.push({ startD: start, endD: end });
+            break;
+        }
+    }
+    let fullWeeks = 0;
+    weeks.forEach((item) => {
+        if (item.endD - item.startD === 6) {
+            fullWeeks += 1;
+        }
     });
-    mondaysShow.textContent = dateMondays;
+    fullWeeksShow.textContent = fullWeeks;
 }
