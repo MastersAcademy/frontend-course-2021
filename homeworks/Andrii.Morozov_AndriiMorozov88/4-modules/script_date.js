6e234fe6a1b25d283202d61669bc5569ffe6abae
Andrii Morozov <slon88888@ukr.net>
2021-11-15 19:22:33 +0200

    added without decorations

--- homeworks/Andrii.Morozov_AndriiMorozov88/4-modules/script_date.js
+++ homeworks/Andrii.Morozov_AndriiMorozov88/4-modules/script_date.js
@@ -1,11 +1,17 @@
 import {
-    receiveDayDates,
-    getLongMonth,
-    getShortestWeek,
-    getFullWeek,
+    getMondaysOfMonth,
+    isMonthLong,
+    shortestWeekDaysNumber,
+    fullWeeksNumberInMonth,
 } from './time.js';
 
-receiveDayDates();
-getLongMonth();
-getShortestWeek();
-getFullWeek();
+const calendar = document.querySelector('[data-date]');
+calendar.addEventListener('change', () => {
+    const date = new Date(calendar.value);
+    const monthOnly = date.getMonth();
+    const yearOnly = date.getFullYear();
+    console.log(getMondaysOfMonth(monthOnly, yearOnly));
+    console.log(isMonthLong(monthOnly));
+    console.log(shortestWeekDaysNumber(monthOnly, yearOnly));
+    console.log(fullWeeksNumberInMonth(monthOnly, yearOnly));
+});
--- homeworks/Andrii.Morozov_AndriiMorozov88/4-modules/time.js
+++ homeworks/Andrii.Morozov_AndriiMorozov88/4-modules/time.js
@@ -1,127 +1,83 @@
-const calendar = document.querySelector('[data-date]');
-export function receiveDayDates() {
-    const monthDays = document.querySelector('[data-day-dates]');
-    monthDays.style.display = 'flex';
-    monthDays.style.height = '30px';
-    const long = [0, 2, 4, 6, 7, 9, 11];
-    const short = [3, 5, 8, 10];
-    calendar.addEventListener('change', () => {
-        monthDays.innerText = '';
-        const date = new Date(calendar.value);
-        const weekDay = date.getDay();
-        const monthOnly = date.getMonth();
-        const yearOnly = date.getFullYear();
-        let lastDate;
-        if (long.includes(monthOnly)) {
-            lastDate = 32;
-        } else if (short.includes(monthOnly)) {
-            lastDate = 31;
-        } else {
-            lastDate = 29;
+const long = [0, 2, 4, 6, 7, 9, 11];
+const short = [3, 5, 8, 10];
+export function getMondaysOfMonth(month, year) {
+    let lastDate;
+    if (long.includes(month)) {
+        lastDate = 32;
+    } else if (short.includes(month)) {
+        lastDate = 31;
+    } else {
+        lastDate = 29;
+    }
+    const mondayArray = [];
+    let k = 0;
+    for (let i = 1; i < lastDate; i++) {
+        const checkDay = new Date(year, month, i);
+        if (checkDay.getDay() === 1) {
+            mondayArray[k] = i;
+            k += 1;
         }
-        for (let i = 1; i < lastDate; i++) {
-            const checkDay = new Date(yearOnly, monthOnly, i);
-            if (checkDay.getDay() === weekDay) {
-                const dates = document.createElement('div');
-                monthDays.append(dates);
-                dates.style.margin = '5px';
-                dates.innerText = i;
-            }
-        }
-    });
+    }
+    return mondayArray;
 }
-export function getLongMonth() {
-    const longMonthOutput = document.querySelector('[data-long-month]');
-    longMonthOutput.style.height = '30px';
-    const long = [0, 2, 4, 6, 7, 9, 11];
-    const btn = document.querySelector('[data-long]');
-    btn.addEventListener('click', () => {
-        const date = new Date(calendar.value);
-        const monthOnly = date.getMonth();
-        longMonthOutput.style.margin = '5px';
-        longMonthOutput.innerHTML = long.includes(monthOnly);
-    });
+export function isMonthLong(month) {
+    return long.includes(month);
 }
-export function getShortestWeek() {
-    const long = [0, 2, 4, 6, 7, 9, 11];
-    const short = [3, 5, 8, 10];
-    const buttonShortWeek = document.querySelector('[data-btn-short-week]');
-    const resultShortWeek = document.querySelector('[data-short-week]');
-    resultShortWeek.style.height = '30px';
-    calendar.addEventListener('change', () => {
-        const date = new Date(calendar.value);
-        const monthOnly = date.getMonth();
-        const yearOnly = date.getFullYear();
-        const firstDate = new Date(yearOnly, monthOnly, 1);
-        let lastDay;
-        if (long.includes(monthOnly)) {
-            const lastDate = new Date(yearOnly, monthOnly, 31);
-            lastDay = lastDate.getDay();
-        } else if (short.includes(monthOnly)) {
-            const lastDate = new Date(yearOnly, monthOnly, 30);
-            lastDay = lastDate.getDay();
-        } else {
-            const lastDate = new Date(yearOnly, monthOnly, 28);
-            lastDay = lastDate.getDay();
-        }
+export function shortestWeekDaysNumber(month, year) {
+    const firstDate = new Date(year, month, 1);
+    let lastDay;
+    if (long.includes(month)) {
+        const lastDate = new Date(year, month, 31);
+        lastDay = lastDate.getDay();
+    } else if (short.includes(month)) {
+        const lastDate = new Date(year, month, 30);
+        lastDay = lastDate.getDay();
+    } else {
+        const lastDate = new Date(year, month, 28);
+        lastDay = lastDate.getDay();
+    }
 
-        if (lastDay === 0) {
-            lastDay = 7;
+    if (lastDay === 0) {
+        lastDay = 7;
+    }
+    let firstDay = firstDate.getDay();
+    if (firstDay === 0) {
+        firstDay = 7;
+    }
+    const amountFirstDays = 8 - firstDay;
+    let shortWeekDays;
+    if (lastDay !== 0) {
+        if (amountFirstDays > lastDay) {
+            shortWeekDays = lastDay;
+        } else if (amountFirstDays < lastDay) {
+            shortWeekDays = amountFirstDays;
+        } else {
+            shortWeekDays = lastDay;
         }
-        let firstDay = firstDate.getDay();
-        if (firstDay === 0) {
-            firstDay = 7;
-        }
-        const amountFirstDays = 8 - firstDay;
-        let shortWeekDays;
-        if (lastDay !== 0) {
-            if (amountFirstDays > lastDay) {
-                shortWeekDays = lastDay;
-            } else if (amountFirstDays < lastDay) {
-                shortWeekDays = amountFirstDays;
-            } else {
-                shortWeekDays = (lastDay);
-            }
-        }
-        buttonShortWeek.addEventListener('click', () => {
-            resultShortWeek.style.margin = '5px';
-            resultShortWeek.innerHTML = shortWeekDays;
-        });
-    });
+    }
+    return shortWeekDays;
 }
-export function getFullWeek() {
-    const long = [0, 2, 4, 6, 7, 9, 11];
-    const short = [3, 5, 8, 10];
-    const buttonFullWeek = document.querySelector('[data-btn-full-week]');
-    const resultFullWeek = document.querySelector('[data-full-week]');
-    calendar.addEventListener('change', () => {
-        const date = new Date(calendar.value);
-        const monthOnly = date.getMonth();
-        const yearOnly = date.getFullYear();
-        const firstDate = new Date(yearOnly, monthOnly, 1);
-        let weekAmount = 3;
-        if (firstDate.getDay() === 1) {
+export function fullWeeksNumberInMonth(month, year) {
+    const firstDate = new Date(year, month, 1);
+    let weekAmount = 3;
+    if (firstDate.getDay() === 1) {
+        weekAmount = 4;
+    }
+    if (long.includes(month)) {
+        const lastDate = new Date(year, month, 31);
+        if (lastDate.getDay() < 3) {
             weekAmount = 4;
         }
-        if (long.includes(monthOnly)) {
-            const lastDate = new Date(yearOnly, monthOnly, 31);
-            if (lastDate.getDay() < 3) {
-                weekAmount = 4;
-            }
-        } else if (short.includes(monthOnly)) {
-            const lastDate = new Date(yearOnly, monthOnly, 30);
-            if (lastDate.getDay() < 2) {
-                weekAmount = 4;
-            }
-        } else if (monthOnly === 1) {
-            const lastDate = new Date(yearOnly, monthOnly, 28);
-            if (lastDate.getDay() === 0) {
-                weekAmount = 4;
-            }
+    } else if (short.includes(month)) {
+        const lastDate = new Date(year, month, 30);
+        if (lastDate.getDay() < 2) {
+            weekAmount = 4;
         }
-        buttonFullWeek.addEventListener('click', () => {
-            resultFullWeek.style.margin = '5px';
-            resultFullWeek.innerHTML = weekAmount;
-        });
-    });
+    } else if (month === 1) {
+        const lastDate = new Date(year, month, 28);
+        if (lastDate.getDay() === 0) {
+            weekAmount = 4;
+        }
+    }
+    return weekAmount;
 }
