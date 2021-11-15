f223f7cd32219e99803bd5ef3d02ae4c0d7dd9c8
Koss <nikkarchinskiyp@gmail.com>
2021-11-15 19:17:57 +0200

    penultimate сщььше

--- homeworks/nikita.karchinskiy_Nikkarchinskiy/4-moduls/index.js
+++ homeworks/nikita.karchinskiy_Nikkarchinskiy/4-moduls/index.js
@@ -1,1 +1,7 @@
-// На выходных сделаю, не успевал с работой(
+// import { daysInMonthFnc, mondaysInMonth } from './exports.js';
+
+// const inputData = document.querySelector('[data-data]');
+// const monthIs = document.querySelector('[data-mounth-long-answer]');
+// const monthMondays = document.querySelector('[data-mondays-answer]');
+// const shortWeeks = document.querySelector('[data-shortest-week-answer]');
+// const getMondeys = document.querySelector('[data-monday-btn]');
