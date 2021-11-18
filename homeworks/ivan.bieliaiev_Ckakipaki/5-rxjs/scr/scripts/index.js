7acab33c264c5bbdee9a983abddd496a7a892fa3
Chakipaki <belyaeve2@icloud.com>
2021-11-18 21:26:35 +0200

    version 1

--- homeworks/ivan.bieliaiev_Ckakipaki/5-rxjs/scr/scripts/index.js
+++ homeworks/ivan.bieliaiev_Ckakipaki/5-rxjs/scr/scripts/index.js
@@ -1,67 +1,36 @@
 const { fromEvent } = window.rxjs;
 const { scan, filter } = window.rxjs.operators;
 
-const headerEl = document.querySelector('.header');
-const btnElCopy = document.createElement('button');
-btnElCopy.classList.add('main__button-scroll-');
-btnElCopy.innerText = 'buy me';
+const headerEl = document.querySelector(('[data-header-container]'));
+const burgerEl = document.querySelector('[data-burger-icon]');
+const navContainerEl = document.querySelector('[data-navigation]');
+// const mainBtnEl = document.querySelector('[data-main-button]');
 
-const burgerEl = document.querySelector('[data-burger-menu]');
-const menuEl = document.querySelector('.menu__links');
+let headerHeight = 80;
 
 burgerEl.addEventListener('click', () => {
     if (headerEl.classList.length === 1) {
-        headerEl.classList.add('header-burger');
-        menuEl.style.display = 'flex';
-        menuEl.style.flexDirection = 'column';
+        headerHeight = 220;
+        headerEl.classList.add('header-responsive');
+        navContainerEl.classList.add('header__menu-responsive');
     } else {
-        headerEl.classList.remove('header-burger');
-        menuEl.style.display = 'none';
-        menuEl.style.flexDirection = 'row';
+        headerHeight = 80;
+        headerEl.classList.remove('header-responsive');
+        navContainerEl.classList.remove('header__menu-responsive');
     }
 });
 
-// const btnEl = document.querySelector('.main__button');
-// // Test visibility button
-// function visible(element) {
-//     if (element.offsetWidth === 0 || element.offsetHeight === 0) return false;
-//     const height = document.documentElement.clientHeight;
-//     const rects = element.getClientRects();
-//     const onTop = function (r) {
-//         const x = (r.left + r.right) / 2;
-//         const y = (r.top + r.bottom) / 2;
-//         return document.elementFromPoint(x, y) === element;
-//     };
-//     for (let i = 0, l = rects.length; i < l; i++) {
-//         const r = rects[i];
-//         const inViewport = r.top > 0 ? r.top <= height : (r.bottom > 0 && r.bottom <= height);
-//         if (inViewport && onTop(r)) return true;
-//     }
-//     return false;
-// }
-//
-// const scrollingButton$ = fromEvent(document, 'scroll')
-//     .pipe(
-//         scan(() => (visible(btnEl))),
-//     );
-//
-// scrollingButton$.subscribe((option) => {
-//     if (option === true) {
-//         headerEl.appendChild(btnElCopy);
-//     }
-// });
-
-// Scrolling
 const scrolling$ = fromEvent(document, 'scroll')
     .pipe(
-        scan(() => (window.scrollY)),
-        filter((posY) => posY > 60),
+        scan(() => window.scrollY),
+        filter((posY) => posY > headerHeight),
         scan((acc, val) => acc.concat(val), []),
     );
-scrolling$.subscribe((item) => {
-    if (item[item.length - 2] - item[item.length - 1] > 0) {
-        headerEl.style.display = 'flex';
-    } else if (item[item.length - 2] - item[item.length - 1] < 0) {
-        headerEl.style.display = 'none';
+
+scrolling$.subscribe((posArr) => {
+    if (posArr[posArr.length - 1] - posArr[posArr.length - 2] > 0) {
+        headerEl.classList.add('header-hide');
+    } else if (posArr[posArr.length - 1] - posArr[posArr.length - 2] < 0) {
+        headerEl.classList.remove('header-hide');
     }
 });
