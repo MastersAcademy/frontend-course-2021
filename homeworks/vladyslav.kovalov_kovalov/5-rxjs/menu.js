d56b01e288a0a9e570f1a4fc2cf7aa3509356e66
kovalov <vlad.kovalov.92@gmail.com>
2021-11-18 14:26:32 +0200

    updated rxjs logic

--- homeworks/vladyslav.kovalov_kovalov/5-rxjs/menu.js
+++ homeworks/vladyslav.kovalov_kovalov/5-rxjs/menu.js
@@ -1,4 +1,5 @@
 const { fromEvent } = window.rxjs;
+const { map, filter } = window.rxjs.operators;
 
 export class Menu {
     constructor(header, headerLogo, burgerButton, navigationMenu) {
@@ -6,24 +7,22 @@ export class Menu {
         this.headerLogo = headerLogo;
         this.burgerButton = burgerButton;
         this.navigationMenu = navigationMenu;
-        this.px = 500;
+        this.px = 50;
         this.toggleDropdownMenu();
         this.toggleHeader();
     }
 
     toggleDropdownMenu() {
         const source = fromEvent(this.burgerButton, 'click');
-        source.subscribe(() => this.navigationMenu.classList.toggle('display-none'));
+        source.subscribe(() => this.navigationMenu.classList.toggle('opened-menu'));
     }
 
     toggleHeader() {
         const source = fromEvent(window, 'scroll');
-        source.subscribe(() => this.isScrolled(this.px));
-    }
-
-    isScrolled(px) {
-        const position = window.scrollY;
-        if (position > px) this.header.classList.add('display-none');
-        if (position === 0) this.header.classList.remove('display-none');
+        const header = source.pipe(
+            map(() => window.scrollY),
+            filter((value) => value < 200),
+        );
+        header.subscribe((value) => { this.header.style.top = `-${value}px`; });
     }
 }
