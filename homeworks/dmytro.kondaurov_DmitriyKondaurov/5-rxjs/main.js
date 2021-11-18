d688e93c62d43b32f60e6c4a96c8d1331b28f5f3
Dmytro Kondaurov <invisibl@ukr.net>
2021-11-19 01:20:20 +0200

    hide header logic done

--- homeworks/dmytro.kondaurov_DmitriyKondaurov/5-rxjs/main.js
+++ homeworks/dmytro.kondaurov_DmitriyKondaurov/5-rxjs/main.js
@@ -1,2 +1,26 @@
-// const { fromEvent } = window.rxjs;
-// const { map } = window.rxjs.operators;
+const { fromEvent } = window.rxjs;
+const { map, filter, distinctUntilChanged } = window.rxjs.operators;
+let curPos = 0;
+let directionUp;
+
+const scroll = fromEvent(document, 'scroll');
+const positions = scroll.pipe(
+    filter(() => {
+        if (window.scrollY - curPos > 50) {
+            curPos = window.scrollY;
+            directionUp = false;
+            return true;
+        } if (window.scrollY - curPos < -50) {
+            curPos = window.scrollY;
+            directionUp = true;
+            return true;
+        }
+        return false;
+    }),
+    map(() => directionUp),
+    distinctUntilChanged(),
+);
+
+positions.subscribe(() => {
+    document.querySelector('[data-header-visible]').classList.toggle('header_hide');
+});
