5c9aa7516eb8fc5529aa5ea20f6b867067878c1a
Oleksandr Prylutskiy <fleks1986@gmail.com>
2021-11-20 14:18:43 +0200

    Scroll is ready

--- homeworks/oleksandr.prylutskiy_SanSandru4/5-rxjs/script.js
+++ homeworks/oleksandr.prylutskiy_SanSandru4/5-rxjs/script.js
@@ -1,27 +1,40 @@
 const gambMenuEl = document.querySelector('[data-min-menu]');
 const headerEl = document.querySelector('[data-attr-header]');
+const burgerIcon = document.querySelector('[data-attr-bars]');
+const closeIcon = document.querySelector('[data-attr-times]');
 
 const { fromEvent } = window.rxjs;
-const { map, filter } = window.rxjs.operators;
+const {
+    map, filter, pairwise,
+} = window.rxjs.operators;
+
+closeIcon.style.display = 'none';
 
 gambMenuEl.addEventListener('click', () => {
-    const x = document.querySelector('[data-nav-menu]');
-    const bars = document.querySelector('[data-attr-bars]');
-    const times = document.querySelector('[data-attr-times]');
-    if (x.className === 'header__menu') {
-        x.className += ' responsive';
-        bars.style.display = 'none';
-        times.style.display = 'block';
+    const listMenu = document.querySelector('[data-nav-menu]');
+    if (listMenu.classList.contains('header__menu')) {
+        listMenu.classList.remove('header__menu');
+        listMenu.classList.add('header__menu--opened');
+        burgerIcon.style.display = 'none';
+        closeIcon.style.display = 'block';
     } else {
-        x.className = 'header__menu';
-        bars.style.display = 'block';
-        times.style.display = 'none';
+        listMenu.classList.add('header__menu');
+        listMenu.classList.remove('header__menu--opened');
+        burgerIcon.style.display = 'block';
+        closeIcon.style.display = 'none';
     }
 });
 
 fromEvent(window, 'scroll').pipe(
     map(() => window.pageYOffset),
-    filter((value) => (value > 50) || (value < window.pageYOffset)),
-    // pairwise(window.pageYOffset[0] - window.pageYOffset[1]),
+    pairwise(),
+    filter((value) => (value[0] - value[1] > 50) || (value[1] - value[0] > 50)),
+    map((value) => value[1] > value[0]),
 )
-    .subscribe(() => { headerEl.classList.toggle('header--hidden'); });
+    .subscribe((value) => {
+        if (value === true) {
+            headerEl.classList.add('header--hidden');
+        } else {
+            headerEl.classList.remove('header--hidden');
+        }
+    });
