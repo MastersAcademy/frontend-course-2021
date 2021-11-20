0459c07bc9af334d853d41f22b2d99b4bf38cf3c
Vadim Fedonyuk <mr.fedonyuk777@gmail.com>
2021-11-20 18:47:52 +0200

    fix burger menu

--- homeworks/vadim.fedonyuk_Vadim1997/5-rxjs/app.js
+++ homeworks/vadim.fedonyuk_Vadim1997/5-rxjs/app.js
@@ -9,17 +9,20 @@ const {
 const headerEl = document.querySelector('[data-header]');
 const buttonBurgerEl = document.querySelector('[data-burger-button]');
 const menuEl = document.querySelector('[data-navigation-menu]');
-buttonBurgerEl.addEventListener('click', () => {
-    menuEl.classList.toggle('menu--opened');
-    buttonBurgerEl.classList.toggle('active');
-});
+
 fromEvent(window, 'scroll')
     .pipe(
         throttleTime(200),
-        map(() => window.pageYOffset),
+        map(() => window.scrollY),
         pairwise(),
-        map((scroll) => scroll[1] - scroll[0]),
+        map(([previousScroll, currentScroll]) => currentScroll - previousScroll),
         filter((scroll) => Math.abs(scroll) >= 50),
         map((scroll) => scroll < 0),
     )
     .subscribe((scroll) => headerEl.classList.toggle('hidden-header', !scroll));
+
+fromEvent(buttonBurgerEl, 'click')
+    .subscribe(() => {
+        menuEl.classList.toggle('menu--opened');
+        buttonBurgerEl.classList.toggle('active');
+    });
