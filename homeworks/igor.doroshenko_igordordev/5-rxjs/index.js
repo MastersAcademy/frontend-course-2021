9ee78a3347627e37c8660d56f76e5d40127a7094
igordordev <88446424+igordordev@users.noreply.github.com>
2021-11-21 00:59:00 +0200

    fixed lint errors

--- homeworks/igor.doroshenko_igordordev/5-rxjs/index.js
+++ homeworks/igor.doroshenko_igordordev/5-rxjs/index.js
@@ -5,15 +5,15 @@ const {
     pairwise,
 } = window.rxjs.operators;
 
-const pageHeaderEl = document.querySelector("[header]");
-const headerNavigationEl = document.querySelector("[header__navigation]");
-const navigationBurgerEl = document.querySelector("[header__navigation-burger]");
-const click$ = rxjs.fromEvent(navigationBurgerEl, 'click');
-const scroll$ = rxjs.fromEvent(document, 'scroll');
+const pageHeaderEl = document.querySelector('[header]');
+const headerNavigationEl = document.querySelector('[header__navigation]');
+const navigationBurgerEl = document.querySelector('[header__navigation-burger]');
+const click$ = fromEvent(navigationBurgerEl, 'click');
+const scroll$ = fromEvent(document, 'scroll');
 
 click$.subscribe(() => {
-    headerNavigationEl.classList.toggle("header__navigation--active");
-    navigationBurgerEl.classList.toggle("header__navigation-burger--active");
+    headerNavigationEl.classList.toggle('header__navigation--active');
+    navigationBurgerEl.classList.toggle('header__navigation-burger--active');
 });
 
 scroll$.pipe(
@@ -21,7 +21,11 @@ scroll$.pipe(
     pairwise(),
     filter(([lastScroll, currentScroll]) => {
         let pixelDifference;
-        (lastScroll > currentScroll) ? pixelDifference = lastScroll - currentScroll : pixelDifference = currentScroll - lastScroll;
+        if (lastScroll > currentScroll) {
+            pixelDifference = lastScroll - currentScroll;
+        } else {
+            pixelDifference = currentScroll - lastScroll;
+        }
         return pixelDifference > 50;
     }),
     map(([lastScroll, currentScroll]) => lastScroll < currentScroll),
