0b03504c9b39aae1a2b805fa183ad7ff0b0065f0
Kostya <qa.kost@gmail.com>
2021-11-21 23:35:16 +0200

    add new classes, code and accomodate classes to bem

--- homeworks/kostya.bublyk_ConstBagel/5-rxjs/scripts/script.js
+++ homeworks/kostya.bublyk_ConstBagel/5-rxjs/scripts/script.js
@@ -2,16 +2,23 @@ const { fromEvent } = window.rxjs;
 const { map } = window.rxjs.operators;
 
 const pageHeaderEl = document.querySelector('[data-role="page-header-section"]');
+const toggleMenuButtonEl = document.querySelector('[data-role="toggle-menu-button"]');
+const headerMainEl = document.querySelector('[data-role="header-main-section"]');
 const pageNavigationEl = document.querySelector('[data-role="page-navigation-section"]');
 
 const changeHeaderClasses = (addedStyle, removedStyle) => {
     pageHeaderEl.classList.add(addedStyle);
     pageHeaderEl.classList.remove(removedStyle);
-}; 
+};
 
 fromEvent(window, 'scroll').pipe(
     map(() => this.scrollY)
 ).subscribe((position) =>
         position > 50 ? 
-        changeHeaderClasses('header-desappear', 'header-appear') :
-        changeHeaderClasses('header-appear', 'header-desappear'));
+        changeHeaderClasses('page__header--hide', 'page__header--show') :
+        changeHeaderClasses('page__header--show', 'page__header--hide'));
+
+toggleMenuButtonEl.addEventListener('click', () => {
+    headerMainEl.classList.toggle('header__main--open');
+    pageNavigationEl.classList.toggle('header__navigation--show');
+});
