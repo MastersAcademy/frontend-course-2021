4b08f2f65e5067c7282cef68f5bdbbdd7f208eca
Kostya <qa.kost@gmail.com>
2021-11-22 19:47:50 +0200

    fix issues related to comments

--- homeworks/kostya.bublyk_ConstBagel/5-rxjs/scripts/script.js
+++ homeworks/kostya.bublyk_ConstBagel/5-rxjs/scripts/script.js
@@ -1,10 +1,11 @@
 const { fromEvent } = window.rxjs;
-const { map } = window.rxjs.operators;
+const { map, filter } = window.rxjs.operators;
 
 const pageHeaderEl = document.querySelector('[data-role="page-header-section"]');
 const toggleMenuButtonEl = document.querySelector('[data-role="toggle-menu-button"]');
 const headerMainEl = document.querySelector('[data-role="header-main-section"]');
 const pageNavigationEl = document.querySelector('[data-role="page-navigation-section"]');
+let counter = 0;
 
 const changeHeaderClasses = (addedStyle, removedStyle) => {
     pageHeaderEl.classList.add(addedStyle);
@@ -12,8 +13,9 @@ const changeHeaderClasses = (addedStyle, removedStyle) => {
 };
 
 fromEvent(window, 'scroll').pipe(
-    map(() => this.scrollY),
-).subscribe((position) => (position > 50 ? changeHeaderClasses('page__header--hide', 'page__header--show')
+    map(() => ++counter),
+    filter((count) => count % 5 === 0),
+).subscribe(() => (this.scrollY > 50 ? changeHeaderClasses('page__header--hide', 'page__header--show')
     : changeHeaderClasses('page__header--show', 'page__header--hide')));
 
 toggleMenuButtonEl.addEventListener('click', () => {
