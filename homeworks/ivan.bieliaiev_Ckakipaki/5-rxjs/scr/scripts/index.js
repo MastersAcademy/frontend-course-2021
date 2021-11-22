745031384fce5e3f2801c42ab10f1c8752950540
Chakipaki <belyaeve2@icloud.com>
2021-11-22 14:59:18 +0200

    Update index.js

--- homeworks/ivan.bieliaiev_Ckakipaki/5-rxjs/scr/scripts/index.js
+++ homeworks/ivan.bieliaiev_Ckakipaki/5-rxjs/scr/scripts/index.js
@@ -20,21 +20,15 @@ let rectBottom = rect.bottom + windowTop;
 burgerEl.addEventListener('click', () => {
     if (menuEl.style.display === 'none') {
         headerHeight = 220;
-        // Header container height 220 color red
         headerEl.style.height = '220px';
         headerEl.style.background = '#f75f5f';
-        // Menu show
         menuEl.style.display = 'flex';
-        // Main Container > new padding
         mainEl.style.padding = '220px 0';
     } else {
         headerHeight = 80;
-        // Header container default > remove hide
         headerEl.style.background = '#111';
         headerEl.style.height = '80px';
-        // Menu hide
         menuEl.style.display = 'none';
-        // Main container > default
         mainEl.style.padding = '80px 0';
     }
 });
@@ -44,16 +38,12 @@ window.addEventListener('resize', () => {
     rect = mainBtn.getBoundingClientRect();
     rectBottom = rect.bottom + windowTop;
     if (window.innerWidth <= 768) {
-        // Menu hide
         menuEl.style.display = 'none';
     } else {
         headerHeight = 80;
-        // Header container default
         headerEl.style.background = '#111';
         headerEl.style.height = '80px';
-        // Main container > default
         mainEl.style.padding = '80px 0';
-        // Menu show
         menuEl.style.display = 'flex';
     }
 });
@@ -68,55 +58,29 @@ const scrolling$ = fromEvent(document, 'scroll')
 
 scrolling$.subscribe((posArr) => {
     if (headerHeight === 220) {
-        // Header container default
         headerEl.style.background = '#111';
         headerEl.style.height = '80px';
-        // Main container > default
         mainEl.style.padding = '80px 0';
-        // Menu hide
         menuEl.style.display = 'none';
     }
     if (posArr[posArr.length - 1] - posArr[posArr.length - 2] > 0) {
-        // Header container animation hide
         headerEl.classList.add('header-hide');
         headerEl.classList.remove('header-show');
-        // headerEl.style.animation = 'hideHeader .5s 1 linear forwards';
-        // Header default inner hide
         headerDefaultEl.classList.add('header__inner-default-hide');
-        // headerDefaultEl.style.display = 'none';
-        // After bottom scroll bottom
         if (posArr[posArr.length - 1] > rectBottom) {
-            // Header container animation show
             headerEl.classList.remove('header-hide');
             headerEl.classList.add('header-show');
-            // headerEl.style.animation = 'showHeader .5s 1 linear forwards';
-            // Header after button show
-            // headerAfterBtnEl.style.display = 'flex';
             headerAfterBtnEl.classList.add('header__inner-afterBtn-show');
         }
     } else if (posArr[posArr.length - 1] - posArr[posArr.length - 2] < 0) {
-        // Header container animation show
-        // headerEl.classList.remove('header-hide');
         headerEl.classList.remove('header-hide');
         headerEl.classList.add('header-show');
-        // headerEl.style.animation = 'showHeader .5s 1 linear forwards';
-        // Header default inner show
         headerDefaultEl.classList.remove('header__inner-default-hide');
-        // headerDefaultEl.style.display = 'flex';
-        // After bottom scroll top
         if (posArr[posArr.length - 1] > rectBottom) {
-            // Header after button hide
             headerAfterBtnEl.classList.remove('header__inner-afterBtn-show');
-            // headerAfterBtnEl.style.display = 'none';
-            // Button in default header inner show
             headerDefBtnEl.classList.add('header__button-show');
-            // headerDefBtnEl.style.display = 'block';
-            // headerDefBtnEl.style.animation = 'showButton .5s 1 linear forwards';
         } else {
-            // Button in default header inner hide
             headerDefBtnEl.classList.remove('header__button-show');
-            // headerDefBtnEl.style.animation = 'hideButton .5s 1 linear forwards';
-            // headerDefBtnEl.style.display = 'none';
         }
     }
 });
