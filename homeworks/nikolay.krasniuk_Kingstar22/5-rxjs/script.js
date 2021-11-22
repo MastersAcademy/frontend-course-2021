c11e3b1432b06d4fb39a4b08340e9e1476d75e19
Nikolay <nikolaykrasniuk@gmail.com>
2021-11-22 07:53:28 +0200

    completed * task

--- homeworks/nikolay.krasniuk_Kingstar22/5-rxjs/script.js
+++ homeworks/nikolay.krasniuk_Kingstar22/5-rxjs/script.js
@@ -7,6 +7,10 @@ const dynamicMenuEl = document.querySelector('[data-header-menu]');
 const hamburgerEl = document.querySelector('[data-header-burger]');
 const menuEl = document.querySelector('[data-menu]');
 const fixedMenu = document.querySelector('body');
+const LogoEl = document.querySelector('[data-logo]');
+const dynamicLogoEl = document.querySelector('[data-active-logo]');
+const trigerBtnEl = document.querySelector('[data-btn]');
+const dynamicBtnEl = document.querySelector('[data-active-btn]');
 
 const scrollStream$ = fromEvent(window, 'scroll');
 scrollStream$.pipe(
@@ -14,13 +18,25 @@ scrollStream$.pipe(
     map(() => window.pageYOffset),
     pairwise(),
     filter(([prevScroll, currentScroll]) => Math.abs((prevScroll - currentScroll)) > 50),
-    map(([prevScroll, currentScroll]) => prevScroll > currentScroll),
+    map(([prevScroll, currentScroll]) => ({
+        scrollMoveToUp: prevScroll > currentScroll,
+        scrollMoveDownUnderBtn: currentScroll > trigerBtnEl.offsetTop,
+    })),
     distinctUntilChanged(),
 ).subscribe((value) => {
-    if (value) {
+    if (value.scrollMoveToUp && !value.scrollMoveDownUnderBtn) {
         dynamicMenuEl.classList.add('active');
-    } else {
+        dynamicBtnEl.classList.remove('btn-active');
+    } else if (!value.scrollMoveToUp && !value.scrollMoveDownUnderBtn) {
         dynamicMenuEl.classList.remove('active');
+    } else if (!value.scrollMoveToUp && value.scrollMoveDownUnderBtn) {
+        dynamicMenuEl.classList.add('active');
+        dynamicBtnEl.classList.add('btn-active');
+        LogoEl.classList.add('logo-active');
+        dynamicLogoEl.classList.add('logo-scroll-active');
+    } else if (value.scrollMoveToUp && value.scrollMoveDownUnderBtn) {
+        dynamicLogoEl.classList.remove('logo-scroll-active');
+        LogoEl.classList.remove('logo-active');
     }
 });
 
