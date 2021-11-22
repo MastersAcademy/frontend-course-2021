2e27e13ab6ad1e983d578685a6fb7e14f45eaeec
Chakipaki <belyaeve2@icloud.com>
2021-11-22 09:09:35 +0200

    Fixed mobile button and class

--- homeworks/ivan.bieliaiev_Ckakipaki/5-rxjs/scr/scripts/index.js
+++ homeworks/ivan.bieliaiev_Ckakipaki/5-rxjs/scr/scripts/index.js
@@ -5,14 +5,11 @@ const burgerEl = document.querySelector('[data-burger-icon]');
 const menuEl = document.querySelector('[data-navigation]');
 const headerEl = document.querySelector('[data-header]');
 const headerDefBtnEl = document.querySelector('[data-defHeader-button]');
-const headerAfterBtnBtnEl = document.querySelector('[data-afterBtn-button]');
 const mainEl = document.querySelector('[data-main]');
 const mainBtn = document.querySelector('[data-main-button]');
 
 const headerDefaultEl = document.querySelector('[data-header-default]');
 const headerAfterBtnEl = document.querySelector('[data-header-after-button]');
-
-headerAfterBtnBtnEl.style.display = 'block';
 
 let headerHeight = 80;
 
@@ -23,16 +20,22 @@ let rectBottom = rect.bottom + windowTop;
 burgerEl.addEventListener('click', () => {
     if (menuEl.style.display === 'none') {
         headerHeight = 220;
-        menuEl.style.display = 'flex';
+        // Header container height 220 color red
         headerEl.style.height = '220px';
-        mainEl.style.padding = '220px 0';
         headerEl.style.background = '#f75f5f';
+        // Menu show
+        menuEl.style.display = 'flex';
+        // Main Container > new padding
+        mainEl.style.padding = '220px 0';
     } else {
         headerHeight = 80;
+        // Header container default > remove hide
         headerEl.style.background = '#111';
-        mainEl.style.padding = '80px 0';
-        menuEl.style.display = 'none';
         headerEl.style.height = '80px';
+        // Menu hide
+        menuEl.style.display = 'none';
+        // Main container > default
+        mainEl.style.padding = '80px 0';
     }
 });
 
@@ -41,13 +44,17 @@ window.addEventListener('resize', () => {
     rect = mainBtn.getBoundingClientRect();
     rectBottom = rect.bottom + windowTop;
     if (window.innerWidth <= 768) {
+        // Menu hide
         menuEl.style.display = 'none';
     } else {
         headerHeight = 80;
+        // Header container default
         headerEl.style.background = '#111';
-        mainEl.style.padding = '80px 0';
-        menuEl.style.display = 'flex';
         headerEl.style.height = '80px';
+        // Main container > default
+        mainEl.style.padding = '80px 0';
+        // Menu show
+        menuEl.style.display = 'flex';
     }
 });
 
@@ -61,31 +68,55 @@ const scrolling$ = fromEvent(document, 'scroll')
 
 scrolling$.subscribe((posArr) => {
     if (headerHeight === 220) {
+        // Header container default
         headerEl.style.background = '#111';
-        mainEl.style.padding = '80px 0';
-        menuEl.style.display = 'none';
         headerEl.style.height = '80px';
+        // Main container > default
+        mainEl.style.padding = '80px 0';
+        // Menu hide
+        menuEl.style.display = 'none';
     }
     if (posArr[posArr.length - 1] - posArr[posArr.length - 2] > 0) {
-        headerEl.style.animation = 'hideHeader .5s 1 linear forwards';
-        headerDefaultEl.style.display = 'none';
+        // Header container animation hide
+        headerEl.classList.add('header-hide');
+        headerEl.classList.remove('header-show');
+        // headerEl.style.animation = 'hideHeader .5s 1 linear forwards';
+        // Header default inner hide
+        headerDefaultEl.classList.add('header__inner-default-hide');
+        // headerDefaultEl.style.display = 'none';
+        // After bottom scroll bottom
         if (posArr[posArr.length - 1] > rectBottom) {
-            headerEl.style.animation = 'showHeader .5s 1 linear forwards';
-            headerAfterBtnEl.style.display = 'flex';
-            headerDefaultEl.style.display = 'none';
+            // Header container animation show
+            headerEl.classList.remove('header-hide');
+            headerEl.classList.add('header-show');
+            // headerEl.style.animation = 'showHeader .5s 1 linear forwards';
+            // Header after button show
+            // headerAfterBtnEl.style.display = 'flex';
+            headerAfterBtnEl.classList.add('header__inner-afterBtn-show');
         }
     } else if (posArr[posArr.length - 1] - posArr[posArr.length - 2] < 0) {
-        headerEl.style.animation = 'showHeader .5s 1 linear forwards';
-        headerDefaultEl.style.display = 'flex';
+        // Header container animation show
+        // headerEl.classList.remove('header-hide');
+        headerEl.classList.remove('header-hide');
+        headerEl.classList.add('header-show');
+        // headerEl.style.animation = 'showHeader .5s 1 linear forwards';
+        // Header default inner show
+        headerDefaultEl.classList.remove('header__inner-default-hide');
+        // headerDefaultEl.style.display = 'flex';
+        // After bottom scroll top
         if (posArr[posArr.length - 1] > rectBottom) {
-            headerAfterBtnEl.style.display = 'none';
-            headerDefBtnEl.style.display = 'none';
-            headerDefaultEl.style.display = 'flex';
-            headerDefBtnEl.style.display = 'block';
-            headerDefBtnEl.style.animation = 'showButton .5s 1 linear forwards';
+            // Header after button hide
+            headerAfterBtnEl.classList.remove('header__inner-afterBtn-show');
+            // headerAfterBtnEl.style.display = 'none';
+            // Button in default header inner show
+            headerDefBtnEl.classList.add('header__button-show');
+            // headerDefBtnEl.style.display = 'block';
+            // headerDefBtnEl.style.animation = 'showButton .5s 1 linear forwards';
         } else {
-            headerDefBtnEl.style.animation = 'hideButton .5s 1 linear forwards';
-            headerDefBtnEl.style.display = 'none';
+            // Button in default header inner hide
+            headerDefBtnEl.classList.remove('header__button-show');
+            // headerDefBtnEl.style.animation = 'hideButton .5s 1 linear forwards';
+            // headerDefBtnEl.style.display = 'none';
         }
     }
 });
