41b0b8400fe0eca4d743655a70cf17dd4ca8bf90
Kostya <qa.kost@gmail.com>
2021-11-23 00:40:42 +0200

    fix problem with burger and header

--- homeworks/kostya.bublyk_ConstBagel/5-rxjs/scripts/script.js
+++ homeworks/kostya.bublyk_ConstBagel/5-rxjs/scripts/script.js
@@ -1,22 +1,40 @@
 const { fromEvent } = window.rxjs;
-const { map, filter } = window.rxjs.operators;
+const { tap, map, filter } = window.rxjs.operators;
 
 const pageHeaderEl = document.querySelector('[data-role="page-header-section"]');
 const toggleMenuButtonEl = document.querySelector('[data-role="toggle-menu-button"]');
 const headerMainEl = document.querySelector('[data-role="header-main-section"]');
 const pageNavigationEl = document.querySelector('[data-role="page-navigation-section"]');
-let counter = 0;
+let previousPosition = 0;
 
 const changeHeaderClasses = (addedStyle, removedStyle) => {
     pageHeaderEl.classList.add(addedStyle);
     pageHeaderEl.classList.remove(removedStyle);
 };
 
+const comparePoints = (previusPoint, currentPoint) => (currentPoint - previusPoint) > 50 || (previusPoint - currentPoint) > 50;
+
 fromEvent(window, 'scroll').pipe(
-    map(() => ++counter),
-    filter((count) => count % 5 === 0),
-).subscribe(() => (this.scrollY > 50 ? changeHeaderClasses('page__header--hide', 'page__header--show')
-    : changeHeaderClasses('page__header--show', 'page__header--hide')));
+    tap(() => {
+        if (!window.pageYOffset) {
+            changeHeaderClasses('page__header--show', 'page__header--hide');
+        }
+    }),
+    map(() => window.pageYOffset),
+    filter((currentPosition) => comparePoints(previousPosition, currentPosition)),
+).subscribe((position) => {
+    if (!position) {
+        changeHeaderClasses('page__header--show', 'page__header--hide');
+    }
+    else if (!previousPosition) {
+        changeHeaderClasses('page__header--hide', 'page__header--show');
+    }
+    else {
+        pageHeaderEl.classList.toggle('page__header--hide');
+        pageHeaderEl.classList.toggle('page__header--show');
+    }
+    previousPosition = position;
+});
 
 toggleMenuButtonEl.addEventListener('click', () => {
     headerMainEl.classList.toggle('header__main--open');
