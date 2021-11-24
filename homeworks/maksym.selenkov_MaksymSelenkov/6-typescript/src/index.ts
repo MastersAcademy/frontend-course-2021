2e5d1b183e8c2a8cf8e0e5467f9d54b510c69df9
MaksymSelenkov <snegopad100@gmail.com>
2021-11-24 16:17:36 +0200

    almost-complete-task

--- homeworks/maksym.selenkov_MaksymSelenkov/6-typescript/src/index.ts
+++ homeworks/maksym.selenkov_MaksymSelenkov/6-typescript/src/index.ts
@@ -1,11 +1,59 @@
-import * as _ from 'lodash';
+const keyEl = document.querySelector('[data-key]');
+const pointsEl = document.querySelector('[data-points]');
+const bubbleEl: any = document.querySelector('[data-bubble]');
+let points: number = 100;
 
-function component() {
-    const element = document.createElement('div');
 
-    element.innerHTML = _.join(['HeLLo', 'WEBpack'], ' ');
-
-    return element;
+function generateKey(): void {
+    const alphabet: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    keyEl.textContent = alphabet[Math.floor(Math.random() * alphabet.length)];
+    keyEl.classList.add('key__symbol');
 }
 
-document.body.appendChild(component());
+setInterval(generateKey, 2000);
+
+function changeBubble(points: number): void {
+    bubbleEl.style.width = points + 100 + 'px';
+    bubbleEl.style.height = points + 100 + 'px';
+}
+
+document.addEventListener('keydown', (e) => {
+    console.log(e);
+    if(e.key.toUpperCase() === keyEl.textContent) {
+        points += Math.floor(Math.random() * (10 - 5 + 1)) + 5;
+        changeBubble(points);
+        if(points >= 200) {
+            const winEl = document.createTextNode('You win !!!');
+            document.body.append(winEl);
+            // e.preventDefault();
+        }
+        return pointsEl.textContent = points.toString();
+    }
+    if(e.key.toUpperCase() !== keyEl.textContent) {
+        points -= Math.floor(Math.random() * (25 - 20 + 1)) + 20;
+        changeBubble(points)
+        if(points <= 0) {
+            const looseEl = document.createTextNode('You loose :(');
+            document.body.append(looseEl);
+            // e.preventDefault();
+        }
+        return pointsEl.textContent = points.toString();
+    }
+    // if(e.key == undefined) {
+    //     points -= Math.floor(Math.random() * (15 - 10 + 1)) + 10;
+    //     changeBubble(points)
+        // if(points >= 200) {
+        //     const winEl = document.createTextNode('You win !!!');
+        //     document.body.append(winEl);
+        //     e.preventDefault();
+        // }
+    //     if(points <= 0) {
+    //         const looseEl = document.createTextNode('You loose :(');
+    //         document.body.append(looseEl);
+    //             e.preventDefault();
+    //     }
+    //     return pointsEl.textContent = points.toString();
+    // }
+})
+
+
