8937b18a39786c73a98ae1107c80a098e579d219
MaksymSelenkov <snegopad100@gmail.com>
2021-11-26 20:33:00 +0200

    add-noPresseKey

--- homeworks/maksym.selenkov_MaksymSelenkov/6-typescript/src/index.ts
+++ homeworks/maksym.selenkov_MaksymSelenkov/6-typescript/src/index.ts
@@ -6,34 +6,41 @@ let points: number = 100;
 const refreshInterval = setInterval(() => {
     generateKey();
     moveBar();
-}, 2000);
+    document.addEventListener('keydown', playGame);
 
-const absentUser = setInterval(()=> {
-    points -= getRandom(10, 15);
-    changeBubble(points);
-    if(points >= 200) finishGame('You win !!!');
-    if(points <= 0) finishGame('You loose :(');
-}, 1998);
-
-document.addEventListener('keydown', playGame);
-
-function playGame(e: KeyboardEvent): string {
-    const correctKey: boolean = e.key.toUpperCase() === keyEl.textContent;
-    if(correctKey) {
-        clearInterval(absentUser);
-        points += getRandom(5, 10);
+    const noPressedKey = setTimeout(()=> {
+        points -= getRandom(10, 15);
+        pointsEl.textContent = points.toString();
         changeBubble(points);
         if(points >= 200) finishGame('You win !!!');
-        return pointsEl.textContent = points.toString();
-    }
-    if(!correctKey) {
-        clearInterval(absentUser);
-        points -= getRandom(20, 25);
-        changeBubble(points);
         if(points <= 0) finishGame('You loose :(');
-        return pointsEl.textContent = points.toString();
+    }, 1998);
+
+    function playGame(e: KeyboardEvent): string {
+        clearTimeout(noPressedKey);
+        const correctKey: boolean = e.key.toUpperCase() === keyEl.textContent;
+        if(correctKey) {
+            points += getRandom(5, 10);
+            changeBubble(points);
+            if(points >= 200) finishGame('You win !!!');
+            return pointsEl.textContent = points.toString();
+        }
+        if(!correctKey) {
+            points -= getRandom(20, 25);
+            changeBubble(points);
+            if(points <= 0) finishGame('You loose :(');
+            return pointsEl.textContent = points.toString();
+        }
     }
-}
+
+    function finishGame(text: string): void {
+        const finishEl: Text = document.createTextNode(text);
+        document.body.append(finishEl);
+        document.removeEventListener('keydown', playGame);
+        clearInterval(refreshInterval);
+    }
+
+}, 2000);
 
 function generateKey(): void {
     const alphabet: string = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -44,13 +51,6 @@ function generateKey(): void {
 function changeBubble(points: number): void {
     bubbleEl.style.width = points + 100 + 'px';
     bubbleEl.style.height = points + 100 + 'px';
-}
-
-function finishGame(text: string): void {
-    const finishEl: Text = document.createTextNode(text);
-    document.body.append(finishEl);
-    document.removeEventListener('keydown', playGame);
-    clearInterval(refreshInterval);
 }
 
 function moveBar(): void {
@@ -71,3 +71,14 @@ function moveBar(): void {
 function getRandom(min: number, max: number): number {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+
+
+
+
+
+
+
+
+
+
