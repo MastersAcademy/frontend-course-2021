31326bf24214c52cc0572ba79943b1f946686b51
Dmytro Kondaurov <invisibl@ukr.net>
2021-11-26 21:59:15 +0200

    first positive result, full logic done

--- homeworks/dmytro.kondaurov_DmitriyKondaurov/6-typescript/src/index.ts
+++ homeworks/dmytro.kondaurov_DmitriyKondaurov/6-typescript/src/index.ts
@@ -2,6 +2,7 @@ import './styles.css';
 
 const startBtnEl: HTMLElement = document.querySelector('[data-start-btn]');
 const stopBtnEl: HTMLElement = document.querySelector('[data-stop-btn]');
+const restartBtnEl: HTMLElement = document.querySelector('[data-restart-btn]');
 const conditionEl: HTMLElement = document.querySelector('[data-condition-container]');
 const progressBarEl: HTMLProgressElement = document.querySelector('[data-progress-bar]');
 const totalScoreEl: HTMLElement = document.querySelector('[data-total-score]');
@@ -9,11 +10,26 @@ const curPointsEl: HTMLElement = document.querySelector('[data-cur-points]');
 let curPoints: number;
 let intervalId: NodeJS.Timer;
 type letterT = 'A'|'B'|'C'|'D'|'E'|'F'|'G'|'H'|'I'|'J'|'K'|'L'|'M'|'N'|'O'|'P'|'Q'|'R'|'S'|'T'|'U'|'V'|'W'|'X'|'Y'|'Z';
+let keyPressedFlag: boolean = false;
 
 const startGame = (): void => {
     if (!intervalId) {
         intervalId = setInterval(gameFlow, 2000);
     }
+}
+
+const stopGame = (): void => {
+    clearInterval(intervalId);
+    intervalId = null;
+}
+
+const restartGame = (): void => {
+    stopGame();
+    curPoints = 0;
+    keyPressedFlag = false;
+    curPointsEl.textContent = curPoints.toString();
+    totalScoreEl.textContent = '100';
+    startGame();
 }
 
 const randomLetterGen = (): string | letterT => {
@@ -22,35 +38,35 @@ const randomLetterGen = (): string | letterT => {
 }
 
 const checkKeyValue = (key: KeyboardEvent): void => {
-    // document.removeEventListener('keydown', checkKeyValue, false);
+    document.removeEventListener('keydown', checkKeyValue, false);
+    keyPressedFlag = true;
     if (key.key.toUpperCase() === conditionEl.textContent) {
         curPoints = Math.floor(Math.random() * 5) + 5;
         curPointsEl.textContent = curPoints.toString();
         totalScoreEl.textContent = (Number(totalScoreEl.textContent) + curPoints).toString();
     } else {
-        curPoints = Math.floor(Math.random() * - 5) - 10;
+        curPoints = Math.floor(Math.random() * - 20) - 5;
         curPointsEl.textContent = curPoints.toString();
         totalScoreEl.textContent = (Number(totalScoreEl.textContent) + curPoints).toString();
     }
 }
 
-const gameFlow = (): void => {
+const gameFlow = async (): Promise<void> => {
     if (Number(totalScoreEl.textContent) < 200 && Number(totalScoreEl.textContent) > 0) {
         conditionEl.textContent = randomLetterGen();
-        moveProgressBar();
+        keyPressedFlag = false;
         document.addEventListener('keydown', checkKeyValue, false);
+        moveProgressBar();
+        await setTimeout(() => {
+            if (!keyPressedFlag) {
+                curPoints = Math.floor(Math.random() * - 5) - 10;
+                curPointsEl.textContent = curPoints.toString();
+                totalScoreEl.textContent = (Number(totalScoreEl.textContent) + curPoints).toString();
+            }
+        }, 1990);
     } else {
         stopGame();
     }
-}
-
-const keyboardListener = (): boolean => {
-
-    return true;
-}
-const stopGame = (): void => {
-    clearInterval(intervalId);
-    intervalId = null;
 }
 
 
@@ -73,4 +89,5 @@ const moveProgressBar = (): void => {
 }
 
 startBtnEl.addEventListener('click', startGame);
+restartBtnEl.addEventListener('click', restartGame);
 stopBtnEl.addEventListener('click', stopGame);
