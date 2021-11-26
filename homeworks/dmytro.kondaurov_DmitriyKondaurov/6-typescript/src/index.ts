505131663adf0a9cab2230f17e0340dded5d0616
Dmytro Kondaurov <invisibl@ukr.net>
2021-11-26 15:32:07 +0200

    check key value,  cur points and total score logic done

--- homeworks/dmytro.kondaurov_DmitriyKondaurov/6-typescript/src/index.ts
+++ homeworks/dmytro.kondaurov_DmitriyKondaurov/6-typescript/src/index.ts
@@ -4,7 +4,9 @@ const startBtnEl: HTMLElement = document.querySelector('[data-start-btn]');
 const stopBtnEl: HTMLElement = document.querySelector('[data-stop-btn]');
 const conditionEl: HTMLElement = document.querySelector('[data-condition-container]');
 const progressBarEl: HTMLProgressElement = document.querySelector('[data-progress-bar]');
-
+const totalScoreEl: HTMLElement = document.querySelector('[data-total-score]');
+const curPointsEl: HTMLElement = document.querySelector('[data-cur-points]');
+let curPoints: number;
 let intervalId: NodeJS.Timer;
 type letterT = 'A'|'B'|'C'|'D'|'E'|'F'|'G'|'H'|'I'|'J'|'K'|'L'|'M'|'N'|'O'|'P'|'Q'|'R'|'S'|'T'|'U'|'V'|'W'|'X'|'Y'|'Z';
 
@@ -19,13 +21,32 @@ const randomLetterGen = (): string | letterT => {
     return alphabet[Math.floor(Math.random() * alphabet.length)];
 }
 
-const gameFlow = (): void => {
-    conditionEl.textContent = randomLetterGen();
-    moveProgressBar();
+const checkKeyValue = (key: KeyboardEvent): void => {
+    // document.removeEventListener('keydown', checkKeyValue, false);
+    if (key.key.toUpperCase() === conditionEl.textContent) {
+        curPoints = Math.floor(Math.random() * 5) + 5;
+        curPointsEl.textContent = curPoints.toString();
+        totalScoreEl.textContent = (Number(totalScoreEl.textContent) + curPoints).toString();
+    } else {
+        curPoints = Math.floor(Math.random() * - 5) - 10;
+        curPointsEl.textContent = curPoints.toString();
+        totalScoreEl.textContent = (Number(totalScoreEl.textContent) + curPoints).toString();
+    }
 }
 
-const keyboardListener = (inputKey: string): string => {
-    return inputKey;
+const gameFlow = (): void => {
+    if (Number(totalScoreEl.textContent) < 200 && Number(totalScoreEl.textContent) > 0) {
+        conditionEl.textContent = randomLetterGen();
+        moveProgressBar();
+        document.addEventListener('keydown', checkKeyValue, false);
+    } else {
+        stopGame();
+    }
+}
+
+const keyboardListener = (): boolean => {
+
+    return true;
 }
 const stopGame = (): void => {
     clearInterval(intervalId);
