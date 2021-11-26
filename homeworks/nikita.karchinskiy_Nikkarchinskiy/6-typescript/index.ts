eb3c84cd3058d0a3a1b081d65374d4294fe80324
Koss <nikkarchinskiyp@gmail.com>
2021-11-26 19:24:22 +0200

    6-typescript

---
+++ homeworks/nikita.karchinskiy_Nikkarchinskiy/6-typescript/index.ts
@@ -0,0 +1,17 @@
+// const btnResume = document.querySelector<HTMLElement>('[data-start-btn]');
+// const btnPause = document.querySelector<HTMLElement>('[data-stop-btn]');
+// const score = document.querySelector<HTMLElement>('[data-score]');
+// const circle = document.querySelector<HTMLElement>('[data-circle]');
+// const plusScore = document.querySelector<HTMLElement>('[dara-plus-score]');
+// const btnTask = document.querySelector<HTMLElement>('[data-btn-task]');
+// const lineProgres = document.querySelector<HTMLElement>('[data-line-progres]');
+// let letter: string;
+
+// const newLetter = (str: string): string => {
+//     return (str[Math.floor(Math.random() * str.length)]).toLocaleUpperCase();
+// }
+
+// btnResume.addEventListener('click', function() {
+//     console.log(newLetter)
+// });
+console.log(1);
