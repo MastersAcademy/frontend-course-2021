eee55886c7593f9ee42efe0d80d52cb3e9ee51a8
kovalov <vlad.kovalov.92@gmail.com>
2021-12-01 12:43:12 +0200

    added webpack setting & file structure

---
+++ homeworks/vladyslav.kovalov_kovalov/7-oop/dist/bundle.js
@@ -0,0 +1,12 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+
+console.log('hi!');
+
+/******/ })()
+;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiYnVuZGxlLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7QUFBQSxPQUFPLENBQUMsR0FBRyxDQUFDLEtBQUssQ0FBQyIsInNvdXJjZXMiOlsid2VicGFjazovLzYtdHlwZXNjcmlwdC8uL3NyYy9pbmRleC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnaGkhJylcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
