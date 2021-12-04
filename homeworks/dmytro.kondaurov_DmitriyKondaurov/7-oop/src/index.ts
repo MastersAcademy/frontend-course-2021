b754e26610583fa3dd85fd94ff32d1744cc4bc04
Dmytro Kondaurov <invisibl@ukr.net>
2021-12-04 09:22:39 +0200

    render images from Gallery method

--- homeworks/dmytro.kondaurov_DmitriyKondaurov/7-oop/src/index.ts
+++ homeworks/dmytro.kondaurov_DmitriyKondaurov/7-oop/src/index.ts
@@ -6,6 +6,49 @@ const closeImgBtnEl: HTMLElement = document.querySelector('[data-close-image-btn
 const listOfImagesEl: Array<HTMLElement> = Array.from(document.querySelectorAll('[data-img-container]'));
 const template: HTMLElement = document.querySelector('[data-gallery-item-template]');
 const preloaderEl: HTMLElement = document.querySelector('[data-preloader]');
+const imageUriList: Array<string> = [
+    '../src/images/1.jpg',
+    '../src/images/2.jpg',
+    '../src/images/3.jpg',
+    '../src/images/4.jpg',
+    '../src/images/5.jpg',
+    '../src/images/6.jpg',
+]
+
+class ImageGallery {
+    constructor(
+        private imageGalleryContainer: HTMLElement,
+        private uploadImgBtnEl: HTMLElement,
+        private imageUriList: Array<string>,
+        private preloaderEl: HTMLElement,
+        private templateImgEl: HTMLElement,
+    ) {
+
+    }
+    renderImages() {
+        const newImgEl: HTMLElement = (<HTMLTemplateElement>template).content.querySelector('[data-img-container]');
+        this.imageUriList.forEach(imageUri => {
+            const cloneNewImgEl: Node = newImgEl.cloneNode(true);
+            (<HTMLImageElement>(<HTMLElement>cloneNewImgEl).querySelector('[data-gallery-img]')).src = imageUri;
+            this.imageGalleryContainer.append(cloneNewImgEl);
+        })
+    }
+}
+
+class Images {
+    constructor(
+        private imageTemplate: HTMLElement,
+        private closeImgBtnEl: HTMLElement,
+        private imageSrc: Blob | MediaSource,
+        private preloaderEl: HTMLElement,
+    ) {
+
+
+    }
+}
+
+const gallery = new ImageGallery(galleryContainer, uploadImgBtnEl, imageUriList, preloaderEl, template);
+gallery.renderImages();
 
 uploadImgBtnEl.addEventListener('change', async (event: Event ): Promise<void> => {
     preloaderEl.classList.remove('hide');
