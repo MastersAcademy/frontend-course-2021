df03b776f16eb7cb54b6df11076823dc13fc847c
Dmytro Kondaurov <invisibl@ukr.net>
2021-12-04 11:38:10 +0200

    OOP logic done

--- homeworks/dmytro.kondaurov_DmitriyKondaurov/7-oop/src/index.ts
+++ homeworks/dmytro.kondaurov_DmitriyKondaurov/7-oop/src/index.ts
@@ -4,7 +4,7 @@ const galleryContainer: HTMLElement = document.querySelector('[data-gallery-cont
 const uploadImgBtnEl: HTMLElement = document.querySelector('[data-upload-image-btn]');
 const closeImgBtnEl: HTMLElement = document.querySelector('[data-close-image-btn]');
 const listOfImagesEl: Array<HTMLElement> = Array.from(document.querySelectorAll('[data-img-container]'));
-const template: HTMLElement = document.querySelector('[data-gallery-item-template]');
+const template: HTMLTemplateElement = document.querySelector('[data-gallery-item-template]');
 const preloaderEl: HTMLElement = document.querySelector('[data-preloader]');
 const imageUriList: Array<string> = [
     '../src/images/1.jpg',
@@ -16,62 +16,50 @@ const imageUriList: Array<string> = [
 ]
 
 class ImageGallery {
+    private closeImgBtnEl: HTMLElement = document.querySelector('[data-close-image-btn]');
+    private activeImageEl: HTMLElement;
     constructor(
         private imageGalleryContainer: HTMLElement,
         private uploadImgBtnEl: HTMLElement,
         private imageUriList: Array<string>,
         private preloaderEl: HTMLElement,
-        private templateImgEl: HTMLElement,
+        private templateImgEl: HTMLTemplateElement,
     ) {
-
+        this.uploadImgBtnEl.addEventListener('change', async (event: Event ): Promise<void> => {
+            this.preloaderEl.classList.remove('hide');
+            await setTimeout(() => {
+                const newImageSrc: Blob | MediaSource = (<HTMLInputElement>event.target).files[0];
+                const newImgEl: HTMLElement = this.templateImgEl.content.querySelector('[data-img-container]');
+                const cloneNewImgEl: Node = newImgEl.cloneNode(true);
+                (<HTMLImageElement>(<HTMLElement>cloneNewImgEl)
+                    .querySelector('[data-gallery-img]'))
+                    .src = URL.createObjectURL(newImageSrc);
+                (<HTMLImageElement>(<HTMLElement>cloneNewImgEl)).addEventListener('click', (event: MouseEvent) => {
+                    (<HTMLElement>event.currentTarget).classList.add('active');
+                    this.closeImgBtnEl.classList.remove('hide');
+                })
+                this.imageGalleryContainer.append(cloneNewImgEl);
+                this.preloaderEl.classList.add('hide');
+            }, 2000);
+        })
+        this.closeImgBtnEl.addEventListener('click', () => {
+            this.activeImageEl = document.querySelector('.gallery__item.active')
+            this.activeImageEl.classList.remove('active');
+        })
     }
-    renderImages() {
+    build() {
         const newImgEl: HTMLElement = (<HTMLTemplateElement>template).content.querySelector('[data-img-container]');
         this.imageUriList.forEach(imageUri => {
             const cloneNewImgEl: Node = newImgEl.cloneNode(true);
             (<HTMLImageElement>(<HTMLElement>cloneNewImgEl).querySelector('[data-gallery-img]')).src = imageUri;
+            (<HTMLImageElement>(<HTMLElement>cloneNewImgEl)).addEventListener('click', (event: MouseEvent) => {
+                (<HTMLElement>event.currentTarget).classList.add('active');
+                closeImgBtnEl.classList.remove('hide');
+            })
             this.imageGalleryContainer.append(cloneNewImgEl);
         })
     }
 }
 
-class Images {
-    constructor(
-        private imageTemplate: HTMLElement,
-        private closeImgBtnEl: HTMLElement,
-        private imageSrc: Blob | MediaSource,
-        private preloaderEl: HTMLElement,
-    ) {
-
-
-    }
-}
-
 const gallery = new ImageGallery(galleryContainer, uploadImgBtnEl, imageUriList, preloaderEl, template);
-gallery.renderImages();
-
-uploadImgBtnEl.addEventListener('change', async (event: Event ): Promise<void> => {
-    preloaderEl.classList.remove('hide');
-    await setTimeout(() => {
-        const newImageSrc: Blob | MediaSource = (<HTMLInputElement>event.target).files[0];
-        const newImgEl: HTMLElement = (<HTMLTemplateElement>template).content.querySelector('[data-img-container]');
-        const cloneNewImgEl: Node = newImgEl.cloneNode(true);
-        (<HTMLImageElement>(<HTMLElement>cloneNewImgEl)
-            .querySelector('[data-gallery-img]'))
-            .src = URL.createObjectURL(newImageSrc);
-        galleryContainer.append(cloneNewImgEl);
-        preloaderEl.classList.add('hide');
-    }, 2000);
-})
-
-listOfImagesEl.forEach((item: HTMLElement) => {
-    item.addEventListener('click', (event: MouseEvent) => {
-        (<HTMLElement>event.currentTarget).classList.add('active');
-        closeImgBtnEl.classList.remove('hide');
-    })
-});
-
-closeImgBtnEl.addEventListener('click', () => {
-    const activeImageEl: HTMLElement = document.querySelector('.gallery__item.active')
-    activeImageEl.classList.remove('active');
-})
+gallery.build();
