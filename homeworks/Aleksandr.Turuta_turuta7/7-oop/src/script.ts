60ea5cc3783be4ee70cd17501fae331346985814
turuta7 <turuta51@gmail.com>
2021-12-06 10:06:36 +0200

    fix code

--- homeworks/Aleksandr.Turuta_turuta7/7-oop/src/script.ts
+++ homeworks/Aleksandr.Turuta_turuta7/7-oop/src/script.ts
@@ -1,33 +1,33 @@
-class LoaderImage {
-    constructor(private readonly loaderEl: HTMLElement,) { }
-    hidden() {
-        this.loaderEl.classList.remove('display__flex')
+class Hidden {
+    constructor(private readonly dataEl: HTMLElement, private readonly dataClass: string) { }
+    public hidden() {
+        this.dataEl.classList.remove(`${this.dataClass}`)
     }
 
-    visible() {
+    public visible() {
+        this.dataEl.classList.add(`${this.dataClass}`)
+    }
+}
+
+class Base64 {
+    public getBase64(image: File) {
         return new Promise(resolve => {
-            this.loaderEl.classList.add('display__flex')
-            setTimeout(() => {
-                resolve(this.hidden())
-            }, 1000)
+            const reader = new FileReader();
+            reader.readAsDataURL(image);
+            reader.onloadend = () => {
+                resolve(reader.result.toString());
+
+            }
         })
     }
 }
 
 class ImageLibrary {
-
-    private readonly imageEl: HTMLElement;
-    private readonly templateEl: HTMLTemplateElement;
-    private readonly modalRindow: HTMLElement;
-
     constructor(
-        private readonly el: HTMLElement,
-        private readonly tm: HTMLTemplateElement,
-        private readonly mRindow: HTMLElement
+        private readonly imageEl: HTMLElement,
+        private readonly templateEl: HTMLTemplateElement,
+        private readonly modalRindow: HTMLElement
     ) {
-        this.imageEl = el
-        this.templateEl = tm
-        this.modalRindow = mRindow
         this.listenerLoadFile()
     }
 
@@ -38,24 +38,24 @@ class ImageLibrary {
         })
     }
 
-    async getBase64(image: FileList[0]) {
-        const templateImageEl = this.tm.content.cloneNode(true) as HTMLElement
-        const reader = new FileReader();
-        reader.readAsDataURL(image);
-        reader.onloadend = () => {
-            const imageDateEl: HTMLDivElement = templateImageEl.querySelector('[data-photo]')
-            imageDateEl.setAttribute('src', reader.result.toString());
-            this.listenerClickImage(imageDateEl, reader.result.toString())
-        }
+    async getBase64(image: File) {
+        const templateImageEl = this.templateEl.content.cloneNode(true) as HTMLElement
+        const imageDateEl: HTMLDivElement = templateImageEl.querySelector('[data-photo]')
+        loader.visible()
+        setTimeout(() => loader.hidden(), 2000)
+        const getFileBase64 = await base64.getBase64(image)
+        imageDateEl.setAttribute('src', getFileBase64.toString());
+        this.listenerClickImage(templateImageEl.querySelector('[data-button]'), getFileBase64.toString())
     }
 
     listenerClickImage(imageDateEl: HTMLDivElement, render: string) {
         imageDateEl.addEventListener('click', () => {
-            this.modalRindow.classList.add('display__block')
-            new LoaderImage(document.querySelector('[data-loader]')).visible().then(() => {
-                this.modalRindow.querySelector('[data-modal-container]').classList.add('display__flex')
-                this.modalRindow.querySelector('[data-cancel]').classList.add('display__block')
-            });
+            this.modalRindow.classList.add('display__flex')
+            loader.visible()
+            setTimeout(() => loader.hidden(), 2000)
+            this.modalRindow.querySelector('[data-modal-container]').classList.add('display__flex')
+            this.modalRindow.querySelector('[data-cancel-button]').classList.add('display__flex')
+
             this.modalRindow.querySelector('[data-modal-image]').setAttribute('src', render);
             this.listenerClickCancelImage()
         })
@@ -63,13 +63,14 @@ class ImageLibrary {
     }
 
     listenerClickCancelImage() {
-        this.modalRindow.querySelector('[data-cancel]').addEventListener('click', () => {
-            this.modalRindow.classList.remove('display__block')
+        this.modalRindow.querySelector('[data-cancel-button]').addEventListener('click', () => {
+            this.modalRindow.classList.remove('display__flex')
             this.modalRindow.querySelector('[data-modal-container]').classList.remove('display__flex')
-            this.modalRindow.querySelector('[data-cancel]').classList.remove('display__block')
-
+            this.modalRindow.querySelector('[data-cancel-button]').classList.remove('display__flex')
         })
     }
 }
 
+const base64 = new Base64()
+const loader = new Hidden(document.querySelector('[data-loader]'), 'display__flex')
 new ImageLibrary(document.querySelector('[data-main]'), document.querySelector<HTMLTemplateElement>('[data-image-template]'), document.querySelector<HTMLElement>('[data-modal]'))
