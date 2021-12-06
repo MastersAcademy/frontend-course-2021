9e0597564d09ed6221fe6dd1312d24c1071866d8
turuta7 <turuta51@gmail.com>
2021-12-06 18:30:29 +0200

    add method hidden in class ImageLibrary

--- homeworks/Aleksandr.Turuta_turuta7/7-oop/src/script.ts
+++ homeworks/Aleksandr.Turuta_turuta7/7-oop/src/script.ts
@@ -1,11 +1,10 @@
 class Hidden {
-    constructor(private readonly dataEl: HTMLElement, private readonly dataClass: string) { }
-    public hidden() {
-        this.dataEl.classList.remove(`${this.dataClass}`)
+    public hidden(dataEl: HTMLElement, dataClass: string) {
+        dataEl.classList.remove(`${dataClass}`)
     }
 
-    public visible() {
-        this.dataEl.classList.add(`${this.dataClass}`)
+    public visible(dataEl: HTMLElement, dataClass: string) {
+        dataEl.classList.add(`${dataClass}`)
     }
 }
 
@@ -16,7 +15,6 @@ class Base64 {
             reader.readAsDataURL(image);
             reader.onloadend = () => {
                 resolve(reader.result.toString());
-
             }
         })
     }
@@ -26,7 +24,8 @@ class ImageLibrary {
     constructor(
         private readonly imageEl: HTMLElement,
         private readonly templateEl: HTMLTemplateElement,
-        private readonly modalRindow: HTMLElement
+        private readonly modalRindow: HTMLElement,
+        private readonly hidden: Hidden
     ) {
         this.listenerLoadFile()
     }
@@ -41,8 +40,8 @@ class ImageLibrary {
     async getBase64(image: File) {
         const templateImageEl = this.templateEl.content.cloneNode(true) as HTMLElement
         const imageDateEl: HTMLDivElement = templateImageEl.querySelector('[data-photo]')
-        loader.visible()
-        setTimeout(() => loader.hidden(), 2000)
+        this.hidden.visible(document.querySelector('[data-loader]'), 'display__flex')
+        setTimeout(() => this.hidden.hidden(document.querySelector('[data-loader]'), 'display__flex'), 2000)
         const getFileBase64 = await base64.getBase64(image)
         imageDateEl.setAttribute('src', getFileBase64.toString());
         this.listenerClickImage(templateImageEl.querySelector('[data-button]'), getFileBase64.toString())
@@ -51,10 +50,10 @@ class ImageLibrary {
     listenerClickImage(imageDateEl: HTMLDivElement, render: string) {
         imageDateEl.addEventListener('click', () => {
             this.modalRindow.classList.add('display__flex')
-            loader.visible()
-            setTimeout(() => loader.hidden(), 2000)
-            this.modalRindow.querySelector('[data-modal-container]').classList.add('display__flex')
-            this.modalRindow.querySelector('[data-cancel-button]').classList.add('display__flex')
+            this.hidden.visible(document.querySelector('[data-loader]'), 'display__flex')
+            setTimeout(() => this.hidden.hidden(document.querySelector('[data-loader]'), 'display__flex'), 2000)
+            this.hidden.visible(this.modalRindow.querySelector('[data-modal-container]'), 'display__flex')
+            this.hidden.visible(this.modalRindow.querySelector('[data-cancel-button]'), 'display__flex')
 
             this.modalRindow.querySelector('[data-modal-image]').setAttribute('src', render);
             this.listenerClickCancelImage()
@@ -65,12 +64,17 @@ class ImageLibrary {
     listenerClickCancelImage() {
         this.modalRindow.querySelector('[data-cancel-button]').addEventListener('click', () => {
             this.modalRindow.classList.remove('display__flex')
-            this.modalRindow.querySelector('[data-modal-container]').classList.remove('display__flex')
-            this.modalRindow.querySelector('[data-cancel-button]').classList.remove('display__flex')
+            this.hidden.hidden(this.modalRindow.querySelector('[data-modal-container]'), 'display__flex')
+            this.hidden.hidden(this.modalRindow.querySelector('[data-cancel-button]'), 'display__flex')
         })
     }
 }
 
 const base64 = new Base64()
-const loader = new Hidden(document.querySelector('[data-loader]'), 'display__flex')
-new ImageLibrary(document.querySelector('[data-main]'), document.querySelector<HTMLTemplateElement>('[data-image-template]'), document.querySelector<HTMLElement>('[data-modal]'))
+
+new ImageLibrary(
+    document.querySelector('[data-main]'),
+    document.querySelector<HTMLTemplateElement>('[data-image-template]'),
+    document.querySelector<HTMLElement>('[data-modal]'),
+    new Hidden()
+)
