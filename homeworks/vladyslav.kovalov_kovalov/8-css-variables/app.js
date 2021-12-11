84880965f169cdaf13912cad51d82419dc061e1b
kovalov <vlad.kovalov.92@gmail.com>
2021-12-11 19:04:21 +0200

    added theme toggler icon changing

--- homeworks/vladyslav.kovalov_kovalov/8-css-variables/app.js
+++ homeworks/vladyslav.kovalov_kovalov/8-css-variables/app.js
@@ -3,10 +3,12 @@ class ThemeToggler {
         buttonElement,
         pageContainer,
         theme,
+        togglerIcon,
     ) {
         this.buttonElement = buttonElement;
         this.pageContainer = pageContainer;
         this.theme = theme;
+        this.togglerIcon = togglerIcon;
         this.eventListeners();
     }
 
@@ -17,20 +19,32 @@ class ThemeToggler {
     }
 
     getTheme() {
-        console.log(this.theme.dataset.theme);
         if (this.theme.dataset.theme === 'dark') this.setTheme('light');
         else this.setTheme('dark');
     }
 
     setTheme(themeName) {
+        if (themeName === 'dark') this.togglerIcon.setAttribute('xlink:href', 'icons/icons-sprite.svg#theme-toggler-light');
+        else this.togglerIcon.setAttribute('xlink:href', 'icons/icons-sprite.svg#theme-toggler-dark');
+
         this.pageContainer.setAttribute('data-theme', themeName);
     }
 }
 
-const darkTheme = new ThemeToggler(
-    document.querySelector('[data-theme-toggler]'),
-    document.querySelector('[data-page-container]'),
-    document.querySelector('[data-theme]'),
-);
+// new ThemeToggler(
+//     document.querySelector('[data-theme-toggler]'),
+//     document.querySelector('[data-page-container]'),
+//     document.querySelector('[data-theme]'),
+//     document.querySelector('[data-theme-toggler-icon]'),
+// );
 
-darkTheme();
+function initThemeToggler() {
+    return new ThemeToggler(
+        document.querySelector('[data-theme-toggler]'),
+        document.querySelector('[data-page-container]'),
+        document.querySelector('[data-theme]'),
+        document.querySelector('[data-theme-toggler-icon]'),
+    );
+}
+
+initThemeToggler();
