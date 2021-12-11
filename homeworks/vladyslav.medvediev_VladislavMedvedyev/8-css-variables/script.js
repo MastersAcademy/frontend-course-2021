3705b5059379611b250e962a973b060ae70ba248
VladislavMedvedyev <vlad.medvedyev@gmail.com>
2021-12-11 19:27:40 +0200

    End task

---
+++ homeworks/vladyslav.medvediev_VladislavMedvedyev/8-css-variables/script.js
@@ -0,0 +1,25 @@
+const htmlEl = document.querySelector('[data-html]');
+const nightThemeToggleEl = document.querySelector('[data-night-theme]');
+const dayThemeToggleEl = document.querySelector(['[data-day-theme]']);
+
+nightThemeToggleEl.addEventListener('click', () => {
+    htmlEl.style.setProperty('--button-color', 'rgba(253, 126, 119, 1)');
+    htmlEl.style.setProperty('--background-color', 'rgba(13, 13, 13, 1)');
+    htmlEl.style.setProperty('--header-and-footer-text-color', 'rgba(255, 255, 255, 1)');
+    htmlEl.style.setProperty('--text-color', 'rgba(255, 255, 255, 1)');
+    htmlEl.style.setProperty('--background-gradient', 'linear-gradient(180deg, rgba(13, 13, 13, 1) 22.12%, rgba(249, 249, 249, 0) 100%)');
+    htmlEl.style.setProperty('--advantages-svg-stroke-color', 'rgba(255, 255, 255, 1)');
+    nightThemeToggleEl.classList.toggle('header__button-night-theme-hidden');
+    dayThemeToggleEl.classList.toggle('header__button-day-theme-visible');
+});
+
+dayThemeToggleEl.addEventListener('click', () => {
+    htmlEl.style.setProperty('--button-color', 'rgba(253, 102, 94, 1)');
+    htmlEl.style.setProperty('--background-color', 'rgba(255, 255, 255, 1)');
+    htmlEl.style.setProperty('--header-and-footer-text-color', 'rgba(88, 88, 88, 1)');
+    htmlEl.style.setProperty('--text-color', 'rgba(0, 0, 0, 1)');
+    htmlEl.style.setProperty('--background-gradient', 'linear-gradient(180deg, rgba(249, 249, 249, 1) 22.12%, rgba(249, 249, 249, 0) 100%)');
+    htmlEl.style.setProperty('--advantages-svg-stroke-color', 'rgba(0, 0, 0, 1)');
+    nightThemeToggleEl.classList.toggle('header__button-night-theme-hidden');
+    dayThemeToggleEl.classList.toggle('header__button-day-theme-visible');
+});
