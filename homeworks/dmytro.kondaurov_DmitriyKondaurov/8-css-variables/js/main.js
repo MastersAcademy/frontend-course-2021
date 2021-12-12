9af952e439e5e2f989751b1f0ac25604f9c0450f
Dmytro Kondaurov <invisibl@ukr.net>
2021-12-12 10:26:15 +0200

    first realise

--- homeworks/dmytro.kondaurov_DmitriyKondaurov/8-css-variables/js/main.js
+++ homeworks/dmytro.kondaurov_DmitriyKondaurov/8-css-variables/js/main.js
@@ -1,1 +1,15 @@
-// const switcher = 0;
+const switcher = document.querySelector('[data-switch-btn]');
+const styleSrc = document.querySelector('[data-style-src]');
+let themeStatus = styleSrc.getAttribute('href');
+
+switcher.addEventListener('click', () => {
+    if (themeStatus === 'style-day.css') {
+        styleSrc.setAttribute('href', 'style-night.css');
+        switcher.setAttribute('src', 'images/cil_sun.png');
+        themeStatus = false;
+    } else {
+        styleSrc.setAttribute('href', 'style-day.css');
+        switcher.setAttribute('src', 'images/ic_sharp-mode-night.png');
+        themeStatus = 'style-day.css';
+    }
+});
