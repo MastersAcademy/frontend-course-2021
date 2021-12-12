0bfb5c1017856e6d836e98502d8746ff7fe51860
Oleksandr Prylutskiy <fleks1986@gmail.com>
2021-12-12 16:19:27 +0200

    dark and light theme is ready

---
+++ homeworks/oleksandr.prylutskiy_SanSandru4/8-css-variables/script.js
@@ -0,0 +1,24 @@
+const changeSkin = document.querySelector('[data-attr-change-skin]');
+const iconSkinEl = document.querySelector('[data-attr-icon]');
+const bodyEl = document.querySelector('body');
+const currentTheme = localStorage.getItem('skin');
+
+function setTheme(name, icon) {
+    bodyEl.setAttribute('data-skin', name);
+    iconSkinEl.setAttribute('xlink:href', icon);
+    localStorage.setItem('skin', name);
+}
+
+if (currentTheme) {
+    bodyEl.setAttribute('data-skin', currentTheme);
+} else {
+    setTheme('light', '#moon');
+}
+
+changeSkin.addEventListener('click', () => {
+    if (bodyEl.getAttribute('data-skin') === 'light') {
+        setTheme('dark', '#sun');
+    } else {
+        setTheme('light', '#moon');
+    }
+});
