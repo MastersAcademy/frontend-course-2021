343a6b90c6262b67c7c2d3c4ee834d445cc013ce
VladislavMedvedyev <vlad.medvedyev@gmail.com>
2021-12-13 05:41:04 +0200

    Comments fix ver.1

--- homeworks/vladyslav.medvediev_VladislavMedvedyev/8-css-variables/script.js
+++ homeworks/vladyslav.medvediev_VladislavMedvedyev/8-css-variables/script.js
@@ -6,9 +6,8 @@ nightThemeToggleEl.addEventListener('click', () => {
     htmlEl.style.setProperty('--button-color', 'rgba(253, 126, 119, 1)');
     htmlEl.style.setProperty('--background-color', 'rgba(13, 13, 13, 1)');
     htmlEl.style.setProperty('--header-and-footer-text-color', 'rgba(255, 255, 255, 1)');
-    htmlEl.style.setProperty('--text-color', 'rgba(255, 255, 255, 1)');
+    htmlEl.style.setProperty('--text-and-advantages-svg-stroke-color', 'rgba(255, 255, 255, 1)');
     htmlEl.style.setProperty('--background-gradient', 'linear-gradient(180deg, rgba(13, 13, 13, 1) 22.12%, rgba(249, 249, 249, 0) 100%)');
-    htmlEl.style.setProperty('--advantages-svg-stroke-color', 'rgba(255, 255, 255, 1)');
     nightThemeToggleEl.classList.toggle('header__button-night-theme-hidden');
     dayThemeToggleEl.classList.toggle('header__button-day-theme-visible');
 });
@@ -17,9 +16,8 @@ dayThemeToggleEl.addEventListener('click', () => {
     htmlEl.style.setProperty('--button-color', 'rgba(253, 102, 94, 1)');
     htmlEl.style.setProperty('--background-color', 'rgba(255, 255, 255, 1)');
     htmlEl.style.setProperty('--header-and-footer-text-color', 'rgba(88, 88, 88, 1)');
-    htmlEl.style.setProperty('--text-color', 'rgba(0, 0, 0, 1)');
+    htmlEl.style.setProperty('--text-and-advantages-svg-stroke-color', 'rgba(0, 0, 0, 1)');
     htmlEl.style.setProperty('--background-gradient', 'linear-gradient(180deg, rgba(249, 249, 249, 1) 22.12%, rgba(249, 249, 249, 0) 100%)');
-    htmlEl.style.setProperty('--advantages-svg-stroke-color', 'rgba(0, 0, 0, 1)');
     nightThemeToggleEl.classList.toggle('header__button-night-theme-hidden');
     dayThemeToggleEl.classList.toggle('header__button-day-theme-visible');
 });
