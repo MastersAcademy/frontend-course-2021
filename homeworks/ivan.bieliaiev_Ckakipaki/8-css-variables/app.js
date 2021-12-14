65454a83470c93a087e3fc3b4d8c009fb7ddd38c
Chakipaki <belyaeve2@icloud.com>
2021-12-14 22:42:48 +0200

    Add some responsive on blocks

--- homeworks/ivan.bieliaiev_Ckakipaki/8-css-variables/app.js
+++ homeworks/ivan.bieliaiev_Ckakipaki/8-css-variables/app.js
@@ -1,30 +1,26 @@
-const switchEl = document.querySelector('[data-switch]');
-const switchIConEl = switchEl.querySelector('[data-switch-href]');
-let mode = 'light';
+const switchLunaEl = document.querySelector('[data-switch-luna]');
+const switchSunEl = document.querySelector('[data-switch-sun]');
 
 const switchToDark = () => {
-    switchIConEl.href = './src/sprite.svg#sun';
+    switchLunaEl.style.display = 'none';
     document.body.style.setProperty('--color-background', '#111');
     document.body.style.setProperty('--navigation-color', '#FFF');
     document.body.style.setProperty('--color-font', '#FFF');
     document.querySelector('.realize-house').style.background = 'linear-gradient(to top, var(--color-background), var(--color-background), var(--color-font), var(--color-background), var(--color-background))';
-    mode = 'dark';
 };
 
 const switchToWhite = () => {
-    switchIConEl.href = './src/sprite.svg#luna';
+    switchLunaEl.style.display = 'block';
     document.body.style.setProperty('--navigation-color', '#585858');
     document.body.style.setProperty('--color-background', '#FFF');
     document.body.style.setProperty('--color-font', '#111');
     document.querySelector('.realize-house').style.background = '';
-    mode = 'light';
 };
 
-switchEl.addEventListener('click', () => {
-    console.log('click');
-    if (mode === 'light') {
-        switchToDark();
-    } else if (mode === 'dark') {
-        switchToWhite();
-    }
+switchLunaEl.addEventListener('click', () => {
+    switchToDark();
+});
+
+switchSunEl.addEventListener('click', () => {
+    switchToWhite();
 });
