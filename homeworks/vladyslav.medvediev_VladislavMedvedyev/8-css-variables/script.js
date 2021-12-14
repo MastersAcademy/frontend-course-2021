1d6c5828f4bee925202f16c6262ac6c5e0fb06c0
VladislavMedvedyev <vlad.medvedyev@gmail.com>
2021-12-14 02:58:24 +0200

    Comments fix ver.2

--- homeworks/vladyslav.medvediev_VladislavMedvedyev/8-css-variables/script.js
+++ homeworks/vladyslav.medvediev_VladislavMedvedyev/8-css-variables/script.js
@@ -1,23 +1,15 @@
-const htmlEl = document.querySelector('[data-html]');
 const nightThemeToggleEl = document.querySelector('[data-night-theme]');
 const dayThemeToggleEl = document.querySelector(['[data-day-theme]']);
+const bodyEl = document.querySelector('[data-body]');
 
 nightThemeToggleEl.addEventListener('click', () => {
-    htmlEl.style.setProperty('--button-color', 'rgba(253, 126, 119, 1)');
-    htmlEl.style.setProperty('--background-color', 'rgba(13, 13, 13, 1)');
-    htmlEl.style.setProperty('--header-and-footer-text-color', 'rgba(255, 255, 255, 1)');
-    htmlEl.style.setProperty('--text-and-advantages-svg-stroke-color', 'rgba(255, 255, 255, 1)');
-    htmlEl.style.setProperty('--background-gradient', 'linear-gradient(180deg, rgba(13, 13, 13, 1) 22.12%, rgba(249, 249, 249, 0) 100%)');
+    bodyEl.setAttribute('data-theme-night', '');
     nightThemeToggleEl.classList.toggle('header__button-night-theme-hidden');
     dayThemeToggleEl.classList.toggle('header__button-day-theme-visible');
 });
 
 dayThemeToggleEl.addEventListener('click', () => {
-    htmlEl.style.setProperty('--button-color', 'rgba(253, 102, 94, 1)');
-    htmlEl.style.setProperty('--background-color', 'rgba(255, 255, 255, 1)');
-    htmlEl.style.setProperty('--header-and-footer-text-color', 'rgba(88, 88, 88, 1)');
-    htmlEl.style.setProperty('--text-and-advantages-svg-stroke-color', 'rgba(0, 0, 0, 1)');
-    htmlEl.style.setProperty('--background-gradient', 'linear-gradient(180deg, rgba(249, 249, 249, 1) 22.12%, rgba(249, 249, 249, 0) 100%)');
+    bodyEl.removeAttribute('data-theme-night');
     nightThemeToggleEl.classList.toggle('header__button-night-theme-hidden');
     dayThemeToggleEl.classList.toggle('header__button-day-theme-visible');
 });
