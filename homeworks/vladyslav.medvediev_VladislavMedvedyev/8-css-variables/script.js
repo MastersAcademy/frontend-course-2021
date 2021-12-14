1fe6d0ed749772946c76f15e4abe3e2f31a31647
VladislavMedvedyev <vlad.medvedyev@gmail.com>
2021-12-14 21:37:06 +0200

    bodyAttributeToggle

--- homeworks/vladyslav.medvediev_VladislavMedvedyev/8-css-variables/script.js
+++ homeworks/vladyslav.medvediev_VladislavMedvedyev/8-css-variables/script.js
@@ -1,15 +1,18 @@
 const nightThemeToggleEl = document.querySelector('[data-night-theme]');
 const dayThemeToggleEl = document.querySelector('[data-day-theme]');
 const bodyEl = document.querySelector('[data-body]');
+const buttonEl = document.querySelector('[data-button]');
 
-nightThemeToggleEl.addEventListener('click', () => {
-    bodyEl.setAttribute('data-theme-night', '');
-    nightThemeToggleEl.classList.toggle('header__button-night-theme-hidden');
-    dayThemeToggleEl.classList.toggle('header__button-day-theme-visible');
-});
+function bodyAttributeToggle() {
+    if (bodyEl.hasAttribute('data-theme-night')) {
+        bodyEl.removeAttribute('data-theme-night');
+    } else {
+        bodyEl.setAttribute('data-theme-night', '');
+    }
+}
 
-dayThemeToggleEl.addEventListener('click', () => {
-    bodyEl.removeAttribute('data-theme-night');
+buttonEl.addEventListener('click', () => {
+    bodyAttributeToggle();
     nightThemeToggleEl.classList.toggle('header__button-night-theme-hidden');
     dayThemeToggleEl.classList.toggle('header__button-day-theme-visible');
 });
