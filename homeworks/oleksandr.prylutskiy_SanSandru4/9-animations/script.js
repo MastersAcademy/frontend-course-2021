f0f172376353521913870498fbe648e71b129f78
Oleksandr Prylutskiy <fleks1986@gmail.com>
2021-12-16 23:14:21 +0200

    all animation is ready

--- homeworks/oleksandr.prylutskiy_SanSandru4/9-animations/script.js
+++ homeworks/oleksandr.prylutskiy_SanSandru4/9-animations/script.js
@@ -2,6 +2,35 @@ const changeSkin = document.querySelector('[data-attr-change-skin]');
 const iconSkinEl = document.querySelector('[data-attr-icon]');
 const bodyEl = document.querySelector('body');
 const currentTheme = localStorage.getItem('skin');
+const preloaderEl = document.querySelector('[data-preloader]');
+const toTopEl = document.querySelector('[data-to-top]');
+
+document.body.onload = (() => {
+    setTimeout(() => {
+        if (!preloaderEl.classList.contains('done')) {
+            preloaderEl.classList.add('done');
+        }
+    }, 1000);
+});
+
+function trackScroll() {
+    const scrolled = window.pageYOffset;
+    const coords = document.documentElement.clientHeight;
+
+    if (scrolled > coords) {
+        toTopEl.classList.add('to-top--show');
+    }
+    if (scrolled < coords) {
+        toTopEl.classList.remove('to-top--show');
+    }
+}
+
+function backToTop() {
+    if (window.pageYOffset > 0) {
+        window.scrollBy(0, -80);
+        setTimeout(backToTop, 20);
+    }
+}
 
 function setTheme(name) {
     bodyEl.setAttribute('data-skin', name);
@@ -23,6 +52,8 @@ function init() {
             setTheme('light');
         }
     });
+    window.addEventListener('scroll', trackScroll);
+    toTopEl.addEventListener('click', backToTop);
 }
 
 init();
