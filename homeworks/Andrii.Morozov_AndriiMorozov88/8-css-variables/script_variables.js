084a4337a23201d3511d10616b7e83d7c1ef7ab2
Andrii Morozov <slon88888@ukr.net>
2021-12-17 01:03:16 +0200

    fixed

--- homeworks/Andrii.Morozov_AndriiMorozov88/8-css-variables/script_variables.js
+++ homeworks/Andrii.Morozov_AndriiMorozov88/8-css-variables/script_variables.js
@@ -1,7 +1,9 @@
 const page = document.querySelector('html');
 const toggleButton = document.querySelector('[data-toggle]');
 const halfmoon = document.querySelector('[data-halfmoon]');
+const sun = document.querySelector('[data-sun]');
 toggleButton.addEventListener('click', () => {
     page.classList.toggle('html--night');
     halfmoon.classList.toggle('themes-toggle__button--hidden');
+    sun.classList.toggle('themes-toggle__button--active');
 });
