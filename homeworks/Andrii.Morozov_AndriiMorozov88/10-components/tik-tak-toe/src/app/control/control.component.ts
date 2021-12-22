cf78fd45fabf47ae7348d3ff5368fb343e4e7e8e
Andrii Morozov <slon88888@ukr.net>
2021-12-22 22:51:08 +0200

    added field component

--- homeworks/Andrii.Morozov_AndriiMorozov88/10-components/tik-tak-toe/src/app/control/control.component.ts
+++ homeworks/Andrii.Morozov_AndriiMorozov88/10-components/tik-tak-toe/src/app/control/control.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-control',
     templateUrl: './control.component.html',
     styleUrls: ['./control.component.css']
 })
-export class ControlComponent implements OnInit {}
+export class ControlComponent {
+
+}
