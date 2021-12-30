31b135794f0b474347c92792f106833970a53755
Andrii Morozov <slon88888@ukr.net>
2021-12-30 17:33:56 +0200

    added animation

--- homeworks/Andrii.Morozov_AndriiMorozov88/8-css-variables/script_variables.js
+++ homeworks/Andrii.Morozov_AndriiMorozov88/8-css-variables/script_variables.js
@@ -2,66 +2,40 @@ const page = document.querySelector('html');
 const toggleButton = document.querySelector('[data-toggle]');
 const halfmoon = document.querySelector('[data-halfmoon]');
 const sun = document.querySelector('[data-sun]');
-// const picture = document.querySelectorAll('picture');
+const imageIn = document.querySelectorAll('[data-image-in]');
+const buttonUp = document.querySelector('[data-button-up]');
 toggleButton.addEventListener('click', () => {
     page.classList.toggle('html--dark');
     halfmoon.classList.toggle('themes-toggle__button--hidden');
     sun.classList.toggle('themes-toggle__button--active');
 });
-
-/* const dreamhouse = document.querySelector('[data-dreamhouse]');
-const design = document.querySelector('[data-design]');
-const archmove = document.querySelector('[data-archmove]');
-const dreamProject = document.querySelector('[data-dream-project]');
-function imageIntersectDreamhouse(entry) {
-    if (entry[0].isIntersecting) {
-        picture[0].classList.remove('photo--hidden');
-        picture[0].classList.add('photo--animate');
-    }
-}
-function imageIntersectDesign(entry) {
-    if (entry[0].isIntersecting) {
-        picture[1].classList.remove('photo--hidden');
-        picture[2].classList.remove('photo--hidden');
-        picture[3].classList.remove('photo--hidden');
-        picture[4].classList.remove('photo--hidden');
-    }
-}
-function imageIntersectArchmove(entry) {
-    if (entry[0].isIntersecting) {
-        picture[5].classList.remove('photo--hidden');
-    }
-}
-function imageIntersectDreamProject(entry) {
-    if (entry[0].isIntersecting) {
-        picture[6].classList.remove('photo--hidden');
-    }
-}
 const options = {
     root: null,
     rootMargin: '0px',
-    threshold: 1.0,
+    threshold: 0.7,
 };
-const dreamhouseObserver = new IntersectionObserver(imageIntersectDreamhouse, options);
-dreamhouseObserver.observe(dreamhouse);
-const designObserver = new IntersectionObserver(imageIntersectDesign, options);
-designObserver.observe(design);
-const archmoveObserver = new IntersectionObserver(imageIntersectArchmove, options);
-archmoveObserver.observe(archmove);
-const dreamProjectObserver = new IntersectionObserver(imageIntersectDreamProject, options);
-dreamProjectObserver.observe(dreamProject); */
-
-const buttonUp = document.querySelector('[data-button-up]');
+function imageIntersect(entry) {
+    if (entry[0].isIntersecting) {
+        entry[0].target.classList.add('image-animation');
+        setTimeout(() => {
+            entry[0].target.classList.remove('image-translate');
+        }, 3000);
+    }
+}
+const observer = new IntersectionObserver(imageIntersect, options);
+for (let count = 0; count < imageIn.length; count++) {
+    observer.observe(imageIn[count]);
+}
+window.addEventListener('scroll', () => {
+    if (window.scrollY < document.documentElement.clientHeight) {
+        buttonUp.classList.replace('button__up--active', 'button__up--hidden');
+    } else {
+        buttonUp.classList.replace('button__up--hidden', 'button__up--active');
+    }
+});
 buttonUp.addEventListener('click', () => {
     window.scrollTo({
         top: 0,
         behavior: 'smooth',
     });
-});
-window.addEventListener('scroll', () => {
-    if (window.scrollY > document.documentElement.clientHeight) {
-        buttonUp.classList.replace('button__up--hidden', 'button__up--active');
-    } else {
-        buttonUp.classList.replace('button__up--active', 'button__up--hidden');
-    }
 });
