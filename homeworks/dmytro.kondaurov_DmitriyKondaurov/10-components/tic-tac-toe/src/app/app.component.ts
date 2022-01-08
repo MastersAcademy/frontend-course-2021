aa732ff101b852dbd417e5f0f977c99b142798d0
Dmytro Kondaurov <invisibl@ukr.net>
2022-01-08 11:48:55 +0200

    all game flow logic done

--- homeworks/dmytro.kondaurov_DmitriyKondaurov/10-components/tic-tac-toe/src/app/app.component.ts
+++ homeworks/dmytro.kondaurov_DmitriyKondaurov/10-components/tic-tac-toe/src/app/app.component.ts
@@ -26,23 +26,34 @@ export class AppComponent {
     }
 
     stateCheck(player:number) {
-        if (player === 1) {
-            for (const row in this.state) {
-                if (this.state[row].every((item:number) => item === 1)) {
-                    this.scorePl1++;
-                    alert('Player 1 win!')
-                    this.resetCurGame();
-                }
+        for (const row in this.state) {
+            if (this.state[row].every((item:number) => item === 1)) {
+                this.scorePl1++;
+                alert('Player 1 win!')
+                this.resetCurGame();
+            } else if (this.state[row].every((item:number) => item === 2)) {
+                this.scorePl2++;
+                alert('Player 2 win!')
+                this.resetCurGame();
             }
-
-        } else {
-            for (const row in this.state) {
-                if (this.state[row].every((item:number) => item === 2)) {
-                    this.scorePl2++;
-                    alert('Player 2 win!')
-                    this.resetCurGame();
-                }
-            }
+        }
+        if (this.state[0].every((item:number) => item != 0)
+            && this.state[1].every((item:number) => item != 0)
+            && this.state[2].every((item:number) => item != 0)
+        ) {
+            alert('Draw!')
+            this.resetCurGame();
+        }
+        if (
+            (this.state[0][0] === player && this.state[1][0] === player && this.state[2][0] === player)
+            || (this.state[0][1] === player && this.state[1][1] === player && this.state[2][1] === player)
+            || (this.state[0][2] === player && this.state[1][2] === player && this.state[2][2] === player)
+            || (this.state[0][0] === player && this.state[1][1] === player && this.state[2][2] === player)
+            || (this.state[0][2] === player && this.state[1][1] === player && this.state[2][0] === player)
+        ) {
+            player === 1? this.scorePl1++ : this.scorePl2++;
+            alert('Player '+player+' win!')
+            this.resetCurGame();
         }
     }
 }
