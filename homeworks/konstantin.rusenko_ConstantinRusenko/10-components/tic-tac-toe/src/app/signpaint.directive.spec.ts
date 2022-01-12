6bde3fd3346aed02b088d0434ba92651b4440123
ConstantinRusenko <krusenko@gmail.com>
2022-01-12 23:32:51 +0200

    change

--- homeworks/konstantin.rusenko_ConstantinRusenko/10-components/tic-tac-toe/src/app/signpaint.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SignpaintDirective } from './signpaint.directive';
-
-describe('SignpaintDirective', () => {
-    it('should create an instance', () => {
-        const directive = new SignpaintDirective();
-        expect(directive).toBeTruthy();
-    });
-});
