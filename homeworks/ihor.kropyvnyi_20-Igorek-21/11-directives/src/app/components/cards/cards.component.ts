9b692defd0196aa4f9d8369ca449dcdd81f1deb5
20-Igorek-21 <igor.kropyvnyi@gmail.com>
2022-01-15 02:08:05 +0200

    create cards component

---
+++ homeworks/ihor.kropyvnyi_20-Igorek-21/11-directives/src/app/components/cards/cards.component.ts
@@ -0,0 +1,36 @@
+import { Component,  OnDestroy, OnInit } from '@angular/core';
+import { Recipes} from '../../models/recipes.model';
+import { finalize, Subscription } from 'rxjs';
+import {GetRecipesService} from '../../services';
+
+@Component({
+    selector: 'app-cards',
+    templateUrl: './cards.component.html',
+    styleUrls: ['./cards.component.css']
+})
+export class CardsComponent implements OnInit, OnDestroy{
+
+  recipes: Recipes[] = [];
+  isLoader = false;
+  private subscription: Subscription = new Subscription();
+
+  constructor( private getRecipes: GetRecipesService ) { }
+
+  ngOnInit(): void {
+      this.isLoader = true;
+      this.subscription.add(this.getRecipes.getRecipes()
+          .pipe( (finalize( () => {
+              this.isLoader = false;
+          })))
+          .subscribe({
+              next: (data: Recipes[]): void => {
+                  this.recipes = (data);
+              }
+          })
+      )
+  }
+
+  ngOnDestroy() {
+      this.subscription.unsubscribe()
+  }
+}
