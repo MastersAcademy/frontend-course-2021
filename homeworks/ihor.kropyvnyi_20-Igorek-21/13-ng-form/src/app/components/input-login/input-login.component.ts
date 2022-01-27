a86d7964db0bbd33f159ff6a0c708bdd764ddbb3
20-Igorek-21 <igor.kropyvnyi@gmail.com>
2022-01-28 00:48:21 +0200

    create new component input-login

---
+++ homeworks/ihor.kropyvnyi_20-Igorek-21/13-ng-form/src/app/components/input-login/input-login.component.ts
@@ -0,0 +1,46 @@
+import {Component, forwardRef, Input} from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
+import {LoginPageComponent} from '../login-page/login-page.component';
+
+@Component({
+    selector: 'app-input-login',
+    templateUrl: './input-login.component.html',
+    styleUrls: ['./input-login.component.css'],
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            useExisting: forwardRef( () => InputLoginComponent),
+            multi: true
+        }
+    ]
+})
+export class InputLoginComponent implements ControlValueAccessor {
+
+    @Input() value = '';
+
+    constructor(public loginPageComponent: LoginPageComponent) {}
+
+    public handleInput(value: string) {
+        this.onChange(value)
+    }
+
+    public writeValue(value: string) {
+        this.value = value
+    }
+
+    private onChange(value: string) {
+        this.value = value
+    }
+
+    public onTouched() {
+        console.log('touch')
+    }
+
+    public registerOnChange(fn: any) {
+        this.onChange = fn
+    }
+
+    public registerOnTouched(fn: any) {
+        this.onTouched = fn
+    }
+}
