9f0c282bfd7f88df7b828d618c1d34e9f4cfbf3b
ConstantinRusenko <krusenko@gmail.com>
2022-01-29 02:04:22 +0200

    Revert "fix linter mistake"
    This reverts commit 03c32363d410422ff5a6f978ee8915967af3a356.

--- homeworks/konstantin.rusenko_ConstantinRusenko/12-http/http/src/app/app.component.spec.ts
+++ homeworks/konstantin.rusenko_ConstantinRusenko/12-http/http/src/app/app.component.spec.ts
@@ -20,4 +20,16 @@ describe('AppComponent', () => {
         expect(app).toBeTruthy();
     });
 
+    it('should have as title \'http\'', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        const app = fixture.componentInstance;
+        expect(app.title).toEqual('http');
+    });
+
+    it('should render title', () => {
+        const fixture = TestBed.createComponent(AppComponent);
+        fixture.detectChanges();
+        const compiled = fixture.nativeElement as HTMLElement;
+        expect(compiled.querySelector('.content span')?.textContent).toContain('http app is running!');
+    });
 });
