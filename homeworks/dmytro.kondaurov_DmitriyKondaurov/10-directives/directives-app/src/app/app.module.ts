99692add91d2fa7b0ba3ea2a40ac7dd535e5396a
Dmytro Kondaurov <invisibl@ukr.net>
2022-03-14 12:27:47 +0200

    fix app component for style card component

--- homeworks/dmytro.kondaurov_DmitriyKondaurov/10-directives/directives-app/src/app/app.module.ts
+++ homeworks/dmytro.kondaurov_DmitriyKondaurov/10-directives/directives-app/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { CardComponent } from './components/card/card.component';
 
@@ -15,4 +14,4 @@ import { CardComponent } from './components/card/card.component';
     providers: [],
     bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
